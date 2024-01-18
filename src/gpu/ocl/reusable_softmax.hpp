e1e17bd32e10ee9cc9f79e754259e76b30128cf8
Gallagher Pryor <gallagher.pryor@intel.com>
2024-01-18 09:35:50 -0500

    gpu: ocl: softmax: initial unoptimized reusable implementation
    This commit entails a baseline reusable softmax implementation. One
    OpenCL kernel handles all parameterizations of softmax invocation
    except for data type. It borrows heavily from the reference
    implementation to focus on demonstrating new implementation for a
    primitive and reusable dispatch.

--- src/gpu/ocl/reusable_softmax.hpp
+++ src/gpu/ocl/reusable_softmax.hpp
@@ -20,6 +20,7 @@
 #include "common/c_types_map.hpp"
 #include "common/nstl.hpp"
 #include "common/primitive.hpp"
+#include "gpu/compute/compute.hpp"
 #include "gpu/compute/dispatch_reusable.hpp"
 #include "gpu/gpu_primitive.hpp"
 #include "gpu/gpu_resource.hpp"

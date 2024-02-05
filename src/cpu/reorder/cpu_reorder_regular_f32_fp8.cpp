49b9b626e162c2d74e5b81f250f60a5bf91d8bad
Tomasz Czeszun <tomasz.czeszun@intel.com>
2024-02-05 13:51:54 -0800

    x64: enable fp8 reorder through emulation

--- src/cpu/reorder/cpu_reorder_regular_f32_fp8.cpp
+++ src/cpu/reorder/cpu_reorder_regular_f32_fp8.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2023-2025 Intel Corporation
+* Copyright 2023-2024 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -31,7 +31,6 @@ const impl_list_map_t &regular_f32_fp8_impl_list_map() {
         }},
         // f32 -> f8_e5m2
         {{f32, f8_e5m2, 0}, {
-            DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_uni_reorder_direct_copy_t))
             DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_blk_reorder_t))
             DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_uni_reorder_t))
 
@@ -41,7 +40,6 @@ const impl_list_map_t &regular_f32_fp8_impl_list_map() {
         }},
         // f32 -> f8_e4m3
         {{f32, f8_e4m3, 0}, {
-            DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_uni_reorder_direct_copy_t))
             DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_blk_reorder_t))
             DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_uni_reorder_t))
 

e35064b0de3086c53f225896cc5ab2d8732d4f73
Jonathan Deakin <jonathan.deakin@arm.com>
2024-02-07 16:00:35 +0000

    cpu: aarch64: softmax: fall back to ref if stag != dtag
    `acl_softmax` would return incorrect answer if stag did not match dtag,
    for example
    ```sh
    ./benchdnn -v5 --softmax --stag=axb --dtag=abx 2x19x16x64
    ```
    This patch fixes that by falling back to ref if stag != dtag.

--- src/cpu/aarch64/acl_softmax.hpp
+++ src/cpu/aarch64/acl_softmax.hpp
@@ -43,7 +43,117 @@ struct acl_softmax_fwd_t : public primitive_t {
         using cpu_softmax_fwd_pd_t::cpu_softmax_fwd_pd_t;
 
         DECLARE_COMMON_PD_T("acl", acl_softmax_fwd_t);
-        status_t init(engine_t *engine);
+
+        status_t init(engine_t *engine) {
+
+            bool ok = is_fwd()
+                    && set_default_formats() == status::success
+                    // ACL only supports matching src/dst (this must come after
+                    // set_default_formats() to handle format_kind::any)
+                    && *src_md() == *dst_md()
+                    && utils::one_of(
+                            src_md()->data_type, data_type::f32, data_type::f16)
+                    && attr()->has_default_values();
+            if (!ok) return status::unimplemented;
+
+            // Get memory desc to find sizes and dims
+            const memory_desc_wrapper src_d(src_md());
+            const data_type_t data_type = src_d.data_type();
+
+            // ACL only supports plain tensors, can be permuted but not blocked
+            if (!src_d.is_plain()) return status::unimplemented;
+
+            // Guards against a 0-sized dimension
+            if (src_d.has_zero_dim()) return status::unimplemented;
+
+            // No scaling
+            asp_.beta = 1;
+
+            asp_.is_logsoftmax = is_logsoftmax();
+
+            // The strides give us the in memory inner size
+            dim_t inner_size_ = src_d.blocking_desc().strides[axis()];
+
+            dim_t axis_size_ = axis_size();
+
+            // The outer size is any left-over dimensions not inner or on the axis
+            dim_t outer_size_ = src_d.nelems() / (inner_size_ * axis_size_);
+
+            // In this context, NHWC tells ACL that the logical and physical
+            // dimensions are the same
+            arm_compute::DataLayout acl_layout = arm_compute::DataLayout::NHWC;
+
+            const arm_compute::DataType acl_data_t
+                    = acl_utils::get_acl_data_t(data_type);
+
+            const int threads = dnnl_get_max_threads();
+            if (inner_size_ == 1) {
+                // A rough empirical heuristic created by fitting a polynomial
+                // of the tensor sizes and thread count to the run time of the
+                // ref and ACL softmax. This variable is greater than zero when
+                // ref is faster, and less than zero when ACL is faster. We can
+                // interpret the constant term as the constant overhead
+                // associated with calling the external library and the negative
+                // coefficient on total_size as ACL being faster at processing
+                // each element
+                double acl_ref_performance_diff = 1 + 0.005 * outer_size_
+                        - 0.0027 * axis_size_
+                                * std::ceil(double(outer_size_) / threads);
+                if (threads > 1 || outer_size_ > 1) {
+                    // Using threads within ACL adds another constant overhead
+                    acl_ref_performance_diff += 17;
+                }
+                if (acl_ref_performance_diff > 0) return status::unimplemented;
+
+                // If the inner size is 1, we can get rid of the dimension.
+                // This stops ACL doing a unnecessary permute
+                arm_compute::TensorShape acl_tensor_shape
+                        = arm_compute::TensorShape(axis_size_, outer_size_);
+                asp_.axis = 0;
+
+                asp_.src_info = arm_compute::TensorInfo(
+                        acl_tensor_shape, 1, acl_data_t, acl_layout);
+                asp_.dst_info = arm_compute::TensorInfo(
+                        acl_tensor_shape, 1, acl_data_t, acl_layout);
+            } else {
+                // A rough empirical heuristic, see comment above
+                // The only difference here is that ACL does a reorder, and so
+                // is considerably better
+                double acl_ref_performance_diff = 1 + 0.005 * outer_size_
+                        - 0.01 * inner_size_ * axis_size_
+                                * std::ceil(double(outer_size_) / threads);
+                if (threads > 1 || outer_size_ > 1) {
+                    // Using threads within ACL adds another constant overhead
+                    acl_ref_performance_diff += 17;
+                }
+
+                if (acl_ref_performance_diff > 0) return status::unimplemented;
+
+                // Irrespective of the input dimensions, we construct a tensor
+                // with dimensions such that softmax can be applied over the
+                // middle axis (1), with the correct stride and vector length.
+                arm_compute::TensorShape acl_tensor_shape
+                        = arm_compute::TensorShape(
+                                inner_size_, axis_size_, outer_size_);
+                asp_.axis = 1;
+
+                asp_.src_info = arm_compute::TensorInfo(
+                        acl_tensor_shape, 1, acl_data_t, acl_layout);
+                asp_.dst_info = arm_compute::TensorInfo(
+                        acl_tensor_shape, 1, acl_data_t, acl_layout);
+            }
+
+            // Validate manually to check for return status
+            if (asp_.is_logsoftmax) {
+                ACL_CHECK_VALID(arm_compute::NELogSoftmaxLayer::validate(
+                        &asp_.src_info, &asp_.dst_info, asp_.beta, asp_.axis));
+            } else {
+                ACL_CHECK_VALID(arm_compute::NESoftmaxLayer::validate(
+                        &asp_.src_info, &asp_.dst_info, asp_.beta, asp_.axis));
+            }
+
+            return status::success;
+        }
 
         acl_softmax_conf_t asp_;
     }; // pd_t

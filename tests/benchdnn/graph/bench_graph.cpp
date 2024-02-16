5413b0ec8022b3e3aa86771cc82ca951b2e50b74
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-02-15 22:00:16 -0800

    benchdnn: graph: add graph print under v7

--- tests/benchdnn/graph/bench_graph.cpp
+++ tests/benchdnn/graph/bench_graph.cpp
@@ -38,9 +38,9 @@ void check_correctness(const settings_t &s) {
         fw.rewrite(dg);
         BENCHDNN_PRINT(7, "[INFO] Graph dump:\n%s\n", dg.get_string().c_str());
 
-        const prb_t prb(dg, i_expected_n_partition);
-        const auto &cpp_pstr = case_to_str(s.json_file, i_in_shapes, i_op_attrs,
-                i_fpmath_mode, i_expected_n_partition, i_mb, i_dt);
+        const prb_t prb(dg, i_fpmath_mode);
+        const auto &cpp_pstr
+                = case_to_str(s.json_file, i_in_shapes, i_op_attrs, i_mb);
         const char *pstr = cpp_pstr.c_str();
         BENCHDNN_PRINT(1, "run: %s\n", pstr);
         res_t res {};

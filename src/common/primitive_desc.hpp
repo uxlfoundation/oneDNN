66ff9021d9844b20978ee5a73f93d2acfef38585
syurkevi <stephen.yurkevitch@intel.com>
2024-02-21 21:21:52 -0800

    common: expand cache-hit/miss verbose messages

--- src/common/primitive_desc.hpp
+++ src/common/primitive_desc.hpp
@@ -24,7 +24,7 @@
 #include "c_types_map.hpp"
 #include "cache_blob.hpp"
 #include "cache_blob_id.hpp"
-#include "cache_hit_types.hpp"
+#include "cache_stats_types.hpp"
 #include "memory_tracking.hpp"
 #include "nstl.hpp"
 #include "opdesc.hpp"
@@ -381,7 +381,7 @@ struct primitive_desc_t : public c_compatible {
     }
 
     virtual status_t create_primitive(
-            std::pair<std::shared_ptr<primitive_t>, cache_state_t> &primitive,
+            std::pair<std::shared_ptr<primitive_t>, cache_hit_t> &primitive,
             engine_t *engine, const cache_blob_t &cache_blob) const = 0;
 
     // This is a proxy interface that is used for creating nested primitives.
@@ -390,24 +390,13 @@ struct primitive_desc_t : public c_compatible {
     status_t create_primitive(std::shared_ptr<primitive_t> &primitive,
             engine_t *engine,
             const cache_blob_t &cache_blob = cache_blob_t()) const {
-        std::pair<std::shared_ptr<primitive_t>, cache_state_t> p;
-        status_t status = create_primitive_nested(p, engine, cache_blob);
-        primitive = p.first;
-        return status;
-    }
-
-    // This is a proxy interface that is used for explicitly creating nested primitives,
-    // This version is used when cache_state_t is required for further analysis
-    status_t create_primitive_nested(
-            std::pair<std::shared_ptr<primitive_t>, cache_state_t> &primitive,
-            engine_t *engine,
-            const cache_blob_t &cache_blob = cache_blob_t()) const {
+        std::pair<std::shared_ptr<primitive_t>, cache_hit_t> p;
         if (get_verbose(verbose_t::debuginfo) >= 1) {
             double start_ms = get_msec();
             CHECK(create_primitive(primitive, engine, cache_blob));
             double duration_ms = get_msec() - start_ms;
-            if (cache_blob) primitive.second = cache_state_t::persistent_hit;
-            const char *str = cache_state2str(primitive.second);
+            if (cache_blob) p.second = cache_hit_t::persistent_cache_hit;
+            const char *str = cache_hit_string(p.second);
 
             VPROF(start_ms, primitive, create_nested, str, info(engine),
                     duration_ms);
@@ -496,7 +485,7 @@ protected:
         return new_pd.release(); \
     } \
     status_t create_primitive( \
-            std::pair<std::shared_ptr<impl::primitive_t>, bool> &primitive, \
+            std::pair<std::shared_ptr<impl::primitive_t>, cache_hit_t> &primitive, \
             dnnl::impl::engine_t *engine, const cache_blob_t &cache_blob) \
             const override { \
         return primitive_t::create_primitive_common<impl_type, pd_t>( \

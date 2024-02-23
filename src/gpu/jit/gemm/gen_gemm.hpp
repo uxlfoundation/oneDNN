5e99f03c721ae3cdbdd814d8e34c89e31f0b199a
Kealan Barbieri <kealan.barbieri@intel.com>
2024-02-23 09:40:08 -0800

    gpu: jit: gemm: per_oc scale fixes

--- src/gpu/jit/gemm/gen_gemm.hpp
+++ src/gpu/jit/gemm/gen_gemm.hpp
@@ -121,8 +121,25 @@ struct gen_gemm_t : public gpu_gemm_t {
 
             // Check parameters.
             if (utils::one_of(d->c_type(), s32, f16, f32, u8, s8)
-                    && utils::one_of(d->a_type(), u8, s8, u4, s4)) {
-                ok &= (utils::one_of(d->b_type(), u8, s8) || wei_decomp_);
+                    && utils::one_of(d->a_type(), u8, s8)) {
+                ok &= (utils::one_of(d->b_type(), u8, s8) || wei_decomp);
+                bool a_zp
+                        = !attr()->zero_points_.has_default_values(DNNL_ARG_A);
+                bool b_zp
+                        = !attr()->zero_points_.has_default_values(DNNL_ARG_B);
+
+                int cmask_a = 0, cmask_b = 0, cmask_c = 0;
+                CHECK(attr()->zero_points_.get(DNNL_ARG_A, &cmask_a));
+                CHECK(attr()->zero_points_.get(DNNL_ARG_B, &cmask_b));
+                CHECK(attr()->zero_points_.get(DNNL_ARG_C, &cmask_c));
+                ok &= utils::one_of(cmask_a, 0, 1 << 1, 1 << 2)
+                        && utils::one_of(cmask_b, 0, 1 << 0)
+                        && utils::one_of(cmask_c, 0, 1 << 0, 1 << 1);
+
+                ao_dims_ = a_zp ? (cmask_a != 0 ? 1 : 0) : -1;
+                bo_dims_ = b_zp ? (cmask_b != 0 ? 1 : 0) : -1;
+                if (swap_ab_) std::swap(ao_dims_, bo_dims_);
+
                 attr_skip_mask |= smask_t::zero_points_runtime;
 
                 ok = ok
@@ -160,33 +177,10 @@ struct gen_gemm_t : public gpu_gemm_t {
                                             DNNL_ARG_DST)))
                     && attr()->post_ops_.check_sum_consistency(
                             d->c_type(), utils::one_of(d->a_type(), s8, u8));
-
-            if (!attr()->zero_points_.has_default_values()) {
-                bool a_zp = !attr_zps.has_default_values(DNNL_ARG_A);
-                bool b_zp = !attr_zps.has_default_values(DNNL_ARG_B);
-
-                int cmask_a = 0, cmask_b = 0, cmask_c = 0;
-                CHECK(attr_zps.get(DNNL_ARG_A, &cmask_a));
-                CHECK(attr_zps.get(DNNL_ARG_B, &cmask_b));
-                CHECK(attr_zps.get(DNNL_ARG_C, &cmask_c));
-
-                wei_zp = a_zp;
-                wei_zp_2d = wei_decomp_ && (cmask_a == ((1 << 0) | (1 << 1)));
-                ok &= (utils::one_of(cmask_a, 0, 1 << 1, 1 << 2) || wei_zp_2d)
-                        && utils::one_of(cmask_b, 0, 1 << 0)
-                        && utils::one_of(cmask_c, 0, 1 << 0, 1 << 1);
-
-                ao_dims_ = a_zp ? (cmask_a != 0 ? 1 : 0) : -1;
-                bo_dims_ = b_zp ? (cmask_b != 0 ? 1 : 0) : -1;
-                if (wei_zp_2d) ao_dims_ = 2;
-                if (swap_ab_) std::swap(ao_dims_, bo_dims_);
-
-                if (wei_zp_2d) {
-                    wei_q2d_group_k
-                            = attr_zps.get_groups_ndims(DNNL_ARG_WEIGHTS) > 0
-                            ? attr_zps.get_groups(DNNL_ARG_WEIGHTS)[0]
-                            : 1;
-                }
+            for (const auto &s :
+                    {DNNL_ARG_SRC, DNNL_ARG_WEIGHTS, DNNL_ARG_DST}) {
+                ok &= utils::one_of(attr()->scales_.get(s).mask_, 0, 1 << 0,
+                        1 << 1, 1 << 2);
             }
 
             if (wei_decomp_

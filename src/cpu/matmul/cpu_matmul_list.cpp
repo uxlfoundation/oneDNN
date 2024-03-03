8d9653739d8c5d21121cb6f9acc91f8ad0cf7eb2
Yair Nahum <yair.y.nahum@intel.com>
2024-03-03 10:19:52 +0200

    cpu: x64: matmul: add imp of avx10_2_512_AMX_2 to the matmul list
    1. added imp of matmul that is avx10_2_512_AMX_2 ISA compliant
    2. fix data type checks prevented avx10_2_512_amx_2 to run
    3. fix to use isa and not hard coded isa in matmul creation
    4. added imp of matmul to imp list that is avx10_2_512 ISA

--- src/cpu/matmul/cpu_matmul_list.cpp
+++ src/cpu/matmul/cpu_matmul_list.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2019-2024 Intel Corporation
+* Copyright 2019-2025 Intel Corporation
 * Copyright 2024 FUJITSU LIMITED
 * Copyright 2021-2025 Arm Ltd. and affiliates
 *
@@ -79,8 +79,10 @@ constexpr impl_list_item_t impl_list[] = REG_MATMUL_P({
         CPU_INSTANCE_AARCH64_ACL(acl_lowp_matmul_t)
         CPU_INSTANCE_AARCH64_ACL(acl_matmul_t)
         CPU_INSTANCE_AARCH64(brgemm_matmul_t<sve_256>)
+        CPU_INSTANCE_AMX(brgemm_matmul_t<avx10_2_512_amx_2>)
         CPU_INSTANCE_AMX(brgemm_matmul_t<avx512_core_amx_fp16>)
         CPU_INSTANCE_AMX(brgemm_matmul_t<avx512_core_amx>)
+        CPU_INSTANCE_AVX512(brgemm_matmul_t<avx10_2_512>)
         CPU_INSTANCE_AVX512(brgemm_matmul_t<avx512_core_fp16>)
         CPU_INSTANCE_AVX512(brgemm_matmul_t<avx512_core_bf16>)
         CPU_INSTANCE_AVX512(brgemm_matmul_t<avx512_core_vnni>)

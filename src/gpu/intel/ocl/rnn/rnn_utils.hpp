679d5fdab88cacb2ccb813b5af4cb6b1c41dd151
Roy Oursler <roy.j.oursler@intel.com>
2024-03-05 07:54:07 -0800

    gpu: ocl: rnn: remove use of get_sub_storage
    Logic for sub storage size is getting complicated and not worth maintaining
    without a performance benefit.

--- src/gpu/intel/ocl/rnn/rnn_utils.hpp
+++ src/gpu/intel/ocl/rnn/rnn_utils.hpp
@@ -413,9 +413,6 @@ struct sub_buffer_t {
     }
 
 private:
-    static constexpr bool use_clone(dim_t size, dim_t offset) {
-        return size == unset || offset % OCL_BUFFER_ALIGNMENT != 0;
-    }
     std::unique_ptr<memory_storage_t> buffer_;
 };
 

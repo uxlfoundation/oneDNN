5931c76b4a7849827b895a8e258492d3c8ec5622
Roy Oursler <roy.j.oursler@intel.com>
2024-03-05 07:54:07 -0800

    gpu: ocl: rnn: remove use of get_sub_storage
    Logic for sub storage size is getting complicated and not worth maintaining
    without a performance benefit.

--- src/gpu/intel/ocl/rnn/rnn_utils.hpp
+++ src/gpu/intel/ocl/rnn/rnn_utils.hpp
@@ -383,13 +383,8 @@ struct sub_buffer_t {
     sub_buffer_t() : buffer_(nullptr) {}
     sub_buffer_t(const memory_storage_t &buffer, dim_t offset = 0,
             dim_t size = unset)
-        : buffer_(buffer.is_null()
-                        ? nullptr
-                        : (use_clone(size, offset) ? buffer.clone()
-                                                   : buffer.get_sub_storage(
-                                                           offset, size))) {
-        if (buffer_ && use_clone(size, offset))
-            buffer_->set_offset(static_cast<size_t>(offset));
+        : buffer_(buffer.is_null() ? nullptr : buffer.clone()) {
+        if (buffer_) buffer_->set_offset(static_cast<size_t>(offset));
     }
 
     sub_buffer_t(const sub_buffer_t &buffer, dim_t offset = 0)
@@ -416,9 +411,6 @@ struct sub_buffer_t {
     }
 
 private:
-    static constexpr bool use_clone(dim_t size, dim_t offset) {
-        return size == unset || offset % OCL_BUFFER_ALIGNMENT != 0;
-    }
     std::unique_ptr<memory_storage_t> buffer_;
 };
 

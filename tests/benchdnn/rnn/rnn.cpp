6f82a527dcab72bf35eb550e39884cfd35988703
Roy Oursler <roy.j.oursler@intel.com>
2024-03-07 10:52:09 -0800

    benchdnn: rnn: skip unsupported int8 weights layout

--- tests/benchdnn/rnn/rnn.cpp
+++ tests/benchdnn/rnn/rnn.cpp
@@ -836,6 +836,10 @@ void skip_unimplemented_prb(const prb_t *prb_, res_t *res) {
             res->reason = skip_reason::case_not_supported;
             return;
         }
+        if (is_gpu() && prb.tag[1] != tag::any) {
+            res->state = SKIPPED, res->reason = CASE_NOT_SUPPORTED;
+            return;
+        }
     }
 
     // LSTM w/ projection is not supported for bf16

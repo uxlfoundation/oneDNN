78f26d1ab61ac2c65c367ef2be94441e674d9fc9
Andrey Kalinin <andrey.kalinin@intel.com>
2024-03-08 12:46:08 -0800

    x64: brgemm unrolled kernel: support fp8 via conversion to f16

--- src/cpu/x64/brgemm/brgemm.cpp
+++ src/cpu/x64/brgemm/brgemm.cpp
@@ -576,8 +576,8 @@ status_t brgemm_desc_set_attr(
         brg->prfC.dist2 = 0;
 
     // TODO: update conditions once other brgemm implementations are enabled
-    // Currently, fp8 via AMX f16 convert only supported in non-unrolled kernel
-    if (brg->is_fp8 && brg->brgattr.use_uker) return status::unimplemented;
+    if (brg->is_fp8 && brg->brgattr.use_uker && !brg->is_fp8_via_convert())
+        return status::unimplemented;
 
     return status::success;
 }

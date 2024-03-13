039fe07a07dbc8bed2a3a16c0e5a622bfa917cab
Roy Oursler <roy.j.oursler@intel.com>
2024-03-13 14:50:54 -0700

    gpu: ocl: rnn: consolidate aux_data_type size logic

--- src/gpu/intel/ocl/rnn/rnn_utils.cpp
+++ src/gpu/intel/ocl/rnn/rnn_utils.cpp
@@ -297,10 +297,8 @@ void rnn_utils::set_rnn_conf(conf_t &rnn, const rnn_desc_t &rd,
     const bool is_fwd = rnn.is_fwd;
     const bool is_bwd = !rnn.is_fwd;
 
-    int sizeof_states_dt
-            = rnn.dt_conf == all_f32 ? sizeof(cl_float) : sizeof(cl_half);
-    int aux_elsz = rnn.aux_data_type == data_type::f16 ? sizeof(cl_half)
-                                                       : sizeof(float);
+    dim_t aux_elsz
+            = gpu_utils::into<dim_t>(types::data_type_size(rnn.aux_data_type));
     rnn.ws_states_elsz = types::data_type_size(rd.src_layer_desc.data_type);
 
     rnn.scratch_gates_elsz = types::data_type_size(rnn.acc_data_type);

f079a458321a55dcd39291ef93bf8269d8e2f9f1
Simon Ewing <simon.ewing@intel.com>
2024-03-13 09:58:14 -0700

    gpu: jit: introduce reduction injector and implementation

--- src/gpu/jit/jit_generator.hpp
+++ src/gpu/jit/jit_generator.hpp
@@ -106,12 +106,16 @@ template <gpu_gen_t hw>
 struct jit_eltwise_injector_f32;
 
 template <gpu_gen_t hw>
+struct jit_reduction_injector_f32;
+
+template <gpu_gen_t hw>
 struct jit_post_op_injector;
 
 template <gpu_gen_t hw>
 class jit_generator : public ngen::OpenCLCodeGenerator<hw>,
                       public jit_generator_base {
     friend struct jit_eltwise_injector_f32<hw>;
+    friend struct jit_reduction_injector_f32<hw>;
     friend struct jit_post_op_injector<hw>;
     friend struct EmulationImplementation;
 

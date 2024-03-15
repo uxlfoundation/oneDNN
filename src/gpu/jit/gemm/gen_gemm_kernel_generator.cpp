f9c4e78cd2370fa74238ad2a235bc4c3f45e2ae5
Kealan Barbieri <kealan.barbieri@intel.com>
2024-03-15 10:11:48 -0700

    gpu: jit: gemm: fix conversion from bf

--- src/gpu/jit/gemm/gen_gemm_kernel_generator.cpp
+++ src/gpu/jit/gemm/gen_gemm_kernel_generator.cpp
@@ -27364,7 +27364,6 @@ bool gemm_kernel_generator_t<hw>::copyRegisters(Type Ts, Type Td,
                             bool src_bf8 = (Ts_real == Type::bf8);
                             bool dst_bf8 = (Td_real == Type::bf8);
                             bool bf8_align = src_bf8 ^ dst_bf8;
-                            if (bf8_align) allocTemp();
 
                             // Check if separate conversions are needed due to size changes.
                             auto sconvertCP = Ts_real.isInt4()
@@ -27430,6 +27429,12 @@ bool gemm_kernel_generator_t<hw>::copyRegisters(Type Ts, Type Td,
                                                     && (sreg.getOffset() & 1)
                                                     && hw >= HW::XeHP));
                             dconvert &= !bf8_align;
+                            bool prepBF = (!sconvert && !dconvert)
+                                    && Ts_real == Type::bf16;
+                            if (prepBF)
+                                sregConverted = sreg.reinterpret(
+                                        0, ngen::DataType::f)(scrosspack);
+                            if (bf8_align || prepBF) allocTemp();
                             auto dregConverted = dconvert
                                     ? dreg.reinterpret(0, Ts_real.ngen())(
                                             dconvertCP)
@@ -27795,8 +27800,13 @@ bool gemm_kernel_generator_t<hw>::copyRegisters(Type Ts, Type Td,
                                                         sregConverted,
                                                         sreg(scrosspack));
                                             }
-                                        } else if (bfHfCvt) {
-                                            doBfHfCvt();
+                                        } else if (prepBF) {
+                                            shl(nelems_real,
+                                                    copyTemp[0].sub(
+                                                            sreg.getOffset(),
+                                                            ngen::DataType::ud)(
+                                                            scrosspack),
+                                                    sreg.uw()(scrosspack), 16);
                                         }
                                         break;
                                     case 0:
@@ -27851,6 +27861,15 @@ bool gemm_kernel_generator_t<hw>::copyRegisters(Type Ts, Type Td,
                                                                 ? base(1)
                                                                 : base(0, wd,
                                                                         1));
+                                            } else if (prepBF) {
+                                                emov(telems | mmodMov,
+                                                        dregConverted,
+                                                        copyTemp[0].sub(
+                                                                sreg.getOffset(),
+                                                                ngen::DataType::
+                                                                        f)(
+                                                                scrosspack),
+                                                        strategy, state);
                                             } else if (!byteAlign && !bf8_align)
                                                 emov(telems | mmodMov,
                                                         dregConverted,

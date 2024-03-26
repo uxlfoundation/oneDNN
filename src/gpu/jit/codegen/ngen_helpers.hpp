313021c819449f97812e2f0925200a2726994ad2
Kealan Barbieri <kealan.barbieri@intel.com>
2024-03-25 18:03:28 -0700

    gpu: jit: reorder: enable hf8

--- src/gpu/jit/codegen/ngen_helpers.hpp
+++ src/gpu/jit/codegen/ngen_helpers.hpp
@@ -63,6 +63,7 @@ inline ngen::DataType to_ngen(const type_t &type) {
     CASE(bf16, bf);
     CASE(f16, hf);
     CASE(bf8, bf8);
+    CASE(hf8, hf8);
     CASE(tf32, tf32);
     CASE(f32, f);
     CASE(f64, df);

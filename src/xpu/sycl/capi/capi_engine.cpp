9e5a3567c20ece2d09f5e84010873d2c290f68e0
Ankit Manerikar <ankit.v.manerikar@intel.com>
2024-03-27 11:41:15 -0700

    gpu: ocl: engine: new verbose checks for ocl engine creation

--- src/xpu/sycl/capi/capi_engine.cpp
+++ src/xpu/sycl/capi/capi_engine.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2019-2025 Intel Corporation
+* Copyright 2019-2024 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -49,7 +49,7 @@ status_t dnnl_sycl_interop_engine_create(
             VERBOSE_BAD_ENGINE_KIND);
 #endif
 
-    auto ef = dnnl::impl::xpu::sycl::get_engine_factory(kind);
+    auto ef = dnnl::impl::sycl::get_engine_factory(kind);
     VERROR_ENGINE(ef, status::invalid_arguments, VERBOSE_BAD_ENGINE_KIND);
 
     size_t index;

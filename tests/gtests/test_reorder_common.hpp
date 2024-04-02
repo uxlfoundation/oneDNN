110a3a201bf45468e3ad12ca5afcee6f051beeb8
John Osorio <john.osorio@codeplay.com>
2024-04-02 14:16:28 +0100

    gtests: gpu: amd: Fix to a subset of gtests failures

--- tests/gtests/test_reorder_common.hpp
+++ tests/gtests/test_reorder_common.hpp
@@ -141,10 +141,6 @@ protected:
                                 || supported_blocking(prec_o, p.fmt_o))),
                 "Unsupported hip format tag/ data type");
 #endif
-#ifdef DNNL_SYCL_GENERIC
-        SKIP_IF(!(supported_format(p.fmt_i) && supported_format(p.fmt_o)),
-                "Unsupported generic format tag");
-#endif
 
         catch_expected_failures([&]() { RunTest(eng_i, eng_o); },
                 p.expect_to_fail, p.expected_status);

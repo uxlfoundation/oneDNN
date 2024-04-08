65c3159f9f78ceb185a8fd8c542a9ca0ecc919f2
Xuxin, Zeng <xuxin.zeng@intel.com>
2024-04-08 11:20:31 -0700

    cpu: x64: enable data type of scale support in brgemm matmul

--- src/cpu/scale_utils.hpp
+++ src/cpu/scale_utils.hpp
@@ -39,7 +39,7 @@ bool req_copy_scales(
 // to do so scratchpad memory for scales must be booked e.g. using
 // book_precomputed_scales with force_scales_book = true.
 const float *precompute_scales(const memory_tracking::grantor_t &scratchpad,
-        const float *src_scales, const float *wei_scales, dim_t oc,
+        const float *src_scales, const float *wei_scales, dim_t wei_scale_count,
         const primitive_attr_t *attr, float scale_adjust_factor = 1.0f);
 const float *precompute_scales(const memory_tracking::grantor_t &scratchpad,
         const float *src_scales, const float *wei_scales, dim_t ic, dim_t oc,

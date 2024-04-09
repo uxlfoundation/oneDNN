5d50552bf86a4d6c17ab96e8affd01de8052f416
Guskov, Andrey Y <andrey.y.guskov@intel.com>
2024-04-08 19:19:49 -0700

    src: gpu: jit: unify argument key types across generator

--- src/gpu/intel/jit/conv/zero_out.hpp
+++ src/gpu/intel/jit/conv/zero_out.hpp
@@ -51,6 +51,7 @@ public:
 
         auto size = getArgument(arg_names[0]);
         auto ptr = getArgument(arg_names[1]);
+        auto surf = Surface(getArgumentSurfaceIfExists(arg_names[1]));
         auto global_id = ra_.template alloc_sub<uint32_t>();
         auto off0 = ra_.template alloc_sub<uint32_t>();
 

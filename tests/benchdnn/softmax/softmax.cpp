642e85dfb13f5d1e185bb1102b5462ebbe9491e6
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-04-11 15:19:49 -0700

    benchdnn: softmax: increased threshold for relaxed computations in f16

--- tests/benchdnn/softmax/softmax.cpp
+++ tests/benchdnn/softmax/softmax.cpp
@@ -227,6 +227,8 @@ void setup_cmp(compare::compare_t &cmp, const prb_t *prb, data_kind_t kind,
         const args_t &ref_args) {
     const auto trh_dt = (prb->dir & FLAG_FWD) ? prb->ddt : prb->sdt;
     const bool is_flt_or_dbl = trh_dt == dnnl_f32 || trh_dt == dnnl_f64;
+    const bool is_strict_acc
+            = prb->attr.acc_mode == dnnl_accumulation_mode_strict;
     const float trh_coeff_log = prb->alg == LOGSOFTMAX ? 5 : 1;
     const float trh_coeff_f32 = is_flt_or_dbl ? 10.f : 1.f;
     const float trh_coeff_bwd = (prb->dir & FLAG_FWD) ? 1.f : 4.f;

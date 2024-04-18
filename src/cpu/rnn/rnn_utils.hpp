0c041cc5e225b4512a49d589f1ee085c8ba90b36
Yair Nahum <yair.y.nahum@intel.com>
2024-04-18 10:49:35 +0300

    cpu: x64: rnn: check for int8 case if we can use new avx10 isa

--- src/cpu/rnn/rnn_utils.hpp
+++ src/cpu/rnn/rnn_utils.hpp
@@ -414,7 +414,9 @@ struct rnn_conf_t {
 
     inline bool is_cell_int8_amx() const {
 #if DNNL_X64
-        return brgemm_isa == x64::avx512_core_amx && is_cell_dt_int8();
+        return is_cell_dt_int8()
+                && utils::one_of(brgemm_isa, x64::avx512_core_amx,
+                        x64::avx10_2_512_amx_2);
 #else
         return false;
 #endif

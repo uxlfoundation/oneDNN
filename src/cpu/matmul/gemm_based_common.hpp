5902b84604f3f72f43f9eaf8e65041c080ebac79
Alexey Makarevich <alexey.makarevich@intel.com>
2024-04-17 19:38:12 -0700

    check_gemm_binary_per_oc_compatible_formats got rid of int overflow

--- src/cpu/matmul/gemm_based_common.hpp
+++ src/cpu/matmul/gemm_based_common.hpp
@@ -119,7 +119,10 @@ inline bool check_gemm_binary_per_oc_compatible_formats(const matmul_pd_t &pd) {
         ok = ok && strides[i] == strides[i + 1] * dims[i + 1];
 
     // only allowed for nchw and nhwc (b0xb1xMxN or b0xMxNxb1 for matmul)
-    return ok && (strides[ndims - 1] == 1 || strides[1] == 1);
+    for (int i = 1; i < ndims - 1; ++i) {
+        if (dims[i] == 0) { ok = false; }
+    }
+    return ok && strides[0];
 }
 
 inline size_t get_scratchpad_block_elements(const dim_t batch, dim_t M,

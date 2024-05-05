34da89da86be621115504f96d1e18190a33f6ff6
xiang1guo <xiang1.guo@intel.com>
2024-05-05 04:50:28 +0000

    tests: gtest: graph: unit: fix data filling

--- tests/gtests/graph/unit/backend/dnnl/test_large_partition.cpp
+++ tests/gtests/graph/unit/backend/dnnl/test_large_partition.cpp
@@ -1228,11 +1228,17 @@ TEST(test_large_partition_execute, Bf16DistilBertMha) {
     graph::compiled_partition_t cp(p);
     ASSERT_EQ(p.compile(&cp, inputs, outputs, eng), graph::status::success);
 
+    using ltw = graph::logical_tensor_wrapper_t;
+
     std::vector<test_tensor> inputs_ts, outputs_ts;
 
     for (auto &lt : inputs) {
         inputs_ts.emplace_back(*lt, eng);
-        inputs_ts.back().fill<float>();
+        // For select op's bool input
+        if (ltw(lt).data_type() == graph::data_type::boolean)
+            inputs_ts.back().fill<uint8_t>();
+        else
+            inputs_ts.back().fill<float>();
     }
 
     for (auto &lt : outputs) {

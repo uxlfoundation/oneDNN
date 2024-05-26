511655423bc5317e7984587e6b9d973fc1e1f97f
Zhang, Rong A <rong.a.zhang@intel.com>
2024-05-26 09:43:07 -0400

    graph: dnnl: add pattern for gnorm

--- src/graph/backend/dnnl/patterns/fusions.hpp
+++ src/graph/backend/dnnl/patterns/fusions.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2022-2025 Intel Corporation
+* Copyright 2022-2024 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -47,7 +47,6 @@ DNNL_BACKEND_REGISTER_PATTERN_DECLARE(layernorm_fusion)
 DNNL_BACKEND_REGISTER_PATTERN_DECLARE(sum_fusion)
 DNNL_BACKEND_REGISTER_PATTERN_DECLARE(concat_fusion)
 DNNL_BACKEND_REGISTER_PATTERN_DECLARE(groupnorm_fusion)
-DNNL_BACKEND_REGISTER_PATTERN_DECLARE(mlp)
 
 #undef DNNL_BACKEND_REGISTER_PATTERN_DECLARE
 

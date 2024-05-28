1da7b52aac66ac1dbf678323d31de29f6e0ff447
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-05-28 16:20:31 -0700

    benchdnn: brgemm: introduce ukernel API support

--- tests/benchdnn/brgemm/brgemm.hpp
+++ tests/benchdnn/brgemm/brgemm.hpp
@@ -28,8 +28,7 @@
 
 #if defined(DNNL_X64) && DNNL_X64 == 1
 #include "src/cpu/x64/brgemm/brgemm.hpp"
-#elif defined(DNNL_AARCH64) && DNNL_AARCH64 == 1 \
-        && (DNNL_CPU_RUNTIME != DNNL_RUNTIME_NONE)
+#elif defined(DNNL_AARCH64) && DNNL_AARCH64 == 1
 #include "src/cpu/aarch64/brgemm/brgemm.hpp"
 #endif
 

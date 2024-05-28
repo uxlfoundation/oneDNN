e1887d70a64419f6b2710c89d268aeedbdb76b0d
Annop Wongwathanarat <annop.wongwathanarat@arm.com>
2024-05-28 13:49:58 +0000

    cpu: aarch64: Move allocation of accumulator from ACL to oneDNN scratchpad

--- src/cpu/aarch64/acl_convolution_utils.hpp
+++ src/cpu/aarch64/acl_convolution_utils.hpp
@@ -101,27 +101,29 @@ status_t init_scratchpad(op_t &conv, memory_tracking::registrar_t &scratchpad,
 template <typename conv_obj_t, typename conv_pd_t, typename src_data_t,
         typename wei_data_t = src_data_t, typename dst_data_t = src_data_t,
         typename bia_data_t = src_data_t>
-status_t execute_forward_conv_acl(const exec_ctx_t &ctx,
-        conv_obj_t *acl_conv_obj, const conv_pd_t *pd,
-        const std::map<int, conv_key_t> &conv_keys) {
+status_t execute_forward_conv_acl(
+        const exec_ctx_t &ctx, conv_obj_t &acl_conv_obj, const conv_pd_t *pd) {
+    bool with_bias = pd->acp_.with_bias;
+    bool use_dst_acc_for_sum = pd->acp_.use_dst_acc_for_sum;
 
     auto src_base = CTX_IN_MEM(const src_data_t *, DNNL_ARG_SRC);
     auto wei_base = CTX_IN_MEM(const wei_data_t *, DNNL_ARG_WEIGHTS);
 
     // import_memory() and free() methods do not allocate/free any additional
     // memory, only acquire/release pointers.
-    arm_compute::Tensor src_tensor;
-    arm_compute::Tensor wei_tensor;
-    arm_compute::Tensor bia_tensor = nullptr;
-    arm_compute::Tensor dst_tensor;
+    acl_conv_obj.src_tensor.allocator()->import_memory(
+            const_cast<src_data_t *>(src_base));
+    acl_conv_obj.wei_tensor.allocator()->import_memory(
+            const_cast<wei_data_t *>(wei_base));
 
-    auto const acp = pd->acp_;
-    src_tensor.allocator()->init(acp.src_tensor_info);
-    wei_tensor.allocator()->init(acp.wei_tensor_info);
-    dst_tensor.allocator()->init(acp.dst_tensor_info);
+    const auto scratchpad = ctx.get_scratchpad_grantor();
 
-    src_tensor.allocator()->import_memory(const_cast<src_data_t *>(src_base));
-    wei_tensor.allocator()->import_memory(const_cast<wei_data_t *>(wei_base));
+    // If we have an unfused sum post op, put the result in a scratchpad tensor.
+    // Result will be summed to the dst during acl_post_ops.execute
+    auto dst_base = use_dst_acc_for_sum
+            ? scratchpad.get<void>(memory_tracking::names::key_none)
+            : CTX_OUT_MEM(dst_data_t *, DNNL_ARG_DST);
+    acl_conv_obj.dst_tensor.allocator()->import_memory(dst_base);
 
     const auto scratchpad = ctx.get_scratchpad_grantor();
 
--- src/cpu/aarch64/acl_gemm_convolution.hpp
+++ src/cpu/aarch64/acl_gemm_convolution.hpp
@@ -43,7 +43,36 @@ struct acl_gemm_convolution_fwd_t : public primitive_t {
         DECLARE_COMMON_PD_T(
                 "gemm:acl", acl_gemm_convolution_fwd_t, USE_GLOBAL_SCRATCHPAD);
 
-        status_t init(engine_t *engine);
+        status_t init(engine_t *engine) {
+            using namespace data_type;
+            using smask_t = primitive_attr_t::skip_mask_t;
+
+            bool ok = is_fwd()
+                    && set_default_alg_kind(alg_kind::convolution_direct)
+                    && expect_data_types(
+                            src_type, wei_type, bia_type, dst_type, undef)
+                    && !has_zero_dim_memory()
+                    && attr()->has_default_values(
+                            smask_t::post_ops | smask_t::fpmath_mode, dst_type)
+                    && output_scales_mask_ok() && zero_points_ok();
+            if (!ok) return status::unimplemented;
+
+            CHECK(acl_convolution_utils::init_conf_gemm(acp_, src_md_,
+                    weights_md_, dst_md_, bias_md_, *desc(), *attr()));
+
+            CHECK(post_ops.init(
+                    engine, attr_.post_ops_, dst_md_, acp_.act_info));
+            acp_.use_dst_acc_for_sum = post_ops.has_sum();
+
+            if (acp_.use_dst_acc_for_sum) {
+                const memory_desc_wrapper dst_d(&dst_md_);
+                auto scratchpad = scratchpad_registry().registrar();
+                scratchpad.book(memory_tracking::names::key_none,
+                        dst_d.nelems(), dst_d.data_type_size());
+            }
+
+            return status::success;
+        }
 
         acl_conv_conf_t acp_;
         acl_post_ops_t post_ops;

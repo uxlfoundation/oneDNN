e5cee6083ca40c15d3e5c9ffcaedc57c8e0e5aba
Svetlozar Georgiev <svetlozar.georgiev@codeplay.com>
2024-05-30 11:35:54 +0100

    gpu: sycl: pooling: Reduce argument size

--- src/gpu/generic/sycl/ref_pooling.cpp
+++ src/gpu/generic/sycl/ref_pooling.cpp
@@ -66,6 +66,9 @@ status_t ref_pooling_fwd_t::pd_t::init_conf() {
 
     const auto *att = attr();
     const auto &attr_po = att->post_ops_;
+    if (attr_po.len() > sycl_post_ops_t::max_post_ops) {
+        return dnnl_unimplemented;
+    }
     conf_.po_len = attr_po.len();
     for (auto i = 0; i < attr_po.len(); ++i) {
         if (attr_po.contain(binary, i)) {

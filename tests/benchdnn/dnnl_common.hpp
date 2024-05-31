bb9638dce5298c14bed43d0c304df7c4480dbd0f
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-05-31 16:43:50 -0700

    benchdnn: styling

--- tests/benchdnn/dnnl_common.hpp
+++ tests/benchdnn/dnnl_common.hpp
@@ -910,8 +910,8 @@ void init_memory_args(dnn_mem_map_t &mem_map, const prb_t *prb,
             const int exec_sc_arg = DNNL_ARG_ATTR_SCALES | exec_arg;
             dims_t dims = {};
             int64_t ndims = 1;
-            const auto mask = sc.get_mask(
-                    exec_arg, prim_kind, query_md_ndims(wei_md), has_groups);
+            const auto mask = sc.get_mask(exec_arg, prim_kind,
+                    query_md_ndims(wei_md), has_channel_groups);
 
             if (mask > 0) {
                 const auto &md = query_md(const_pd, exec_arg);

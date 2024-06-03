b542eceb4cab4c470cbe6649de30d35f290927b7
Denis Samoilov <denis.samoylov@intel.com>
2024-06-03 09:26:11 -0700

    cpu: sycl: isolate cpu sycl

--- src/cpu/sycl/stream.cpp
+++ src/cpu/sycl/stream.cpp
@@ -39,13 +39,7 @@ status_t stream_t::init() {
                 : ::sycl::property_list {};
         impl()->set_queue(::sycl::queue(sycl_ctx, sycl_dev, props));
     } else {
-        // TODO: Compare device and context of the engine with those of the
-        // queue after SYCL adds support for device/context comparison.
-        //
-        // For now perform some simple checks.
-        auto sycl_dev = queue().get_device();
-        const bool args_ok = engine()->kind() == engine_kind::cpu
-                && (sycl_dev.is_cpu() || xpu::sycl::is_host(sycl_dev));
+        const bool args_ok = engine()->kind() == engine_kind::cpu;
         if (!args_ok) return status::invalid_arguments;
     }
 
@@ -53,7 +47,7 @@ status_t stream_t::init() {
 }
 
 void stream_t::after_exec_hook() {
-    sycl_ctx().set_deps(xpu::sycl::event_t());
+    sycl_ctx().set_deps(impl::sycl::sycl_event_t());
 }
 
 } // namespace sycl

8fd1a4704db2b4f642893917a85ecd3e4b72a2ca
deepeshfujitsu <deepesh.agrawal@fujitsu.com>
2024-06-07 13:33:44 +0530

    Enable bf16 datatype support for AArch64 jit_uni_softmax and relax BenchDNN threshold for softmax on AArch64

--- src/cpu/aarch64/jit_uni_softmax.hpp
+++ src/cpu/aarch64/jit_uni_softmax.hpp
@@ -75,8 +75,10 @@ struct jit_uni_softmax_fwd_t : public primitive_t {
             const auto src_dt = src_md()->data_type;
             const auto dst_dt = dst_md()->data_type;
             bool ok = mayiuse(isa) && is_fwd() && !has_zero_dim_memory()
-                    && utils::one_of(src_dt, f32, s8, u8)
-                    && utils::one_of(dst_dt, f32, s8, u8)
+                    && utils::one_of(src_dt, f32, bf16, s8, u8)
+                    && utils::one_of(dst_dt, f32, bf16, s8, u8)
+                    && ((src_dt == bf16 || dst_dt == bf16) ? mayiuse_bf16()
+                                                           : true)
                     && (mayiuse(sve_512) || mayiuse(sve_256)
                             || mayiuse(sve_128))
                     && attr()->has_default_values(skip_mask_t::scales_runtime)
@@ -160,10 +162,11 @@ struct jit_uni_softmax_bwd_t : public primitive_t {
                     && utils::one_of(dst_md()->data_type, f32, bf16)
                     && utils::one_of(diff_dst_md()->data_type, f32, bf16)
                     && utils::one_of(diff_src_md()->data_type, f32, bf16)
-                    && IMPLICATION(utils::one_of(bf16, dst_md()->data_type,
-                                           diff_dst_md()->data_type,
-                                           diff_src_md()->data_type),
-                            mayiuse_bf16())
+                    && ((dst_md()->data_type == bf16
+                                || diff_dst_md()->data_type == bf16
+                                || diff_src_md()->data_type == bf16)
+                                    ? mayiuse_bf16()
+                                    : true)
                     && (mayiuse(sve_512) || mayiuse(sve_256))
                     && attr()->has_default_values()
                     && set_default_formats() == status::success;

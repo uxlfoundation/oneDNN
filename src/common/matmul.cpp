3a7c04d3ea2f04e0d2f3f16227a2f4bc6f528c1b
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-06-24 11:22:51 -0700

    common, cpu, gpu, benchdnn: add support for src scales mask of 3

--- src/common/matmul.cpp
+++ src/common/matmul.cpp
@@ -78,13 +78,11 @@ status_t matmul_attr_check(const matmul_desc_t &desc, const engine_t *engine,
     int ndims_wei = desc.weights_desc.ndims;
     assert(ndims_src >= 2);
     assert(ndims_wei >= 2);
+    int src_qmask_M = 1 << (ndims_src - 2);
     int src_qmask_K = 1 << (ndims_src - 1);
-    int wei_qmask_N = 1 << (ndims_wei - 1);
+
     int wei_qmask_K = 1 << (ndims_wei - 2);
     int wei_qmask_N = 1 << (ndims_wei - 1);
-
-    int dst_qmask_K = 1 << (ndims_dst - 2);
-    int dst_qmask_N = 1 << (ndims_dst - 1);
 
     // Check scales
     if (!attr->scales_.has_default_values()) {
@@ -97,7 +95,8 @@ status_t matmul_attr_check(const matmul_desc_t &desc, const engine_t *engine,
         const int mask_dst = sc.get(DNNL_ARG_DST).mask_;
 
         // Check allowed masks.
-        VCHECK_MATMUL_UNIMPL(utils::one_of(mask_src, 0, src_qmask_K)
+        VCHECK_MATMUL_UNIMPL(utils::one_of(mask_src, 0, src_qmask_K,
+                                     src_qmask_M + src_qmask_K)
                         && utils::one_of(mask_wei, 0, wei_qmask_N,
                                 wei_qmask_N + wei_qmask_K)
                         && mask_dst == 0,

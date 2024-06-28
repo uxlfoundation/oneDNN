078ed005e82d2de4129eb5a224892f41d289216a
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2024-06-27 17:17:56 -0700

    gpu: jit: codegen: add alloc/var bind tracing

--- src/gpu/intel/jit/codegen/reg_buf.hpp
+++ src/gpu/intel/jit/codegen/reg_buf.hpp
@@ -21,6 +21,7 @@
 #include <unordered_set>
 
 #include "gpu/intel/jit/codegen/register_allocator.hpp"
+#include "gpu/intel/jit/ir/core.hpp"
 #include "gpu/intel/jit/ir/grf_permutation.hpp"
 #include "gpu/intel/jit/ngen/ngen.hpp"
 #include "gpu/intel/jit/utils/utils.hpp"

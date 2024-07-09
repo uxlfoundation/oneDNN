ae556f688a4eaa1307ecb7ea81271fbe8412f0c0
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-07-08 21:55:14 -0700

    benchdnn: brgemm: add binary post-op support

--- tests/benchdnn/brgemm/brgemm.cpp
+++ tests/benchdnn/brgemm/brgemm.cpp
@@ -336,6 +336,7 @@ int init_kernel(kernel_args_t &kernel_args) {
     if (res->state == SKIPPED) return OK;
 
     attr_args_t attr_args;
+    attr_args.prepare_post_ops_mds(prb->attr, prb->ndims, prb->dst_dims.data());
     const auto &wei_scale = prb->attr.scales.get(DNNL_ARG_WEIGHTS);
     if (wei_scale.policy == policy_t::PER_OC) {
         attr_args.prepare_scales(prb->attr, DNNL_ARG_WEIGHTS, 2);
@@ -381,6 +382,7 @@ int init_kernel(kernel_args_t &kernel_args) {
     kernel_args.scratchpad_size_ = brgemm_desc.get_wsp_buffer_size();
 #else // !defined(DNNL_EXPERIMENTAL_UKERNEL)
     attr_args_t attr_args;
+    attr_args.prepare_post_ops_mds(prb->attr, prb->ndims, prb->dst_dims.data());
     auto dnnl_attr = make_benchdnn_dnnl_wrapper(
             create_dnnl_attr(prb->attr, attr_args));
 
@@ -481,14 +483,6 @@ void skip_invalid_prb(const prb_t *prb, res_t *res) {
         return;
     }
 
-    if (prb->attr.post_ops.binary_index() >= 0) {
-        // TODO: binary post-op is not supported yet.
-        BENCHDNN_PRINT(2, "%s\n",
-                "Binary post-op is not supported in the driver yet.");
-        res->state = SKIPPED;
-        res->reason = skip_reason::case_not_supported;
-        return;
-    }
 #else
     if (!prb->attr.is_def()) {
         bool non_def_scales = !prb->attr.scales.is_def();
@@ -506,10 +500,8 @@ void skip_invalid_prb(const prb_t *prb, res_t *res) {
         if (non_def_po) {
             const auto &po = prb->attr.post_ops;
             bool has_sum = po.find(attr_t::post_ops_t::kind_t::SUM) != -1;
-            bool has_binary = po.binary_index() != -1;
-            if (has_sum || has_binary) {
-                BENCHDNN_PRINT(
-                        2, "%s\n", "Sum or binary post-ops are not supported");
+            if (has_sum) {
+                BENCHDNN_PRINT(2, "%s\n", "Sum post-op is not supported");
                 res->state = SKIPPED;
                 res->reason = skip_reason::case_not_supported;
                 return;
@@ -711,6 +703,41 @@ void init_memory_args(
         // condition.
         mem_map.emplace(
                 DNNL_ARG_SCRATCHPAD, dnn_mem_t(scratchpad_md, test_engine));
+    }
+
+    // Binary post-op.
+    const auto &po = prb->attr.post_ops;
+    for (int idx = 0; idx < po.len(); ++idx) {
+        const auto &e = po.entry[idx];
+        if (!e.is_binary_kind()) continue;
+
+        int po_arg = DNNL_ARG_ATTR_MULTIPLE_POST_OP(idx) | DNNL_ARG_SRC_1;
+        const auto &b = e.binary;
+        int ndims = 2;
+        dims_t dims = prb->dst_dims;
+
+        using mask_input_t
+                = attr_t::post_ops_t::entry_t::binary_t::mask_input_t;
+        int mask = -1;
+        if (b.mask_input == mask_input_t::mask) {
+            mask = b.mask;
+        } else if (b.mask_input == mask_input_t::policy) {
+            mask = attr_t::policy2mask(po_arg, b.policy, dnnl_matmul, 2);
+        } else {
+            mask = attr_t::get_default_mask(b.policy);
+        }
+
+        switch (mask) {
+            case 0: dims = {1, 1}; break;
+            case 1: dims = {dims[0], 1}; break;
+            case 2: dims = {1, dims[1]}; break;
+            // Masks can be bigger than values above depending on the policy.
+            default: break;
+        }
+
+        auto po_md
+                = dnn_mem_t::init_md(ndims, dims.data(), b.src1_dt, tag::abx);
+        mem_map.emplace(po_arg, dnn_mem_t(po_md, test_engine));
     }
 
     if (!prb->attr.scales.is_def()) {
@@ -930,6 +957,27 @@ int scales_post_processing(dnn_mem_map_t &mem_map) {
     return OK;
 }
 
+int binary_post_op_preprocessing(
+        std::vector<void *> &binary_po_v, const dnn_mem_map_t &mem_map) {
+    binary_po_v.reserve(mem_map.size());
+
+    for (const auto &map_entry : mem_map) {
+        const int exec_arg = map_entry.first;
+
+        const int post_ops_range = DNNL_ARG_ATTR_MULTIPLE_POST_OP(31)
+                - DNNL_ARG_ATTR_MULTIPLE_POST_OP(0);
+        const bool is_post_ops_arg = (exec_arg & post_ops_range);
+        if (!is_post_ops_arg) continue;
+
+        const auto &mem = map_entry.second;
+        void *handle;
+        DNN_SAFE(dnnl_memory_get_data_handle(mem.m_, &handle), WARN);
+        binary_po_v.push_back(handle);
+    }
+
+    return OK;
+}
+
 int init_hw_config(const kernel_args_t &kernel_args) {
 #if !defined(DNNL_EXPERIMENTAL_UKERNEL)
 #if defined(brg_x64)
@@ -995,6 +1043,9 @@ int doit(const prb_t *prb, res_t *res) {
     if (prb->use_dst_as_acc()) acc_ptr = dst_ptr;
 
     SAFE(scales_post_processing(mem_map), WARN);
+
+    std::vector<void *> binary_po_v;
+    SAFE(binary_post_op_preprocessing(binary_po_v, mem_map), WARN);
 
 #if !defined(DNNL_EXPERIMENTAL_UKERNEL)
     std::vector<namespace_impl::brgemm_batch_element_t> v_batch_element(
@@ -1118,9 +1169,10 @@ int doit(const prb_t *prb, res_t *res) {
                          offsets.data(), dst_ptr, scratchpad_ptr),
                 WARN);
     } else {
+        assert(binary_po_v.size() <= 1);
         DNN_SAFE(dnnl_brgemm_execute_postops(brgemm, src_ptr, wei_packed_ptr,
                          offsets.data(), acc_ptr, dst_ptr, scratchpad_ptr,
-                         nullptr),
+                         binary_po_v.empty() ? nullptr : binary_po_v[0]),
                 WARN);
     }
 #endif

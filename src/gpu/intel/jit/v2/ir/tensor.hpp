a4587fc155bd98c6b4e182496d670ae31f16e53a
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2024-07-23 11:09:27 -0700

    gpu: jit: remove unused functionality

--- src/gpu/intel/jit/v2/ir/tensor.hpp
+++ src/gpu/intel/jit/v2/ir/tensor.hpp
@@ -229,16 +229,6 @@ public:
         entries_ = to_entries(tag);
     }
 
-    void stringify(std::ostream &out) const {
-        jit::stringify(out, to_tag(collapse_x().entries()));
-    }
-
-    void parse(std::istream &in) {
-        auto tag = jit::parse<std::string>(in);
-        is_any_ = (tag == "any");
-        entries_ = to_entries(tag);
-    }
-
 private:
     void init_entries(const std::string &s);
     bool has_x() const;
@@ -278,25 +268,6 @@ public:
     bool matches(const layout_tag_t &other, const prb_tile_t &sizes) const;
     std::string str() const;
     IR_DEFINE_DUMP()
-
-#if __cplusplus >= 202002L
-    bool operator==(const layout_tag_t &other) const = default;
-#endif
-
-    void stringify(std::ostream &out) const {
-        jit::stringify(out, raw_tag_);
-        out << ":";
-        jit::stringify(out, type_);
-    }
-
-    void parse(std::istream &in) {
-        desc_ = layout_desc_t();
-        auto s = stream_parse<std::string>(in);
-        auto parts = gpu_utils::split(s, ":");
-        ir_assert(parts.size() == 2);
-        jit::parse(parts[0], raw_tag_);
-        jit::parse(parts[1], type_);
-    }
 
     void stringify(std::ostream &out) const {
         jit::stringify(out, raw_tag_);

e005234e3ad6bf66ae215f60f99ad3e11b20f011
Gu, Yonghao <yonghao.gu@intel.com>
2024-07-29 01:38:16 +0000

    benchdnn: graph: unify the memory pool between test and example

--- tests/benchdnn/graph/memory_pool.hpp
+++ tests/benchdnn/graph/memory_pool.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2024-2025 Intel Corporation
+* Copyright 2024 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -31,6 +31,44 @@
 
 #ifndef UNUSED
 #define UNUSED(x) ((void)(x))
+#endif
+
+struct cpu_deletor {
+    cpu_deletor() = default;
+    void operator()(void *ptr) {
+        if (ptr) free(ptr);
+    }
+};
+
+#ifdef DNNL_WITH_SYCL
+struct sycl_deletor {
+    sycl_deletor() = delete;
+    ::sycl::context ctx_;
+    sycl_deletor(const ::sycl::context &ctx) : ctx_(ctx) {}
+    void operator()(void *ptr) {
+        if (ptr) ::sycl::free(ptr, ctx_);
+    }
+};
+
+void *sycl_malloc_wrapper(
+        size_t size, size_t alignment, const void *dev, const void *ctx) {
+    return malloc_shared(size, *static_cast<const ::sycl::device *>(dev),
+            *static_cast<const ::sycl::context *>(ctx));
+}
+
+void sycl_free_wrapper(
+        void *ptr, const void *device, const void *context, void *event) {
+    // Device is not used in this example, but it may be useful for some users
+    // application.
+    UNUSED(device);
+    // immediate synchronization here is for test purpose. For performance,
+    // users may need to store the ptr and event and handle them separately
+    if (event) {
+        auto sycl_deps_ptr = static_cast<::sycl::event *>(event);
+        sycl_deps_ptr->wait();
+    }
+    free(ptr, *static_cast<const ::sycl::context *>(context));
+}
 #endif
 
 #if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
@@ -77,15 +115,30 @@ static void ocl_free(
 }
 #endif
 
-// This memory pool is for benchdnn graph performance validation. `clear` and
-// `set_capacity` functions aren't thread safe.
-// Note: memory pool for GPU backend currently.
-
-#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL \
-        || DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+// This memory pool is for benchdnn graph performance test. The clear and
+// set_capacity functions aren't thread safe. The multi-threaded scenario is
+// mainly used in Graph Compiler backend.
+// The graph example memory pool is same as this. It's just for simplification.
+// TODO: add some comments to clarify the design and interfaces
 class simple_memory_pool_t {
 public:
-    bool check_allocated_mem(void *&ptr, size_t size) {
+#if defined(DNNL_WITH_SYCL) || DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+#ifdef DNNL_WITH_SYCL
+    void *allocate(
+            size_t size, size_t alignment, const void *dev, const void *ctx)
+#endif
+
+#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+            void *allocate(size_t size, size_t alignment, cl_device_id dev,
+                    cl_context ctx)
+#endif
+    {
+        std::lock_guard<std::mutex> pool_guard(pool_lock);
+        // fake malloc for 0 size
+        if (size == 0) return nullptr;
+
+        void *ptr {nullptr};
+        bool need_alloc_new_mm = true;
         // find alloc mm with same size
         const auto cnt = map_size_ptr_.count(size);
         if (cnt > 0) {
@@ -95,37 +148,21 @@ public:
                 if (is_free_ptr_[it->second.get()]) {
                     ptr = it->second.get();
                     is_free_ptr_[ptr] = false;
-                    return false;
+                    need_alloc_new_mm = false;
                 }
             }
         }
-        return true;
-    }
-
-#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
-    void *allocate(
-            size_t size, size_t alignment, const void *dev, const void *ctx)
-#elif DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
-    void *allocate(
-            size_t size, size_t alignment, cl_device_id dev, cl_context ctx)
-#endif
-    {
-        std::lock_guard<std::mutex> pool_guard(pool_lock);
-        // fake malloc for 0 size
-        if (size == 0) return nullptr;
-
-        void *ptr {nullptr};
-        bool need_alloc_new_mm = check_allocated_mem(ptr, size);
         if (need_alloc_new_mm) {
-#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
+#ifdef DNNL_WITH_SYCL
             auto sh_ptr = std::shared_ptr<void> {
-                    malloc_shared(size, *static_cast<const sycl::device *>(dev),
-                            *static_cast<const sycl::context *>(ctx)),
+                    sycl_malloc_wrapper(size, alignment, dev, ctx),
                     sycl_deletor {*static_cast<const sycl::context *>(ctx)}};
-#elif DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+#endif
+
+#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
             auto sh_ptr = std::shared_ptr<void> {
                     ocl_malloc_device(size, alignment, dev, ctx),
-                    ocl_deletor {dev, ctx}};
+                    [ctx, dev](void *p) { ocl_free(p, dev, ctx, {}); }};
 #endif
             ptr = sh_ptr.get();
             // record the map of mm size and its ptr for reuse
@@ -134,22 +171,65 @@ public:
         }
         return ptr;
     }
+#endif
 
-#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
-    void deallocate(void *ptr) {
+    void *allocate_host(size_t size, size_t alignment) {
         std::lock_guard<std::mutex> pool_guard(pool_lock);
-        is_free_ptr_[ptr] = true;
-        return;
+        if (size == 0) return nullptr;
+
+        void *ptr {nullptr};
+        bool need_alloc_new_mm = true;
+        // find alloc mm with same size
+        const auto cnt = map_size_ptr_.count(size);
+        if (cnt > 0) {
+            const auto Iter = map_size_ptr_.equal_range(size);
+            for (auto it = Iter.first; it != Iter.second; ++it) {
+                // check if same size mm is free
+                if (is_free_ptr_[it->second.get()]) {
+                    ptr = it->second.get();
+                    is_free_ptr_[ptr] = false;
+                    need_alloc_new_mm = false;
+                }
+            }
+        }
+        if (need_alloc_new_mm) {
+            auto sh_ptr = std::shared_ptr<void> {malloc(size), cpu_deletor {}};
+            ptr = sh_ptr.get();
+            // record the map of mm size and its ptr for reuse
+            map_size_ptr_.emplace(std::make_pair(size, sh_ptr));
+            is_free_ptr_[ptr] = false;
+        }
+        return ptr;
     }
-#elif DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
-    void deallocate(void *ptr) {
+
+#ifdef DNNL_WITH_SYCL
+    void deallocate(
+            void *ptr, const void *device, const void *context, void *event) {
         std::lock_guard<std::mutex> pool_guard(pool_lock);
+        if (event) {
+            auto sycl_deps_ptr = static_cast<::sycl::event *>(event);
+            sycl_deps_ptr->wait();
+        }
         is_free_ptr_[ptr] = true;
         return;
     }
 #endif
-
+#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
+    void deallocate(
+            void *ptr, cl_device_id dev, const cl_context ctx, cl_event event) {
+        std::lock_guard<std::mutex> pool_guard(pool_lock);
+        if (event) { OCL_CHECK(clWaitForEvents(1, &event)); }
+        is_free_ptr_[ptr] = true;
+        return;
+    }
+#endif
+    void deallocate_host(void *ptr) {
+        std::lock_guard<std::mutex> pool_guard(pool_lock);
+        is_free_ptr_[ptr] = true;
+        return;
+    }
     void clear() {
+        dnnl::graph::set_compiled_partition_cache_capacity(0);
         map_size_ptr_.clear();
         is_free_ptr_.clear();
     }
@@ -158,34 +238,6 @@ private:
     std::mutex pool_lock;
     std::unordered_multimap<size_t, std::shared_ptr<void>> map_size_ptr_;
     std::unordered_map<void *, bool> is_free_ptr_;
-
-#if DNNL_GPU_RUNTIME == DNNL_RUNTIME_SYCL
-    struct sycl_deletor {
-        sycl_deletor() = delete;
-        sycl_deletor(const ::sycl::context &ctx) : ctx_(ctx) {}
-        void operator()(void *ptr) {
-            if (ptr) ::sycl::free(ptr, ctx_);
-        }
-
-    private:
-        ::sycl::context ctx_;
-    };
-#elif DNNL_GPU_RUNTIME == DNNL_RUNTIME_OCL
-    struct ocl_deletor {
-        ocl_deletor() = delete;
-        ocl_deletor(const cl_device_id dev, const cl_context ctx)
-            : dev_(dev), ctx_(ctx) {}
-        void operator()(void *ptr) {
-            if (ptr) ocl_free(ptr, dev_, ctx_, {});
-        }
-
-    private:
-        cl_device_id dev_;
-        cl_context ctx_;
-    };
-#endif
 };
-
-#endif
 
 #endif

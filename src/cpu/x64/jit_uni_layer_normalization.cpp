4e2cb538275fe324f66eac2fdf489f3cb85492ab
Pirogov, Vadim <vadim.o.pirogov@intel.com>
2024-08-06 09:26:41 -0700

    x64, api, benchdnn: revert dice post op and related changes
    Reverts the following commits:
    * 8bfcff59efbf5c9846d7f75a2a847e35676d582c verbose_converter: add prelu with scaleshift support
    * d3ffb808d5e20068630c4052dbd0b37b7721b59c x64: add jitted prelu with scaleshift postop
    * f2c62e01799eb62d68c2c0c5a348fe16d3dc6e6d x64: properly initialize postop injector constant tables
    * 6ae2d9cc8c485fe4c8db720752324c871c7a302f benchdnn: add prelu postop with scaleshift coverage
    * c11bab0c4970886c84eee1c2e7e98266d05dfa35 api: add dice post op

--- src/cpu/x64/jit_uni_layer_normalization.cpp
+++ src/cpu/x64/jit_uni_layer_normalization.cpp
@@ -634,7 +634,7 @@ protected:
 
         postamble();
 
-        if (postops_injector_)
+        if (with_eltwise_ && postops_injector_)
             postops_injector_->prepare_table(/* generate = */ true);
     }
 };

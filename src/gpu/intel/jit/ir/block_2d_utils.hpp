8cc34cb0c1656e34593bc99abfb7ca1c1e920cb4
Daniel Youssif <daniel.youssif@intel.com>
2024-08-19 09:22:48 -0700

    gpu: jit: conv: Xe3 enabling

--- src/gpu/intel/jit/ir/block_2d_utils.hpp
+++ src/gpu/intel/jit/ir/block_2d_utils.hpp
@@ -33,13 +33,7 @@ inline int block_2d_base_alignment(const hw_t &hw) {
             // XXX: A steppings require 128 byte alignment due to a HW bug.
             return (hw.stepping_id() <= 6) ? 128 : 64;
         case ngen::HW::Xe2:
-#if XE3
-        case ngen::HW::Xe3:
-#endif
-            return 64;
-#if XE3P
-        case ngen::HW::Xe3p: return 4;
-#endif
+        case ngen::HW::Xe3: return 64;
         default: ir_error_not_expected();
     }
     return 0;
@@ -65,14 +59,8 @@ inline bool block_2d_height_ok(int height) {
 inline int block_2d_pitch_alignment(const hw_t &hw) {
     switch (hw.to_ngen()) {
         case ngen::HW::XeHPC: return 8;
-        case ngen::HW::Xe2:
-#if XE3
-        case ngen::HW::Xe3:
-#endif
-            return 16;
-#if XE3P
-        case ngen::HW::Xe3p: return 4;
-#endif
+        case ngen::HW::Xe2: return 16;
+        case ngen::HW::Xe3: return 16;
         default: ir_error_not_expected();
     }
     return 0;

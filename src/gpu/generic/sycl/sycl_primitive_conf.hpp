b34bff8828cbb3f47c6323233b908cab132d873f
Tadej Ciglarič <tadej.ciglaric@codeplay.com>
2024-08-29 15:29:29 +0200

    generic: sycl: refactor how post op arguments are loaded

--- src/gpu/generic/sycl/sycl_primitive_conf.hpp
+++ src/gpu/generic/sycl/sycl_primitive_conf.hpp
@@ -44,8 +44,6 @@ struct sycl_binary_conf_t {
 
     int wk_size;
 
-    xpu::sycl::md_t binary_src_arr[sycl_post_ops_t::max_post_ops];
-
     sycl_post_ops_t post_ops;
 };
 
@@ -101,7 +99,6 @@ struct sycl_eltwise_conf_t {
     dim_t w;
     dim_t wk_size;
     dim_t post_po_len;
-    xpu::sycl::md_t binary_src_arr[sycl::sycl_post_ops_t::max_post_ops];
     sycl_post_ops_t post_ops;
 };
 
@@ -216,7 +213,6 @@ struct sycl_resampling_conf_t {
     size_t work_amount;
 
     xpu::sycl::md_t src_md;
-    xpu::sycl::md_t src1_md[sycl_post_ops_t::max_post_ops];
     xpu::sycl::md_t dst_md;
     xpu::sycl::md_t diff_src_md;
     xpu::sycl::md_t diff_dst_md;
@@ -319,7 +315,6 @@ struct sycl_batch_normalization_conf_t {
 struct sycl_softmax_conf_t {
     prop_kind_t prop_kind;
     xpu::sycl::md_t src_md;
-    xpu::sycl::md_t src1_md[sycl_post_ops_t::max_post_ops];
     xpu::sycl::md_t dst_md;
 
     xpu::sycl::md_t diff_md;
@@ -396,7 +391,6 @@ struct sycl_pooling_base_conf_t {
 
 struct sycl_pooling_fwd_conf_t : public sycl_pooling_base_conf_t {
     xpu::sycl::md_t src_md;
-    xpu::sycl::md_t src1_md[sycl_post_ops_t::max_post_ops];
     xpu::sycl::md_t dst_md;
     sycl_post_ops_t post_ops;
 };

510032c2a0b89b85072fc5d351d9f58e3f53e491
ShanoToni <anton_b_mitkov@abv.bg>
2024-09-09 12:27:04 +0100

    gpu: nvidia: Add support for cublaslt matmul

--- src/gpu/nvidia/cudnn_matmul.cpp
+++ src/gpu/nvidia/cudnn_matmul.cpp
@@ -41,17 +41,31 @@ status_t cudnn_matmul_t::execute(const exec_ctx_t &ctx) const {
     const auto dst_d = ctx.memory_mdw(DNNL_ARG_DST, pd()->dst_md());
     const auto bias_d = ctx.memory_mdw(DNNL_ARG_BIAS, pd()->weights_md(1));
 
+    status_t status;
+    size_t bias_scratchpad_size
+            = 0; // To avoid extra allocation in an executor.
+
+    bool has_runtime_args = matmul_impl_->has_runtime_params();
+    if (has_runtime_args) {
+        // Initialise all runtime parameters
+        status = matmul_impl_->init_parameters(src_d, weights_d, dst_d, bias_d);
+        if (status != status::success) return status;
+
+        bias_scratchpad_size = matmul_impl_->bias_scratch_size();
+    }
+
     nvidia::stream_t *cuda_stream
             = utils::downcast<nvidia::stream_t *>(ctx.stream());
 
-    status_t status = executor_->execute(ctx, ctx.stream()->engine(),
-            matmul_impl_, pd()->params_, src_d, weights_d, dst_d, bias_d);
+    status = executor_->execute(
+            ctx, ctx.stream()->engine(), matmul_impl_, bias_scratchpad_size);
 
     if (pd()->params_->has_runtime_params_) {
         auto &evts = cuda_stream->sycl_ctx().get_sycl_deps().events;
         for (auto e : evts) {
             e.wait();
         }
+        matmul_impl_->cleanup();
     }
     return status;
 }
@@ -62,6 +76,32 @@ status_t cudnn_matmul_lt_t::execute(const exec_ctx_t &ctx) const {
     const auto src_d = ctx.memory_mdw(DNNL_ARG_SRC, pd()->src_md());
     const auto weights_d = ctx.memory_mdw(DNNL_ARG_WEIGHTS, pd()->weights_md());
     const auto dst_d = ctx.memory_mdw(DNNL_ARG_DST, pd()->dst_md());
+    const auto bias_d = ctx.memory_mdw(DNNL_ARG_BIAS, pd()->weights_md(1));
+
+    // To avoid extra allocation in an executor.
+    size_t algo_scratchpad_size = 0;
+    size_t bias_scratchpad_size = 0;
+    size_t block_a_scratchpad_size = 0;
+    size_t block_b_scratchpad_size = 0;
+    size_t block_c_scratchpad_size = 0;
+    size_t src_scale_scratchpad_size = 0;
+    size_t wei_scale_scratchpad_size = 0;
+
+    bool has_runtime_args = matmul_impl_->has_runtime_params();
+    if (has_runtime_args) {
+        // Initialise all runtime parameters
+        auto engine = ctx.stream()->engine();
+        CHECK(matmul_impl_->init_parameters(
+                src_d, weights_d, dst_d, bias_d, engine));
+
+        algo_scratchpad_size = matmul_impl_->algo_scratch_size();
+        bias_scratchpad_size = matmul_impl_->bias_scratch_size();
+        block_a_scratchpad_size = matmul_impl_->block_a_scratch_size();
+        block_b_scratchpad_size = matmul_impl_->block_b_scratch_size();
+        block_c_scratchpad_size = matmul_impl_->block_c_scratch_size();
+        src_scale_scratchpad_size = matmul_impl_->src_scale_size();
+        wei_scale_scratchpad_size = matmul_impl_->wei_scale_size();
+    }
 
     nvidia::stream_t *cuda_stream
             = utils::downcast<nvidia::stream_t *>(ctx.stream());
@@ -77,8 +117,8 @@ status_t cudnn_matmul_lt_t::execute(const exec_ctx_t &ctx) const {
             != ctx.args().end();
 
     if (has_src_scales
-            && (pd()->params_->multi_src_scale_
-                    || pd()->params_->acc_type_ == CUDA_R_32I)) {
+            && (matmul_impl_->multi_src_scale()
+                    || matmul_impl_->scale_type() == CUDA_R_32I)) {
         // src scale sycl binary
         exec_args_t src_scale_binary_args;
         src_scale_binary_args[DNNL_ARG_SRC_0]
@@ -86,7 +126,7 @@ status_t cudnn_matmul_lt_t::execute(const exec_ctx_t &ctx) const {
         src_scale_binary_args[DNNL_ARG_SRC_1] = memory_arg_t {
                 ctx.args().at(DNNL_ARG_ATTR_SCALES | DNNL_ARG_SRC).mem, true};
 
-        std::unique_ptr<memory_t, memory_deleter_t> scratch_mem;
+        std::unique_ptr<memory_t> scratch_mem;
         auto scratchpad_storage
                 = ctx.get_scratchpad_grantor().get_memory_storage(
                         memory_tracking::names::key_matmul_lt_src_scale);
@@ -101,8 +141,8 @@ status_t cudnn_matmul_lt_t::execute(const exec_ctx_t &ctx) const {
         CHECK(src_scale_binary_->execute(binary_ctx));
     }
     if (has_wei_scales
-            && (pd()->params_->multi_wei_scale_
-                    || pd()->params_->acc_type_ == CUDA_R_32I)) {
+            && (matmul_impl_->multi_wei_scale()
+                    || matmul_impl_->scale_type() == CUDA_R_32I)) {
         // wei scale sycl binary
         exec_args_t wei_scale_binary_args;
         wei_scale_binary_args[DNNL_ARG_SRC_0]
@@ -111,7 +151,7 @@ status_t cudnn_matmul_lt_t::execute(const exec_ctx_t &ctx) const {
                 ctx.args().at(DNNL_ARG_ATTR_SCALES | DNNL_ARG_WEIGHTS).mem,
                 true};
 
-        std::unique_ptr<memory_t, memory_deleter_t> scratch_mem;
+        std::unique_ptr<memory_t> scratch_mem;
         auto scratchpad_storage
                 = ctx.get_scratchpad_grantor().get_memory_storage(
                         memory_tracking::names::key_matmul_lt_wei_scale);
@@ -127,12 +167,14 @@ status_t cudnn_matmul_lt_t::execute(const exec_ctx_t &ctx) const {
     }
 
     CHECK(executor_->execute(ctx, ctx.stream()->engine(), matmul_impl_,
-            pd()->params_, src_d, weights_d, dst_d));
+            algo_scratchpad_size, bias_scratchpad_size, block_a_scratchpad_size,
+            block_b_scratchpad_size, block_c_scratchpad_size,
+            src_scale_scratchpad_size, wei_scale_scratchpad_size));
 
-    if (pd()->params_->with_bias_) {
+    if (matmul_impl_->with_bias()) {
         // bias sycl binary
         exec_args_t binary_args;
-        std::unique_ptr<memory_t, memory_deleter_t> scratch_mem;
+        std::unique_ptr<memory_t> scratch_mem;
         if (dst_d.data_type() == dnnl_s8) {
             auto scratchpad_storage
                     = ctx.get_scratchpad_grantor().get_memory_storage(
@@ -155,19 +197,29 @@ status_t cudnn_matmul_lt_t::execute(const exec_ctx_t &ctx) const {
         CHECK(binary_->execute(binary_ctx));
     }
 
-    nvidia::stream_t *cuda_stream
-            = utils::downcast<nvidia::stream_t *>(ctx.stream());
+    if (has_dst_scales
+            && (matmul_impl_->multi_dst_scale()
+                    || matmul_impl_->scale_type() == CUDA_R_32I)) {
+        // dst scale sycl binary
+        exec_args_t dst_scale_binary_args;
+        dst_scale_binary_args[DNNL_ARG_SRC_0]
+                = memory_arg_t {ctx.args().at(DNNL_ARG_DST).mem, true};
+        dst_scale_binary_args[DNNL_ARG_SRC_1] = memory_arg_t {
+                ctx.args().at(DNNL_ARG_ATTR_SCALES | DNNL_ARG_DST).mem, true};
+        dst_scale_binary_args[DNNL_ARG_DST] = ctx.args().at(DNNL_ARG_DST);
 
         exec_ctx_t binary_ctx(ctx, std::move(dst_scale_binary_args));
 
         CHECK(dst_scale_binary_->execute(binary_ctx));
     }
 
-    if (pd()->params_->has_runtime_params_) {
+    if (has_runtime_args) {
         auto &evts = cuda_stream->sycl_ctx().get_sycl_deps().events;
         for (auto e : evts) {
             e.wait();
         }
+
+        matmul_impl_->rt_cleanup();
     }
 
     return status::success;

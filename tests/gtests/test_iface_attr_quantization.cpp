1856a2ed9e7a6a1c7e274c92f699bb1310325d91
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-09-18 19:38:09 -0700

    src, tests: matmul: allow mask 15 for weight decompression feature

--- tests/gtests/test_iface_attr_quantization.cpp
+++ tests/gtests/test_iface_attr_quantization.cpp
@@ -485,8 +485,12 @@ TEST_F(attr_quantization_test_t, TestMatmul) {
                                     data_type::f32, {3, 1})));
                 }
             } else if (arg == DNNL_ARG_SRC) {
-                CHECK_UNIMPL(matmul::primitive_desc(eng, a_md, b_md, c_md,
-                        gen_attr_with_scales(arg, 1 << 1)));
+                // Somehow GPU doeshave this support.
+                const bool is_cpu = get_test_engine_kind() == engine::kind::cpu;
+                if (is_cpu) {
+                    CHECK_UNIMPL(matmul::primitive_desc(eng, a_md, b_md, c_md,
+                            gen_attr_with_scales(arg, 1 << 1)));
+                }
                 if (a_dt == data_type::u8) {
                     CHECK_OK(matmul::primitive_desc(eng, a_md, b_md, c_md,
                             gen_attr_with_scales(
@@ -505,10 +509,10 @@ TEST_F(attr_quantization_test_t, TestMatmul) {
             } else {
                 CHECK_UNIMPL(matmul::primitive_desc(eng, a_md, b_md, c_md,
                         gen_attr_with_scales(arg, 1 << 1)));
+                // scales: unsupported mask only relevant for DST arg.
+                CHECK_UNIMPL(matmul::primitive_desc(eng, a_md, b_md, c_md,
+                        gen_attr_with_scales(arg, 1 << 2)));
             }
-            //scales: unsupported mask
-            CHECK_UNIMPL(matmul::primitive_desc(
-                    eng, a_md, b_md, c_md, gen_attr_with_scales(arg, 1 << 2)));
         }
     }
 }
@@ -518,8 +522,8 @@ CPU_TEST_F(attr_quantization_test_t, TestMatmulBatch) {
         const data_type b_dt
                 = a_dt == data_type::f32 ? data_type::f32 : data_type::s8;
 
-        memory::desc a_md {{2, 5, 10, 64}, a_dt, tag::abcd};
-        memory::desc b_md {{2, 5, 64, 20}, b_dt, tag::abdc};
+        memory::desc a_md {{2, 5, 10, 3}, a_dt, tag::abcd};
+        memory::desc b_md {{2, 5, 3, 20}, b_dt, tag::abdc};
         memory::desc c_md {{2, 5, 10, 20}, data_type::f32, tag::abcd};
         const auto ndims = a_md.get_ndims();
 
@@ -552,12 +556,12 @@ CPU_TEST_F(attr_quantization_test_t, TestMatmulBatch) {
             const auto per_ocic_mask = (1 << (ndims - 1)) + (1 << (ndims - 2));
             if (arg == DNNL_ARG_WEIGHTS) {
                 CHECK_OK(matmul::primitive_desc(eng, a_md, b_md, c_md,
-                        gen_attr_with_scales(arg, per_tensor_mask,
-                                data_type::f32, {32, 1})));
+                        gen_attr_with_scales(
+                                arg, per_tensor_mask, data_type::f32, {3, 1})));
             } else if (arg == DNNL_ARG_SRC) {
                 CHECK_OK(matmul::primitive_desc(eng, a_md, b_md, c_md,
                         gen_attr_with_scales(
-                                arg, per_ocic_mask, data_type::f32, {1, 32})));
+                                arg, per_ocic_mask, data_type::f32, {1, 3})));
             } else {
                 CHECK_UNIMPL(matmul::primitive_desc(eng, a_md, b_md, c_md,
                         gen_attr_with_scales(arg, per_tensor_mask)));

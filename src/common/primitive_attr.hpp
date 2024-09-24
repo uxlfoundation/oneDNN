acd36bca7b0b32a38cff57b661671cc833b9d21f
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-09-24 16:37:19 -0700

    src, tests: add src zero-points advanced parameters support

--- src/common/primitive_attr.hpp
+++ src/common/primitive_attr.hpp
@@ -323,7 +323,12 @@ struct zero_points_t : public c_compatible {
                 && group_ndims_wei == rhs.group_ndims_wei
                 && IMPLICATION(group_ndims_wei > 0,
                         utils::array_cmp(group_dims_wei, rhs.group_dims_wei,
-                                group_ndims_wei));
+                                group_ndims_wei))
+                && data_type_src == rhs.data_type_src
+                && group_ndims_src == rhs.group_ndims_src
+                && IMPLICATION(group_ndims_src > 0,
+                        utils::array_cmp(group_dims_src, rhs.group_dims_src,
+                                group_ndims_src));
     }
 
     // arg-specific checks
@@ -332,7 +337,8 @@ struct zero_points_t : public c_compatible {
         return is_set(arg) == false && has_default_data_type(arg);
     }
     bool has_default_groups(int arg) const {
-        return IMPLICATION(arg == DNNL_ARG_WEIGHTS, group_ndims_wei == 0);
+        return IMPLICATION(arg == DNNL_ARG_WEIGHTS, group_ndims_wei == 0)
+                && IMPLICATION(arg == DNNL_ARG_SRC, group_ndims_src == 0);
     }
     bool has_default_data_type(int arg) const {
         return get_data_type(arg) == data_type::s32;
@@ -355,16 +361,19 @@ struct zero_points_t : public c_compatible {
 
     data_type_t get_data_type(int arg) const {
         if (arg == DNNL_ARG_WEIGHTS) return data_type_wei;
+        if (arg == DNNL_ARG_SRC) return data_type_src;
         return data_type::s32;
     }
 
     const dim_t *get_groups(int arg) const {
         if (arg == DNNL_ARG_WEIGHTS) return group_dims_wei;
+        if (arg == DNNL_ARG_SRC) return group_dims_src;
         return nullptr;
     }
 
     int get_groups_ndims(int arg) const {
         if (arg == DNNL_ARG_WEIGHTS) return group_ndims_wei;
+        if (arg == DNNL_ARG_SRC) return group_ndims_src;
         return 0;
     }
 
@@ -378,7 +387,27 @@ struct zero_points_t : public c_compatible {
     status_t set(int arg) { return set(arg, 0); }
 
 private:
-    const static rounding_mode_t default_mode = rounding_mode::environment;
+    bool is_set_src = false, is_set_wei = false, is_set_dst = false;
+    int mask_src = 0, mask_wei = 0, mask_dst = 0;
+    data_type_t data_type_wei = data_type::s32;
+    int group_ndims_wei = 0;
+    dims_t group_dims_wei {};
+    // TODO: A temporary solution until a single quant abstraction is
+    // introduced.
+    data_type_t data_type_src = data_type::s32;
+    int group_ndims_src = 0;
+    dims_t group_dims_src {};
+
+    int get_mask(int arg) const {
+        int mask = 0;
+        switch (arg) {
+            case DNNL_ARG_SRC: mask = mask_src; break;
+            case DNNL_ARG_WEIGHTS: mask = mask_wei; break;
+            case DNNL_ARG_DST: mask = mask_dst; break;
+            default: mask = 0;
+        }
+        return mask;
+    }
 
     bool check(int arg, dnnl_rounding_mode_t rm) const {
         // Only gradients and matmul dst for gradient computation

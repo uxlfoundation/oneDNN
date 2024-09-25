d19e9d3439d8ced1ad0faf729e148f5d349f1ba0
Kassen, Andrew <andrew.kassen@intel.com>
2024-09-24 20:21:59 -0700

    common: add f64 property queries

--- src/common/type_helpers.hpp
+++ src/common/type_helpers.hpp
@@ -143,6 +143,7 @@ inline T min_value(data_type_t data_type) {
         CASE(f16);
         CASE(bf16);
         CASE(f32);
+        CASE(f64);
         CASE(s32);
         CASE(s8);
         CASE(u8);
@@ -167,7 +168,6 @@ inline T max_value(data_type_t data_type) {
         CASE(f8_e5m2);
         CASE(f8_e4m3);
         CASE(f16);
-        CASE(f32);
         CASE(bf16);
         CASE(f32);
         CASE(s32);
@@ -175,6 +175,7 @@ inline T max_value(data_type_t data_type) {
         CASE(u8);
         CASE(s4);
         CASE(u4);
+        case f64: return nstl::numeric_limits<T>::max();
         case data_type::undef:
         default: assert(!"unknown data_type");
     }
@@ -241,6 +242,7 @@ inline T lowest_value(data_type_t data_type) {
         CASE(u8);
         CASE(s4);
         CASE(u4);
+        case f64: return nstl::numeric_limits<T>::lowest();
         case data_type::undef:
         default: assert(!"unknown data_type");
     }
@@ -263,6 +265,7 @@ inline T digits(data_type_t data_type) {
         CASE(f16);
         CASE(bf16);
         CASE(f32);
+        CASE(f64);
         CASE(s32);
         CASE(s8);
         CASE(u8);

e30443af504c97644111f2e390d33b8f9c3614a6
Roy Oursler <roy.j.oursler@intel.com>
2024-10-03 07:07:31 -0700

    xe: jit: remove to_cpp<int> for offsets

--- src/gpu/intel/jit/ir/linear_expr.hpp
+++ src/gpu/intel/jit/ir/linear_expr.hpp
@@ -50,7 +50,7 @@ expr_t linear_div(const expr_t &e, int64_t factor);
 
 // Simplifies a modulus of an expression by a constant. The expression must be
 // convertable to linear_t.
-expr_t simplify_linear_mod(const expr_t &e, int factor);
+expr_t simplify_linear_mod(const expr_t &e, int64_t factor);
 
 // Returns the base and the increments of linear expression `expr` when
 // incrementing `idxs[i]` by 1:

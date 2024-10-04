4c1e05db42a72572567753dac10ac66bbc76824a
Roy Oursler <roy.j.oursler@intel.com>
2024-10-04 10:15:18 -0700

    xe: ocl: enable most warnings from -Wconversion on clang and icpx

--- src/gpu/intel/jit/ngen/ngen_core.hpp
+++ src/gpu/intel/jit/ngen/ngen_core.hpp
@@ -19,7 +19,7 @@
 
 #ifdef ENABLE_LLVM_WCONVERSION
 #pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wimplicit-int-conversion"
+#pragma clang diagnostic ignored "-Wconversion"
 #endif
 
 
@@ -3468,6 +3468,10 @@ static inline void encodeAtomicDescriptor(HW hw, SendgMessageDescriptor &desc, S
 #endif
 
 } /* namespace NGEN_NAMESPACE */
+
+#ifdef ENABLE_LLVM_WCONVERSION
+#pragma clang diagnostic pop
+#endif
 
 #ifdef ENABLE_LLVM_WCONVERSION
 #pragma clang diagnostic pop

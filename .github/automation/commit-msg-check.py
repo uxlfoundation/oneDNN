cf29982cfe9222bcfa00e0192370035b9aea98c1
Mourad Gouicem <mourad.gouicem@intel.com>
2024-10-08 14:07:11 +0200

    github: apply commit msg check to all commits

--- .github/automation/commit-msg-check.py
+++ .github/automation/commit-msg-check.py
@@ -20,34 +20,45 @@
 
 import argparse
 import subprocess
-import re
 
-# Ensure the scope ends in a colon and that same level scopes are
-# comma delimited.
-# Current implementation only checks the first level scope as ':' can be used
-# in the commit description (ex: TBB::tbb or bf16:bf16).
+# * Ensuring the scopes end in colon and same level scopes are comma delimited.
 # TODO: Limit scopes to an acceptable list of tags.
 def __scopeCheck(msg: str):
-    status = "Message scope: "
+    firstLine = (msg.partition("\n")[0]).strip()
 
-    if not re.match('^[a-z0-9]+(, [a-z0-9]+)*: ', msg):
-        print(f"{status} FAILED: Commit message must follow the format "
-               "<scope>:[ <scope>:] <short description>")
-        return False
+    if not ":" in firstLine:
+        raise ValueError(
+            f"Please see contribution guidelines. First line must contain a scope ending in a colon. Got: {firstLine}"
+        )
 
-    print(f"{status} OK")
-    return True
+    # The last element of the split is the title, which we don't care about. Remove it.
+    scopesArray = firstLine.split(":")[:-1]
+    print("---")
+    print(f"Scopes: {scopesArray}")
 
-# Ensure  a character limit for the first line.
+    for scopes in scopesArray:
+        print("---")
+        print(f"Same-level scope: {scopes}")
+        numWords = len(scopes.split())
+        numCommas = scopes.count(",")
+        print(f"Number of words in scope: {numWords}")
+        print(f"Number of commas in scope: {numCommas}")
+
+        if numWords != numCommas + 1:
+            raise ValueError(
+                f"Please see contribution guidelines. Same-level scopes must be seperated by a comma. If this is true then words == commas + 1."
+            )
+
+
+# * Ensuring a character limit for the first line.
 def __numCharacterCheck(msg: str):
-    status = "Message length:"
-    if len(msg) <= 72:
-        print(f"{status} OK")
-        return True
-    else:
-        print(f"{status} FAILED: Commit message summary must not "
-               "exceed 72 characters.")
-        return False
+    summary = msg.partition("\n")[0]
+    msgSummaryLen = len(summary)
+    if msgSummaryLen >= 72:
+        raise ValueError(
+            f"Please see contribution guidelines. Message summary must be less than 72. Got: {msgSummaryLen}"
+        )
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -58,24 +69,13 @@ def main():
     head: str = args.head
 
     commit_range = base + ".." + head
-    messages = subprocess.run(["git", "rev-list", "--format=oneline",
-        commit_range], capture_output=True, text=True).stdout
+    messages = subprocess.run(["git", "rev-list", "--ancestry-path", commit_range, "--format=oneline"], capture_output=True, text=True).stdout
 
-    is_ok = True
     for i in messages.splitlines():
-      print(i)
       commit_msg=i.split(' ', 1)[1]
-      result = __numCharacterCheck(commit_msg)
-      is_ok = is_ok and result
-      result = __scopeCheck(commit_msg)
-      is_ok = is_ok and result
-
-    if is_ok:
-        print("All commmit messages are formatted correctly. ")
-    else:
-        print("Some commit message checks failed. Please align commit messages "
-              "with Contributing Guidelines and update the PR.")
-        exit(1)
+      print(f"msg: {commit_msg}")
+      __numCharacterCheck(commit_msg)
+      __scopeCheck(commit_msg)
 
 
 if __name__ == "__main__":

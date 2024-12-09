030ba2e12a1eb94ef8bb460ac903512f36c746b1
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2024-12-09 15:17:18 -0800

    xe: jit: introduce while IR statement

--- src/gpu/intel/jit/ir/core.hpp
+++ src/gpu/intel/jit/ir/core.hpp
@@ -2476,6 +2476,38 @@ private:
         : stmt_impl_t(_type_info()), vec(vec) {}
 };
 
+// While loop statement with a condition.
+// C++ equivalent:
+//    while (cond) {
+//        body;
+//    }
+class while_t : public stmt_impl_t {
+public:
+    IR_DECL_STMT_TYPE_ID(while_t)
+
+    static stmt_t make(const expr_t &cond, const stmt_t &body = {}) {
+        return stmt_t(new while_t(cond, body));
+    }
+
+    bool is_equal(const object_impl_t &obj) const override {
+        if (!obj.is<self_type>()) return false;
+        auto &other = obj.as<self_type>();
+
+        return cond.is_equal(other.cond) && body.is_equal(other.body);
+    }
+
+    size_t get_hash() const override { return ir_utils::get_hash(cond, body); }
+
+    IR_DECLARE_TRAVERSERS()
+
+    expr_t cond;
+    stmt_t body;
+
+private:
+    while_t(const expr_t &cond, const stmt_t &body)
+        : stmt_impl_t(_type_info()), cond(cond), body(body) {}
+};
+
 // Function call attribute.
 class func_call_attr_impl_t : public object_impl_t {
 public:

3ae63062bf12ed64065c42c965f84936445647f8
syurkevi <stephen.yurkevitch@intel.com>
2024-12-20 14:09:18 -0800

    xe: adds ukernel based gated mlp kernel

--- src/gpu/intel/gemm/jit/include/gemmstone/generator.hpp
+++ src/gpu/intel/gemm/jit/include/gemmstone/generator.hpp
@@ -85,9 +85,10 @@ protected:
     std::unique_ptr<Injector> postOpInjector;
 
     class status_stream {
+    public:
+        std::stringstream line;
     protected:
         char cc;
-        std::stringstream line;
         bool lineStart = true;
 
         Generator<hw> &parent;

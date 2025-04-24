061e29de20233a2758be015a7d7b02bbd79f7354
Mourad Gouicem <mourad.gouicem@intel.com>
2025-04-24 06:04:40 -0700

    tests: clang tidy cleanup

--- tests/benchdnn/runner/parallel_loop_runner.cpp
+++ tests/benchdnn/runner/parallel_loop_runner.cpp
@@ -1,3 +1,19 @@
+/*******************************************************************************
+* Copyright 2025 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
 /* Copyright 2024 The OpenXLA Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -93,12 +109,12 @@ inline void ParallelLoopRunner::ScheduleAll(
   CountDownAsyncValueRef<Chain> count_down(num_workers);
   auto count_down_done = count_down.AsRef();
 
-  auto parallelize = [this, num_tasks, count_down = std::move(count_down),
-                      parallel_task =
-                          std::forward<ParallelTask>(parallel_task)] {
-    Worker::Parallelize(device_, std::move(count_down), num_tasks,
+  auto parallelize
+          = [this, num_tasks, count_down = std::move(count_down),
+                    parallel_task = std::forward<ParallelTask>(parallel_task)] {
+                Worker::Parallelize(device_, count_down, num_tasks,
                         std::move(parallel_task));
-  };
+            };
 
   done_event_.AndThen(std::move(parallelize));
   done_event_ = std::move(count_down_done);
--- tests/test_thread.cpp
+++ tests/test_thread.cpp
@@ -141,9 +141,8 @@ public:
     bool get_in_parallel() const override { return runner_->is_in_runner(); }
     uint64_t get_flags() const override { return ASYNCHRONOUS; }
     void parallel_for(int n, const std::function<void(int, int)> &fn) override {
-        runner_->Parallelize(n, [fn_ = std::move(fn), n](size_t task_index) {
-            fn_(task_index, n);
-        });
+        runner_->Parallelize(
+                n, [fn_ = fn, n](size_t task_index) { fn_(task_index, n); });
     }
     void wait() override {
         // Here we can wait for donce_event to be concrete.

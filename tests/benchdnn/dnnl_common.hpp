7e731f406cc321035cb4774cc5a13defb0fdac55
Roy Oursler <roy.j.oursler@intel.com>
2025-04-28 12:35:43 -0700

    benchdnn: add --diff-db option

--- tests/benchdnn/dnnl_common.hpp
+++ tests/benchdnn/dnnl_common.hpp
@@ -529,6 +529,32 @@ int init_prim(benchdnn_dnnl_wrapper_t<dnnl_primitive_t> &user_prim,
 
     res->impl_id = dnnl_primitive_get_id(primw);
 
+    if (!get_db().empty() && res->impl_id != 0) {
+        // TODO: Stop using strings and use prb directly
+        std::string s = prb->str();
+
+        auto mode_modifier_start = s.find("--mode-modifier=", 0);
+        auto mode_start = s.find("--mode=", 0);
+        if (mode_modifier_start != std::string::npos) {
+            mode_modifier_start += 17;
+            while (s[mode_modifier_start] == ' ')
+                mode_modifier_start++;
+            s = s.substr(mode_modifier_start, std::string::npos);
+        } else if (mode_start != std::string::npos) {
+            mode_start += 8;
+            while (s[mode_start] == ' ')
+                mode_start++;
+            s = s.substr(mode_start, std::string::npos);
+        }
+
+        auto e = get_db().find(s);
+        if (e != get_db().end() && e->second == res->impl_id) {
+            res->state = SKIPPED;
+            res->reason = "in database";
+            return OK;
+        }
+    }
+
     auto pd = query_pd(primw);
     res->impl_name = query_impl_info(pd);
     BENCHDNN_PRINT(5, "oneDNN implementation: %s\n", res->impl_name.c_str());
--- tests/benchdnn/utils/parser.hpp
+++ tests/benchdnn/utils/parser.hpp
@@ -292,6 +292,8 @@ bool parse_perf_template(const char *&pt, const char *pt_def,
 bool parse_batch(const bench_f bench, const char *str,
         const std::string &option_name = "batch");
 
+bool parse_db(const char *str);
+
 bool parse_help(const char *str, const std::string &option_name = "help");
 bool parse_main_help(const char *str, const std::string &option_name = "help");
 
@@ -318,7 +320,7 @@ bool parse_driver_shared_settings(S &s, const S &def, const char *str) {
             || parse_perf_template(s.perf_template,
                     base_settings_t::perf_template_def, s.perf_template_csv(),
                     str)
-            || parse_reset(s, str) || parse_help(str);
+            || parse_reset(s, str) || parse_db(str) || parse_help(str);
 }
 
 void catch_unknown_options(const char *str);

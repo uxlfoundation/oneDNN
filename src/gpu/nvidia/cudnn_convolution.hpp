f8b33ff4acd43bb3423b01ebdbf7e871ea523be3
ShanoToni <anton_b_mitkov@abv.bg>
2025-05-02 16:50:16 +0100

    gpu: nvidia: Add skips for unsupported post ops for deconv

--- src/gpu/nvidia/cudnn_convolution.hpp
+++ src/gpu/nvidia/cudnn_convolution.hpp
@@ -232,6 +232,7 @@ struct cudnn_convolution_bwd_data_t : public gpu::primitive_t {
                     = utils::downcast<const xpu::sycl::engine_impl_t *>(
                             engine->impl());
             ok = ok && this->set_default_formats();
+            ok = ok && no_post_ops_check(attr());
             ok = ok
                     && (utils::everyone_is(f32, diff_src_md_.data_type,
                                 weights_md_.data_type, diff_dst_md_.data_type)
@@ -309,6 +310,7 @@ struct cudnn_convolution_bwd_weights_t : public gpu::primitive_t {
                     = utils::downcast<const xpu::sycl::engine_impl_t *>(
                             engine->impl());
             ok = ok && this->set_default_formats();
+            ok = ok && no_post_ops_check(attr());
             ok = ok
                     && (utils::everyone_is(f32, src_md_.data_type,
                                 diff_weights_md_.data_type,

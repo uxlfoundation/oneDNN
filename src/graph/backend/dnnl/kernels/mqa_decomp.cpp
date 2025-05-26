f0457d485ff8b1b6e08cfad3d88a2445bd1fef9d
Gu, Yonghao <yonghao.gu@intel.com>
2025-05-26 11:50:11 +0000

    graph: backend: dnnl: support async runtime for mqa_decom and larger partition kernel

--- src/graph/backend/dnnl/kernels/mqa_decomp.cpp
+++ src/graph/backend/dnnl/kernels/mqa_decomp.cpp
@@ -190,11 +190,11 @@ status_t mqa_decomp_kernel_t<quantized, dt>::execute_impl(
 
     // allocate the internal memory
     size_t block_size = mqa_registry_.size();
-    temporary_scratchpad_t scratchpad(
+    auto scratchpad = std::make_shared<temporary_scratchpad_t>(
             block_size * mqa_cfg_.nthr, p_engine_, *g_alloc_);
-    assertm(scratchpad.size() >= mqa_registry_.size(),
+    assertm(scratchpad->size() >= mqa_registry_.size(),
             "no enough scratchpad memory");
-    grantor_t var_grantor = mqa_registry_.grantor(scratchpad.get_buffer());
+    grantor_t var_grantor = mqa_registry_.grantor(scratchpad->get_buffer());
 
     const auto get_mem_dt_size = [](const memory &m) -> size_t {
         return memory::data_type_size(m.get_desc().get_data_type());
@@ -202,6 +202,7 @@ status_t mqa_decomp_kernel_t<quantized, dt>::execute_impl(
 
     const auto loop = [=](int tid, int nthr, dim_t bo, dim_t bi) {
         // prepare execution args and allocate real memory
+        UNUSED(scratchpad);
         prepare_sub_args(var_grantor, tid, block_size, res->mem_map);
 
         // reorder0

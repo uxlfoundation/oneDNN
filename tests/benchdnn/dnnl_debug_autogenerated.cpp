b71fbad48d4f5fbd4d9caaccaba174a9d266b724
Shreyas-fuj <shreyas.shankar@fujitsu.com>
2025-05-28 11:11:48 +0530

    aarch64: matmul: Enabling variable N block sizes for jit int8 matmul

--- tests/benchdnn/dnnl_debug_autogenerated.cpp
+++ tests/benchdnn/dnnl_debug_autogenerated.cpp
@@ -931,6 +931,12 @@ dnnl_format_tag_t str2fmt_tag(const char *str) {
     CASE(BA24b8a);
     CASE(aCB24c8b);
     CASE(abDC24d8c);
+    CASE(abDC8d8c);
+    CASE(abDC16d8c);
+    CASE(aCB8c8b);
+    CASE(aCB16c8b);
+    CASE(BA8b8a);
+    CASE(BA16b8a);
     CASE(x);
     CASE(nc);
     CASE(cn);

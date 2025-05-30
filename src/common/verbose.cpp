abc65003bc8f2b214fb45b397d2515c57b4bb014
Kassen, Andrew <andrew.kassen@intel.com>
2025-05-30 14:34:38 -0700

    common: verbose: add SDPA softmax algorithm

--- src/common/verbose.cpp
+++ src/common/verbose.cpp
@@ -1607,26 +1607,20 @@ std::string init_info_sdpa(const engine_t *e, const pd_t *pd) {
     }
     ss << ",";
 
-    delimiter = "";
+    ss << "alg:" << desc->softmax_alg;
     if (pd->with_attn_mask()) {
         auto *md = pd->attn_mask_md();
-        ss << delimiter << "msk:" << (md->dims[2] == 1 ? 1 : 2) << 'd';
-        delimiter = " ";
+        ss << " msk:" << (md->dims[2] == 1 ? 1 : 2) << 'd';
     } else if (pd->with_causal_mask()) {
-        ss << delimiter;
         if (desc->mask_type == attn_mask_type::top_left)
-            ss << "msk:causal:top_left";
+            ss << " msk:causal:top_left";
         else
-            ss << "msk:causal:bottom_right";
-        delimiter = " ";
+            ss << " msk:causal:bottom_right";
     }
-
     if (pd->with_attn_scale()) {
         // [i]scl:dt for [inverted] scale dt
-        ss << delimiter;
         if (desc->invert_scale) ss << "i";
-        ss << "scl:" << dnnl_dt2str(desc->scale_dt);
-        delimiter = " ";
+        ss << " scl:" << dnnl_dt2str(desc->scale_dt);
     }
 
     ss << "," << md2dim_str(pd->qry_md()) << ":" << md2dim_str(pd->key_md())

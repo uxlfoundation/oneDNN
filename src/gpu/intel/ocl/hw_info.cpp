b26b8ac94703596759308c4e117465bc02411e1f
Roy Oursler <roy.j.oursler@intel.com>
2025-06-04 10:10:28 -0700

    xe: add ngen::Product to device_info

--- src/gpu/intel/ocl/hw_info.cpp
+++ src/gpu/intel/ocl/hw_info.cpp
@@ -56,19 +56,15 @@ xpu::runtime_version_t get_driver_version(cl_device_id device) {
 }
 
 status_t init_gpu_hw_info(impl::engine_t *engine, cl_device_id device,
-        cl_context context, uint32_t &ip_version, compute::gpu_arch_t &gpu_arch,
-        int &gpu_product_family, int &stepping_id, uint64_t &native_extensions,
+        uint32_t &ip_version, compute::gpu_arch_t &gpu_arch,
+        ngen::Product &product, uint64_t &native_extensions,
         bool &mayiuse_systolic, bool &mayiuse_ngen_kernels) {
     using namespace ngen;
-    Product product = ngen::OpenCLCodeGenerator<HW::Unknown>::detectHWInfo(
-            context, device);
+    product = ngen::OpenCLCodeGenerator<HW::Unknown>::detectHWInfo(device);
     bool is_xelpg = (product.family == ngen::ProductFamily::ARL
             || product.family == ngen::ProductFamily::MTL);
 
     gpu_arch = jit::convert_ngen_arch_to_dnnl(ngen::getCore(product.family));
-    gpu_product_family = static_cast<int>(product.family);
-    stepping_id = product.stepping;
-
     mayiuse_systolic = false;
     CHECK(get_ocl_device_enabled_systolic_intel(device, mayiuse_systolic));
     CHECK(get_ocl_device_enabled_native_float_atomics(

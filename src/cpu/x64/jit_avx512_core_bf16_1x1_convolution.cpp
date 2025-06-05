9bb435b0003f96921fef4d745747dedaf5756603
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2025-06-04 17:32:53 -0700

    cpu: x64: jit_convs: fwd: add support for async runtime

--- src/cpu/x64/jit_avx512_core_bf16_1x1_convolution.cpp
+++ src/cpu/x64/jit_avx512_core_bf16_1x1_convolution.cpp
@@ -112,7 +112,7 @@ void jit_avx512_core_bf16_1x1_convolution_fwd_t<dst_type>::execute_forward(
         bias_dw = const_cast<float *>(bias_in);
     }
 
-    parallel(jcp.nthr, [&](const int ithr, const int nthr) {
+    parallel(jcp.nthr, [=](const int ithr, const int nthr) {
         execute_forward_thr(ithr, nthr, src, weights, bias, weights_dw, bias_dw,
                 dst, scratchpad, post_ops_binary_rhs_arg_vec.data(),
                 post_ops_binary_rhs_arg_vec_dw.data());

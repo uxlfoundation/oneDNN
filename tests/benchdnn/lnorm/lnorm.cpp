77431fe9ce0418baee6ccd008ce6a28ec970654b
nscipione <nicolo.scipione@codeplay.com>
2025-06-23 15:40:19 +0100

    generic: sycl: Add check for number of post-ops used in generic backend
    Skip the test if it is more than supported

--- tests/benchdnn/lnorm/lnorm.cpp
+++ tests/benchdnn/lnorm/lnorm.cpp
@@ -453,10 +453,8 @@ dnnl_status_t init_pd(init_pd_args_t<prb_t> &init_pd_args) {
 void skip_unimplemented_prb(const prb_t *prb, res_t *res) {
     skip_unimplemented_data_type(
             {prb->dt[0], prb->dt[1], prb->ss_dt}, prb->dir, res);
-    skip_unimplemented_sum_po(
+    skip_unimplemented_attr(
             prb->attr, res, dnnl_layer_normalization, prb->dt[0]);
-    skip_unimplemented_binary_po(prb->attr, res);
-    skip_unimplemented_prelu_po(prb->attr, res, dnnl_layer_normalization);
 
     if (is_gpu() && prb->attr.post_ops.len() != 0) {
         // GPU does not support post-ops

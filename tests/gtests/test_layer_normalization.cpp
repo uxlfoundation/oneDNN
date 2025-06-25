ccfad99fa7aba4c3e99322d32e38c7c8e0c6285d
yehudaorel <orel.yehuda@intel.com>
2025-06-25 10:05:50 -0700

    tests: lnorm: add gpu vendor check to rms

--- tests/gtests/test_layer_normalization.cpp
+++ tests/gtests/test_layer_normalization.cpp
@@ -100,23 +100,32 @@ protected:
         Forward(training, flags::use_scale | flags::use_shift);
         Forward(training,
                 flags::use_scale | flags::use_shift | flags::use_global_stats);
-        Forward(training, flags::rms_norm);
-        Forward(training,
-                flags::rms_norm | flags::use_scale | flags::use_shift);
-        Forward(training,
-                flags::rms_norm | flags::use_scale | flags::use_shift
-                        | flags::use_global_stats);
+
+        // RMS normalization on GPU is currently limited to Intel GPUs only
+        // todo: remove the vendor check once supported
+        if (!is_nvidia_gpu(eng) && !is_amd_gpu(eng)) {
+            Forward(training, flags::rms_norm);
+            Forward(training,
+                    flags::rms_norm | flags::use_scale | flags::use_shift);
+            Forward(training,
+                    flags::rms_norm | flags::use_scale | flags::use_shift
+                            | flags::use_global_stats);
+        }
 
         Forward(inference);
         Forward(inference, flags::use_global_stats);
         Forward(inference, flags::use_scale | flags::use_shift);
 
-        Forward(inference, flags::rms_norm);
-        Forward(inference,
-                flags::rms_norm | flags::use_scale | flags::use_shift);
-        Forward(inference,
-                flags::rms_norm | flags::use_scale | flags::use_shift
-                        | flags::use_global_stats);
+        // RMS normalization on GPU is currently limited to Intel GPUs only
+        // todo: remove the vendor check once supported
+        if (!is_nvidia_gpu(eng) && !is_amd_gpu(eng)) {
+            Forward(inference, flags::rms_norm);
+            Forward(inference,
+                    flags::rms_norm | flags::use_scale | flags::use_shift);
+            Forward(inference,
+                    flags::rms_norm | flags::use_scale | flags::use_shift
+                            | flags::use_global_stats);
+        }
 
         if (!impl::utils::one_of(p.dst_dt, memory::data_type::f16,
                     memory::data_type::s8, memory::data_type::u8)) {
@@ -132,9 +141,9 @@ protected:
                     flags::use_scale | flags::use_shift
                             | flags::use_global_stats);
 
-            // RMS normalization is currently supported only on CPU.
-            // todo: remove the CPU check once other engines are supported
-            if (get_test_engine_kind() == engine::kind::cpu) {
+            // RMS normalization on GPU is currently limited to Intel GPUs only
+            // todo: remove the vendor check once supported
+            if (!is_nvidia_gpu(eng) && !is_amd_gpu(eng)) {
                 Backward(prop_kind::backward, flags::rms_norm);
                 Backward(prop_kind::backward,
                         flags::rms_norm | flags::use_scale | flags::use_shift);

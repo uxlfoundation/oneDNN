7d8f7f171aa87421f0d4d302117ac1f26ef9f34c
Ankit Manerikar <ankit.v.manerikar@intel.com>
2025-07-11 16:53:37 -0700

    gpu: intel: fix stringstreams with locale-specific wrappers

--- src/gpu/intel/jit/codegen/kernel.hpp
+++ src/gpu/intel/jit/codegen/kernel.hpp
@@ -1282,7 +1282,7 @@ public:
         return interface_.getLocalSize(dim);
     }
     std::string str() {
-        std::ostringstream oss;
+        dnnl_ostringstream_t oss;
         getCode(oss);
         return oss.str();
     }

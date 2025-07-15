uxlfoundation/oneDNN

commit 5ef9899e43ad9167344362ace4a059aa2a4dfbe6

cpu: aarch64: Support for int8 in BRGEMM General and 1x1 Forward Convolution (#3552)
Co-authored-by: Kasture Deeksha <kasturedeeksha@fujitsu.com>

src/cpu/aarch64/jit_brgemm_conv.cpp
@@ -838,9 +838,9 @@ status_t brgemm_convolution_fwd_t<isa>::init(engine_t *engine) {
     for (int i_M = M_begin; i_M < M_end; i_M++) {
         // init "init" and "po" kernels for cases then we never call brgemm kernels
         // e.g. for d/h padded and dilated filter areas
-        const bool filter_in_padding = jcp.f_pad > EXT_KD
-                || jcp.back_pad > EXT_KD || jcp.t_pad > EXT_KH
-                || jcp.b_pad > EXT_KH;
+        const bool filter_in_padding = jcp.f_pad >= EXT_KD
+                || jcp.back_pad >= EXT_KD || jcp.t_pad >= EXT_KH
+                || jcp.b_pad >= EXT_KH;
         // note: overly simplistic condition. Ideally, the condition would
         // only detect cases where there is strictly no overlap between the
         // input and filter.

src/cpu/aarch64/brgemm/brgemm.cpp
@@ -231,7 +231,8 @@ status_t brgemm_desc_set_postops(brgemm_t *brg, const primitive_attr_t *attr,
 
     if (brg->is_int8) {
         if ((brg->dt_a == data_type::s8 && brg->dt_b == data_type::u8)
-                || (dt_bias != data_type::f32) || (dt_d != data_type::f32))
+                || (!one_of(dt_bias, data_type::undef, data_type::f32))
+                || (dt_d != data_type::f32))
             return status::unimplemented;
     }
     if ((brg->dt_a == data_type::bf16 && brg->dt_b == data_type::bf16)

src/cpu/aarch64/brgemm/jit_brgemm_kernel.cpp
@@ -1167,6 +1167,7 @@ void jit_brgemm_kernel_t::store_accumulators(int bd_block2, bool is_bdb_tail,
 
     if (brg.is_int8 && (brg.req_s8s8_compensation || has_zero_points)) {
         Label label_store_without_comp;
+        ldr(reg_do_comp, ptr(X_SP, reg_do_comp_offs_));
         cmp_imm(reg_do_comp, 0, X_TMP_0);
         b(EQ, label_store_without_comp);
         apply_compensation(bd_block, ld_block2, is_ld_tail);

src/cpu/aarch64/jit_brgemm_conv_comp_pad_kernel.cpp
@@ -30,7 +30,8 @@ using namespace Xbyak_aarch64;
 
 namespace jit_uni_brgemm_conv_comp_pad_kernel {
 
-#define GET_OFF(field) offsetof(jit_brgemm_conv_comp_pad_args_t, field)
+#define GET_OFF(field) \
+    (uint32_t) offsetof(jit_brgemm_conv_comp_pad_args_t, field)
 
 template <cpu_isa_t isa>
 jit_uni_brgemm_conv_comp_pad_kernel_t<isa>::
@@ -167,14 +168,14 @@ void jit_uni_brgemm_conv_comp_pad_kernel_t<isa>::khw_loop(const int icb,
         const int mb_tail, const int n_block) {
     Xbyak_aarch64::Label label_kw_loop, label_kw_end, label_kh_loop,
             label_kh_end;
-    add_imm(reg_kh_l, param1, GET_OFF(kh_l), X_TMP_0);
+    ldr(reg_kh_l, ptr(param1, GET_OFF(kh_l)));
     mov(reg_aux_kh_in, reg_in);
 
     L_aligned(label_kh_loop);
     {
         cmp(reg_kh_l, 0);
         b(EQ, label_kh_end);
-        add_imm(reg_kw_l, param1, GET_OFF(kw_l), X_TMP_0);
+        ldr(reg_kw_l, ptr(param1, GET_OFF(kw_l)));
         mov(reg_aux_kw_in, reg_aux_kh_in);
         L_aligned(label_kw_loop);
         {
@@ -196,9 +197,9 @@ void jit_uni_brgemm_conv_comp_pad_kernel_t<isa>::khw_loop(const int icb,
 
 template <cpu_isa_t isa>
 void jit_uni_brgemm_conv_comp_pad_kernel_t<isa>::load_params() {
-    add_imm(reg_in, param1, GET_OFF(ptr_in), X_TMP_0);
-    add_imm(reg_zp_comp_out, param1, GET_OFF(ptr_zp_out), X_TMP_1);
-    add_imm(reg_comp_out, param1, GET_OFF(ptr_cp_out), X_TMP_2);
+    ldr(reg_in, ptr(param1, (GET_OFF(ptr_in))));
+    ldr(reg_zp_comp_out, ptr(param1, (GET_OFF(ptr_zp_out))));
+    ldr(reg_comp_out, ptr(param1, (GET_OFF(ptr_cp_out))));
 }
 
 template <cpu_isa_t isa>
@@ -276,7 +277,7 @@ void jit_uni_brgemm_conv_comp_pad_kernel_t<isa>::generate() {
     const auto mb_tail = div_up(icb_tail, ic_step);
 
     Label label_kd_loop, label_loop_end;
-    add_imm(reg_kd_l, param1, GET_OFF(kd_l), X_TMP_0);
+    ldr(reg_kd_l, ptr(param1, GET_OFF(kd_l)));
 
     zero_accumulators(m_block, n_block);
 

src/cpu/aarch64/jit_brgemm_conv_utils.cpp
@@ -828,7 +828,8 @@ bool brg_blocking_t::fast_check_oc_block() const {
     const auto rnd_oc = rnd_up(oc, acc_simd_w);
     auto res = false;
     if (oc_block == 64) {
-        res = (rnd_oc % oc_block == 0 && rnd_oc * wei_dsz < 192 * 4);
+        res = one_of(src_dt, u8, s8)
+                || (rnd_oc % oc_block == 0 && rnd_oc * wei_dsz < 192 * 4);
     } else if (oc_block == 48) {
         const bool big_spatial
                 = id * ih * iw > 81 * stride_d * stride_h * stride_w;

src/cpu/aarch64/jit_brgemm_post_ops.hpp
@@ -48,7 +48,7 @@ struct brgemm_kernel_diff_bias_t {
     int flags;
 };
 
-#define GET_OFF(field) offsetof(brgemm_kernel_diff_bias_t, field)
+#define GET_OFF(field) (uint32_t) offsetof(brgemm_kernel_diff_bias_t, field)
 
 struct jit_brgemm_kernel_diff_bias_t : public jit_generator {
     jit_brgemm_kernel_diff_bias_t(
@@ -457,8 +457,9 @@ private:
             case data_type::bf16: assert(!"unsupported data type\n"); break;
             default: assert(!"unsupported data type");
         }
-        if (!utils::one_of(type_in, data_type::f32, data_type::bf16))
-            assert(!"unsupported data type\n");
+        if (types::is_integral_dt(type_in)) {
+            scvtf(zmm_in.s, P_ALL_ONE / T_m, zmm_in.s);
+        }
     }
 
     ZReg vector(int m, int n, int n_block) { return ZReg(m * n_block + n); };
@@ -618,9 +619,8 @@ private:
                 add_imm(X_DEFAULT_ADDR, aux_reg_scales,
                         is_oc_scale_ * sizeof(float) * (n * brg.ld_block),
                         X_TMP_0);
-                ldr(vmm_tmp0, ptr(X_DEFAULT_ADDR));
+                ld1w(vmm_tmp0.s, k_mask, ptr(X_DEFAULT_ADDR));
                 fmul(vmm.s, vmm.s, vmm_tmp0.s);
-                if (tail > 0) mov(vmm.s, k_mask / T_m, vmm.s);
             }
         }
 

src/cpu/cpu_convolution_list.cpp
@@ -651,6 +651,8 @@ const std::map<pk_dt_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map()
             CPU_INSTANCE_AVX2(jit_uni_x8s8s32x_convolution_fwd_t<avx2>)
             CPU_INSTANCE_SSE41(jit_uni_x8s8s32x_1x1_convolution_fwd_t<sse41>)
             CPU_INSTANCE_SSE41(jit_uni_x8s8s32x_convolution_fwd_t<sse41>)
+            CPU_INSTANCE_AARCH64(brgemm_1x1_convolution_fwd_t<sve_256>)
+            CPU_INSTANCE_AARCH64(brgemm_convolution_fwd_t<sve_256>)
             CPU_INSTANCE_AARCH64(jit_sve_512_x8s8s32x_convolution_fwd_t<u8, f32>)
             CPU_INSTANCE(gemm_x8s8s32x_convolution_fwd_t)
             CPU_INSTANCE(ref_convolution_int8_fwd_t)

commit 491df25bcc78ab2e7850d7d1b876ef627144a050

cpu: aarch64: Fix nightly test crash caused by PR #3552

src/cpu/aarch64/jit_brgemm_conv.cpp
@@ -1,6 +1,7 @@
 /*******************************************************************************
 * Copyright 2021-2023 Intel Corporation
 * Copyright 2024-2025 FUJITSU LIMITED
+* Copyright 2025 Arm Ltd. and affiliates
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -534,6 +535,9 @@ status_t brgemm_convolution_fwd_t<isa>::pd_t::init(engine_t *engine) {
     if (jcp_.with_scales)
         book_precomputed_scales(scratchpad, attr()->scales_, OC(),
                 jcp_.scale_adjust_factor != 1.0f);
+
+    // temporary fix for large l_pad failing test caused by PR #3552
+    if (2 * jcp_.l_pad > jcp_.ow_block) return status::unimplemented;
 
     return status::success;
 }
547e0bedf94c85e0a73c30178fe7c694b2e55003
Simonov, Alexander <alexander.simonov@intel.com>
2025-08-01 06:56:06 -0700

    cpu: rnn: implement merged gemm layer step using matmul
    One layer case is supported.

--- src/cpu/rnn/cell_common.cpp
+++ src/cpu/rnn/cell_common.cpp
@@ -321,10 +321,17 @@ rnn_merged_layer_execution_sig((ref_rnn_fwd_t<src_type, weights_type,
             : rnn.n_iter - (rnn.skip_dst_iter_copy() ? 1 : 0);
     cell_position |= merged_layer;
 
-    CHECK((this->*gemm_layer_func)('N', 'N', rnn.n_gates * rnn.dhc,
-            rnn.mb * n_iter, rnn.slc, 1.0, w_layer_[0], rnn.weights_layer_ld,
-            src_layer_, src_layer_ld, 0.0, (gemm_acc_t *)scratch_gates_,
-            rnn.scratch_gates_ld));
+    if (rnn.use_matmul) {
+        assert(n_iter == rnn.n_iter);
+
+        CHECK(this->execute_matmul(ctx, this->get_matmul_layer(cell_position),
+                w_layer_[0], src_layer_, scratch_gates_));
+    } else {
+        CHECK((this->*gemm_layer_func)('N', 'N', rnn.n_gates * rnn.dhc,
+                rnn.mb * n_iter, rnn.slc, 1.0, w_layer_[0],
+                rnn.weights_layer_ld, src_layer_, src_layer_ld, 0.0,
+                (gemm_acc_t *)scratch_gates_, rnn.scratch_gates_ld));
+    }
 
     return dnnl_success;
 }

75d56ac9e01099418a785f51ed129c0357e9492d
Guskov, Andrey Y <andrey.y.guskov@intel.com>
2025-08-05 18:14:34 -0700

    xe: matmul: pass user-precomputed partial reductions to gemm

--- src/gpu/intel/matmul/gemm.hpp
+++ src/gpu/intel/matmul/gemm.hpp
@@ -298,6 +298,7 @@ struct gemm_t : public primitive_t {
 
                 scales_t reshaped_scales = gemm_attr.scales_;
                 zero_points_t reshaped_zp = gemm_attr.zero_points_;
+                placeholder_t reshaped_pl = gemm_attr.placeholder_;
                 CHECK(adjust_quant(reshaped_scales, DNNL_ARG_SRC, *a_md,
                         a_md_reshaped, diff_dims));
                 CHECK(adjust_quant(reshaped_scales, DNNL_ARG_WEIGHTS, *b_md,
@@ -310,6 +311,10 @@ struct gemm_t : public primitive_t {
                         b_md_reshaped, diff_dims));
                 CHECK(adjust_quant(reshaped_zp, DNNL_ARG_DST, *c_md,
                         c_md_reshaped, diff_dims));
+                CHECK(adjust_quant(reshaped_pl, DNNL_ARG_SRC, *a_md,
+                        a_md_reshaped, diff_dims));
+                CHECK(adjust_quant(reshaped_pl, DNNL_ARG_WEIGHTS, *b_md,
+                        b_md_reshaped, diff_dims));
 
                 // Reshaping successful - lock in changes
                 a_md = &a_md_reshaped;
@@ -319,6 +324,7 @@ struct gemm_t : public primitive_t {
 
                 gemm_attr.scales_ = reshaped_scales;
                 gemm_attr.zero_points_ = reshaped_zp;
+                gemm_attr.placeholder_ = reshaped_pl;
                 gemm_attr.post_ops_ = reshaped_post_ops;
                 return status::success;
             };

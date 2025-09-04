e581f04232003e0166299b0fc7d36b2d96ba8aaa
Umar Arshad <umar.arshad@intel.com>
2025-09-04 14:08:58 -0700

    xe: sdpa: Select SDPA config using scoring criterias

--- src/gpu/intel/sdpa/configs.hpp
+++ src/gpu/intel/sdpa/configs.hpp
@@ -97,14 +97,12 @@ bool operator==(const config_record_t &key, const config_query_t &query);
 bool operator<(const config_criteria_t &lhs, const config_criteria_t &rhs);
 bool operator<(const config_record_t &lhs, const config_record_t &rhs);
 
-config_t *choose_config(compute::gpu_arch_t arch, dim_t head_size, dim_t seq,
-        bool is_thin_q, bool is_quantized, bool is_integrated, bool is_fma,
-        bool is_f32);
-dim_t round_up_seq_interval(dim_t seq, compute::gpu_arch_t arch);
-
-dim_t nearest_conf_seq_interval(compute::gpu_arch_t arch, dim_t head_size,
+config_record_t *choose_config(compute::gpu_arch_t arch, dim_t head_size,
         dim_t seq, bool is_thin_q, bool is_quantized, bool is_integrated,
         bool is_fma, bool is_f32);
+dim_t round_up_seq_interval(dim_t seq, compute::gpu_arch_t arch);
+
+dim_t nearest_conf_seq_interval(config_record_t *record);
 
 // serializable options for microkernel configuration
 // follows reduced subset of structs from gemmstone that

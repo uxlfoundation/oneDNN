81b82d6ffd8efd996330ca316646517375b2d2ff
Sergey Kazakov <sergey.kazakov@intel.com>
2025-12-03 09:15:00 -0800

    minor

--- src/gpu/intel/gemm/jit/gen_kernel.cpp
+++ src/gpu/intel/gemm/jit/gen_kernel.cpp
@@ -1053,18 +1053,14 @@ void gen_kernel_t::init_interface() {
                 "bo_ptr", ExternalArgumentType::GlobalPtr, bo_access);
     }
 
-    // @@@@@ ?????
-    //bool abo_precond = (problem.aoPtrDims == -1 && problem.aoPtrDims == -1) &&
-    //    (problem.ao_hostscalar || problem.bo_hostscalar);
-
+    // @@@@@
     bool abo_precond = (problem.aoPtrDims == -1 && problem.ao_hostscalar)
             || (problem.boPtrDims == -1 && problem.bo_hostscalar);
-
     if (abo_precond) {
         VDEBUGINFO(4, primitive, gen_kernel,"MY: >>>> @@@@ interface_.newArgument abo");
         interface_.newArgument("abo", DataType::ud);
     }
-    // @@@@@ ?????
+    // @@@@@
 
     if (problem.aScale2D()){
         interface_.newArgument(

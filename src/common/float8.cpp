ea0f46f6c55915d290910fd58570d62319f4dcae
Simonov, Alexander <alexander.simonov@intel.com>
2025-12-23 05:05:18 -0800

    common: implement conversion of bf16 to fp8 e5m2 and e4m3

--- src/common/float8.cpp
+++ src/common/float8.cpp
@@ -71,6 +71,89 @@ float8_e5m2_t &float8_e5m2_t::operator=(float16_t f) {
     return *this;
 }
 
+float8_e5m2_t &float8_e5m2_t::operator=(bfloat16_t f) {
+    constexpr auto bf16_mantissa_len = 7;
+    constexpr auto bf16_mantissa_mask = 0x007f;
+    constexpr auto bf16_exp_mask = 0x7f80;
+    constexpr auto f8_e5m2_mantissa_len = 2;
+
+    const bool is_special = (f.raw_bits_ & bf16_exp_mask)
+            == bf16_exp_mask; // exp has all bits set
+    const bool is_nan = is_special
+            && (f.raw_bits_ & bf16_mantissa_mask); // and non-zero mantissa
+
+    if (is_special) {
+        // sign bit and 5 exp bits = 6 bits, they are S'11111
+        raw_bits_ = ((f.raw_bits_ >> 8) & 0xfc);
+
+        // we always set quiet bit for NaN
+        if (is_nan) {
+            // also add 2 mantissa bits
+            raw_bits_ = raw_bits_ | ((f.raw_bits_ >> 5) & 0x3) | 0x2;
+            return *this;
+        }
+
+        // if infinity then mantissa is zero
+        return *this;
+    }
+
+    // first we extract the sign and make the input positive
+    const unsigned int s8 = (f.raw_bits_ & 0x8000) >> 8;
+    const int fraw = f.raw_bits_ & 0x7fff;
+
+    // we filter out overflow
+    // Note: values in [57344;61184] round to 57344, which is representable
+    // So we overflow above 61184
+    constexpr auto bf16_61184_raw = 0x476f;
+    if (fraw > bf16_61184_raw) {
+        raw_bits_ = s8 | 0x7c; // S'11111'00
+        return *this;
+    }
+    // we filter out underflow when f <= 2^-17
+    constexpr auto bf16_2_to_minus17_raw = 0x3700;
+    if (fraw <= bf16_2_to_minus17_raw) {
+        raw_bits_ = s8;
+        return *this;
+    }
+
+    // Here the idea is to add a large constant to the bfloat16_t to force the
+    // proper rounding to f8_e5m2 accuracy.
+    // Compute the rounding shifter by taking its exponent + 5.
+    // It does not overflow as fraw <= 61184.
+    // This allows to discard 5 bits of mantissa during addition,
+    // leaving the remaining 2 bits perfectly rounded.
+    constexpr bool is_bitcast = true;
+    constexpr auto shifter_exp_adjustment = 5 << bf16_mantissa_len;
+    bfloat16_t shifter(
+            (fraw & bf16_exp_mask) + shifter_exp_adjustment, is_bitcast);
+    // e8 = e16 - e16_bias + e8_bias = e16 - 127 + 15
+    // e8 will be denorm if e8 <= 0 or e16 + 5 < 118
+    constexpr int exp_threshold = 0x3b00; // raw bits of exponent = 118
+    const bool is_denorm = shifter.raw_bits_ < exp_threshold;
+    if (is_denorm) shifter.raw_bits_ = exp_threshold;
+
+    bfloat16_t rounded = (bfloat16_t(fraw, is_bitcast) + shifter);
+    // separate line to force line above to round to bf16
+    rounded = rounded - shifter;
+
+    constexpr auto exp_bias_diff = 112; // bf16_bias - f8_e5m2_bias = 127 - 15
+    int e8 = ((rounded.raw_bits_ & bf16_exp_mask) >> bf16_mantissa_len)
+            - exp_bias_diff;
+
+    constexpr auto mantissa_len_diff = bf16_mantissa_len - f8_e5m2_mantissa_len;
+    uint8_t m8 = (rounded.raw_bits_ & bf16_mantissa_mask) >> mantissa_len_diff;
+
+    // we need to make the implicit mantissa bit explicit for
+    // denorm f8_e4m3
+    if (is_denorm) {
+        m8 = (m8 | (1 << f8_e5m2_mantissa_len)) >> (-e8 + 1);
+        e8 = 0;
+    }
+
+    raw_bits_ = s8 | (e8 << f8_e5m2_mantissa_len) | m8;
+    return *this;
+}
+
 float8_e5m2_t &float8_e5m2_t::operator=(float f) {
     float16_t f16 = f;
     float8_e5m2_t f8 = f16;
@@ -187,6 +270,71 @@ float8_e4m3_t &float8_e4m3_t::operator=(float f) {
     float16_t f16 = f;
     float8_e4m3_t f8 = f16;
     raw_bits_ = f8.raw_bits_;
+    return *this;
+}
+
+float8_e4m3_t &float8_e4m3_t::operator=(bfloat16_t f) {
+    constexpr auto bf16_mantissa_len = 7;
+    constexpr auto bf16_mantissa_mask = 0x007f;
+    constexpr auto bf16_exp_mask = 0x7f80;
+    constexpr auto f8_e4m3_mantissa_len = 3;
+
+    int fraw = f.raw_bits_;
+
+    // first we extract the sign and make the input positive
+    const unsigned int s8 = (fraw & 0x8000) >> 8;
+    fraw = fraw & 0x7fff;
+
+    // we filter out overflow, nan
+    // Note: values in [448;464] round to 448, which is representable
+    // So we overflow above 464
+    constexpr auto bf16_464_raw = 0x43e8;
+    if (fraw > bf16_464_raw) {
+        raw_bits_ = s8 | 0x7f; // S'1111'111
+        return *this;
+    }
+    // we filter out underflow when f <= 2^-10
+    constexpr auto bf16_2_to_minus10_raw = 0x3a80;
+    if (fraw <= bf16_2_to_minus10_raw) {
+        raw_bits_ = s8;
+        return *this;
+    }
+
+    // Here the idea is to add a large constant to the bfloat16_t to force the
+    // proper rounding to f8_e4m3 accuracy.
+    // Compute the rounding shifter by taking its exponent + 4.
+    // It does not overflow as fraw <= 464.
+    // This allows to discard 4 bits of mantissa during addition,
+    // leaving the remaining 3 bits perfectly rounded.
+    constexpr bool is_bitcast = true;
+    constexpr auto shifter_exp_adjustment = 4 << bf16_mantissa_len;
+    bfloat16_t shifter(
+            (fraw & bf16_exp_mask) + shifter_exp_adjustment, is_bitcast);
+    // e8 = e16 - e16_bias + e8_bias = e16 - 127 + 7
+    // e8 will be denorm if e8 <= 0 or e16 + 4 < 125
+    constexpr int exp_threshold = 0x3e80; // raw bits of exponent = 125
+    const bool is_denorm = shifter.raw_bits_ < exp_threshold;
+    if (is_denorm) shifter.raw_bits_ = exp_threshold;
+
+    bfloat16_t rounded = (bfloat16_t(fraw, is_bitcast) + shifter);
+    // separate line to force line above to round to bf16
+    rounded = rounded - shifter;
+
+    constexpr auto exp_bias_diff = 120; // bf16_bias - f8_e4m3_bias = 127 - 7
+    int e8 = ((rounded.raw_bits_ & bf16_exp_mask) >> bf16_mantissa_len)
+            - exp_bias_diff;
+
+    constexpr auto mantissa_len_diff = bf16_mantissa_len - f8_e4m3_mantissa_len;
+    uint8_t m8 = (rounded.raw_bits_ & bf16_mantissa_mask) >> mantissa_len_diff;
+
+    // we need to make the implicit mantissa bit explicit for
+    // denorm f8_e4m3
+    if (is_denorm) {
+        m8 = (m8 | (1 << f8_e4m3_mantissa_len)) >> (-e8 + 1);
+        e8 = 0;
+    }
+
+    raw_bits_ = s8 | (e8 << f8_e4m3_mantissa_len) | m8;
     return *this;
 }
 

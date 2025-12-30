4981683ec42977ccbd6c9ddede4678c37c4f92a2
Umar Arshad <umar.arshad@intel.com>
2025-12-30 10:07:07 -0800

    xe: grp_gemm: Enable microkenrel implementation for grouped gemm
    Only supports power of 2 shapes and f16xf16:f32 types

---
+++ src/gpu/intel/matmul/grouped_micro_gemm.cpp
@@ -0,0 +1,276 @@
+/*******************************************************************************
+* Copyright 2025 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include "gpu/intel/matmul/grouped_micro_gemm.hpp"
+
+#include "gemmstone/microkernel_provider.hpp"
+#include "gpu/intel/compute/ukernels.hpp"
+#include "gpu/intel/compute/utils.hpp"
+#include "gpu/intel/gemm/jit/gen_kernel.hpp"
+#include "gpu/intel/microkernels/shim.hpp"
+
+namespace dnnl {
+namespace impl {
+namespace gpu {
+namespace intel {
+namespace matmul {
+
+status_t grouped_micro_gemm_t::init_microkernels(impl::engine_t *engine) {
+    using namespace jit;
+    using namespace gemmstone;
+    using gemm::jit::convert_dnnl_to_kernel_type;
+
+    assert(engine->kind() == engine_kind::gpu);
+    auto *intel_engine = utils::downcast<intel::engine_t *>(engine);
+    auto *dev_info = intel_engine->device_info();
+    //arch_ = dev_info->gpu_arch();
+    auto pd_ = (pd_t *)primitive_t::pd().get();
+
+    /* Get device information */
+    HWInformation hw_info;
+    hw_info.euCount = dev_info->eu_count();
+    hw_info.gmdid = dev_info->ip_version();
+    hw_info.systolicAvailable = pd()->use_systolic_ukernel_;
+
+    if (hw_info.gmdid == 0) return status::unimplemented;
+
+    int m = static_cast<int>(pd_->dst_md(0)->dims[pd_->dst_md(0)->ndims - 2]);
+    int n = static_cast<int>(pd_->dst_md(0)->dims[pd_->dst_md(0)->ndims - 1]);
+    int k = static_cast<int>(pd_->src_md(0)->dims[pd_->src_md(0)->ndims - 1]);
+
+    // User(rm)                         Gemmstone(cm)
+    // MxN   MxK   KxN        NxM   MxK     KxN
+    // Dst  =  Src  x  Weights  ->  Dst  =  Weights x Src
+
+    //printf("User              D=SxW M=%d, N=%d, K=%d\n", m, n, k);
+    //printf("Grouped MicroGEMM D=WxS M=%d, N=%d, K=%d\n", n, m, k);
+    GEMMProblem problem;
+    problem.Ta_ext = convert_dnnl_to_kernel_type(pd_->weights_md()->data_type);
+    problem.Tb_ext = convert_dnnl_to_kernel_type(pd_->src_md(0)->data_type);
+    problem.Tc_ext = convert_dnnl_to_kernel_type(pd_->dst_md(0)->data_type);
+    problem.Ta = problem.Ta_ext;
+    problem.Tb = problem.Tb_ext;
+    problem.Ts = problem.Tc = Type::f32;
+    problem.A.layout = MatrixLayout::N;
+    problem.B.layout = MatrixLayout::N;
+    problem.C.layout = MatrixLayout::N;
+    problem.A.setAlignment(alignmentForLD(k * problem.Ta));
+    problem.B.setAlignment(alignmentForLD(n * problem.Tb));
+    problem.C.setAlignment(problem.Tc.size());
+    //problem.A.setAlignment(alignmentForLD(ldk));
+
+    SizeParams sizes;
+    sizes.m = static_cast<uint16_t>(m);
+    sizes.n = static_cast<uint16_t>(n);
+    sizes.k = static_cast<uint16_t>(k);
+    //std::cout << "Sizes: " << sizes.m << ", " << sizes.n << ", " << sizes.k << std::endl;
+
+    micro::Package gemm;
+
+    micro::GEMMProtocol::Options opts;
+
+    std::vector<StrategyRequirement> reqs;
+    reqs.push_back(StrategyRequirement::UnrollM == 32);
+    reqs.push_back(StrategyRequirement::UnrollN == 16);
+    reqs.push_back(StrategyRequirement::WGM == 2);
+    reqs.push_back(StrategyRequirement::WGN == 1);
+
+    try {
+        gemm = selectGEMMMicrokernel(opts, hw_info, sizes, problem, reqs);
+    } catch (const std::runtime_error &ex) {
+        return status::unimplemented;
+        //CHECK_BOOL(false,
+        //         "gemm microkernel generation failure with message: %s",
+        //         ex.what());
+    }
+    //printf("Gemm: sg_per_wg_m=%d, sg_per_wg_n=%d, unroll_m=%d, unroll_n=%d\n",
+    //        gemm.getSetting("sg_per_wg_m"), gemm.getSetting("sg_per_wg_n"),
+    //        gemm.getSetting("sg_tile_m"), gemm.getSetting("sg_tile_n"));
+
+    auto sg_size = dev_info->min_subgroup_size();
+
+    /* Generate microkernel shims */
+    micro::ShimOptions shimOptions;
+    shimOptions.subgroupSize = sg_size;
+    shimOptions.useTileOps = true;
+    shimOptions.decorator = "grouped";
+
+    kernel_ctx_.define_int("SUBGROUP_SIZE", sg_size);
+    kernel_ctx_.add_custom_header("gemm_grouped.h",
+            micro::generateShim(
+                    gemm, micro::HostLanguage::OpenCL_C, shimOptions));
+
+    pd_->sg_per_wg_m_ = gemm.getSetting("sg_per_wg_m");
+    pd_->sg_per_wg_n_ = gemm.getSetting("sg_per_wg_n");
+    pd_->sg_tile_m_ = gemm.getSetting("sg_tile_m");
+    pd_->sg_tile_n_ = gemm.getSetting("sg_tile_n");
+
+    return status::success;
+}
+
+status_t grouped_micro_gemm_t::pd_t::init(impl::engine_t *engine) {
+    using namespace data_type;
+
+    src_dt_ = src_md(0)->data_type;
+    wei_dt_ = weights_md(0)->data_type;
+    dst_dt_ = dst_md(0)->data_type;
+
+    memory_desc_wrapper src_d(src_md());
+    memory_desc_wrapper wei_d(weights_md(0));
+    memory_desc_wrapper dst_d(dst_md());
+    //std::cout << "src: " << src_d.dims()[0] << "," << src_d.dims()[1]
+    //          << std::endl;
+    //std::cout << "wei: " << wei_d.dims()[0] << "," << wei_d.dims()[1] << ","
+    //          << wei_d.dims()[2] << std::endl;
+    //std::cout << "dst: " << dst_d.dims()[0] << "," << dst_d.dims()[1]
+    //          << std::endl;
+
+    // Check for grouped encoding on src and dst
+    VDISPATCH_MATMUL(src_d.is_grouped_desc() && dst_d.is_grouped_desc(),
+            VERBOSE_UNSUPPORTED_SPARSE_CFG);
+
+    // Weights should be dense
+    VDISPATCH_MATMUL(!wei_d.is_sparse_desc() && !wei_d.is_grouped_desc(),
+            VERBOSE_UNSUPPORTED_SPARSE_CFG);
+
+    // Extract grouped encoding
+    const auto &src_grouped = src_d.sparse_desc().grouped_desc;
+    const auto &dst_grouped = dst_d.sparse_desc().grouped_desc;
+
+    // Validate matching number of groups
+    VDISPATCH_MATMUL(src_grouped.ngroups == dst_grouped.ngroups,
+            VERBOSE_INCONSISTENT_NDIMS_WITH_VALS, "src ngroups", "dst ngroups",
+            (int)src_grouped.ngroups, (int)dst_grouped.ngroups);
+
+    ngroups_ = src_grouped.ngroups;
+    // only supported dt for now
+    VDISPATCH_MATMUL(utils::one_of(src_dt_, f32, f16)
+                    && src_dt_ == wei_dt_, //&& src_dt_ == dst_type,
+            VERBOSE_UNSUPPORTED_DT_CFG);
+
+    // Check offsets are int32
+    VDISPATCH_MATMUL(
+            src_d.metadata_type(0) == s32 && dst_d.metadata_type(0) == s32,
+            VERBOSE_UNSUPPORTED_SPARSE_CFG);
+
+    // Check for limited Bias support
+    if (with_bias()) {
+        memory_desc_wrapper bia_d(weights_md(1));
+        VDISPATCH_MATMUL(!bia_d.is_sparse_desc() && !bia_d.is_grouped_desc(),
+                VERBOSE_UNSUPPORTED_BIAS_CFG);
+        VDISPATCH_MATMUL(bia_d.ndims() == 2, VERBOSE_UNSUPPORTED_BIAS_CFG);
+        // Bias shape should be [num_experts, N]
+        VDISPATCH_MATMUL(bia_d.dims()[0] == src_grouped.ngroups,
+                VERBOSE_INCONSISTENT_DIM, "bia_d", 0, "src_grouped.ngroups",
+                -1);
+        VDISPATCH_MATMUL(bia_d.dims()[1] == wei_d.dims()[2],
+                VERBOSE_INCONSISTENT_DIM, "bia_d", 1, "wei_d", 2);
+    }
+
+    // No scales/post-ops for now
+    VDISPATCH_MATMUL(attr()->has_default_values(), VERBOSE_UNSUPPORTED_ATTR);
+
+    assert(engine->kind() == engine_kind::gpu);
+    auto *intel_engine = utils::downcast<intel::engine_t *>(engine);
+    auto *dev_info = intel_engine->device_info();
+    //arch_ = dev_info->gpu_arch();
+    VDISPATCH_MATMUL(compute::mayiuse_microkernels(intel_engine),
+            VERBOSE_UNSUPPORTED_DEVICE_FEATURE, "microkernels");
+
+    use_systolic_ukernel_ = intel_engine->mayiuse(
+            compute::device_ext_t::intel_subgroup_matrix_multiply_accumulate);
+    sg_size_ = dev_info->min_subgroup_size();
+
+    return status::success;
+}
+
+status_t grouped_micro_gemm_t::init(impl::engine_t *engine) {
+
+    CHECK(init_microkernels(engine));
+    kernel_ctx_.set_data_type(pd()->dst_md()->data_type);
+    return create_kernel(engine, &kernel_, "grouped_micro_gemm", kernel_ctx_);
+}
+
+status_t grouped_micro_gemm_t::execute(const exec_ctx_t &ctx) const {
+    // buffer 0: values, buffer 1: offsets
+    const auto &src_data = CTX_IN_STORAGE(DNNL_ARG_SRC, 0);
+    const auto &src_offsets = CTX_IN_STORAGE(DNNL_ARG_SRC, 1);
+    const auto &wei_data = CTX_IN_STORAGE(DNNL_ARG_WEIGHTS);
+    auto &dst_data = CTX_OUT_STORAGE(DNNL_ARG_DST, 0);
+    const auto &dst_offsets = CTX_OUT_STORAGE(DNNL_ARG_DST, 1);
+
+    auto pd_ = pd();
+    const auto *src_md = pd()->src_md(0);
+    const auto *wei_md = pd()->weights_md();
+    const auto *dst_md = pd()->dst_md(0);
+
+    const size_t num_groups = pd()->ngroups_;
+
+    const auto &attr_scales = pd()->attr()->scales_;
+    const bool with_src_scales = !attr_scales.has_default_values(DNNL_ARG_SRC);
+    const bool with_bias = pd()->with_bias();
+
+    int m_all = static_cast<int>(dst_md->dims[dst_md->ndims - 2]);
+    int n = static_cast<int>(dst_md->dims[dst_md->ndims - 1]);
+    int k = static_cast<int>(src_md->dims[src_md->ndims - 1]);
+    //printf("m_all=%d, n=%d, k=%d\n", m_all, n, k);
+
+    int ldsrc = static_cast<int>(src_md->dims[src_md->ndims - 1]);
+    int ldwei = static_cast<int>(wei_md->dims[wei_md->ndims - 1]);
+    int lddst = static_cast<int>(dst_md->dims[dst_md->ndims - 1]);
+    //printf("lda=%d, ldb=%d, ldc=%d\n", ldsrc, ldwei, lddst);
+
+    compute::kernel_arg_list_t arg_list;
+    arg_list.append(src_data);
+    arg_list.append(ldsrc);
+    arg_list.append(wei_data);
+    arg_list.append(ldwei);
+    arg_list.append(dst_data);
+    arg_list.append(lddst);
+    arg_list.append(src_offsets);
+    arg_list.append(dst_offsets);
+    arg_list.append(n);
+    arg_list.append(k);
+    //arg_list.append((int)num_groups);
+
+    if (with_bias) {
+        const auto &bias_data = CTX_IN_STORAGE(DNNL_ARG_BIAS);
+        arg_list.append(bias_data);
+    }
+    if (with_src_scales) {
+        const auto &src_scales
+                = CTX_IN_STORAGE(DNNL_ARG_ATTR_SCALES | DNNL_ARG_SRC);
+        arg_list.append(src_scales);
+    }
+
+    //printf("m_all=%d, n=%d, k=%d sg_per_wg_m: %ld sg_per_wg_n: %ld\n", m_all, n,
+    //k, pd_->sg_per_wg_m_, pd_->sg_per_wg_n_);
+    // Use total_tokens as upper bound for M dimension
+    compute::range_t lws = {(size_t)pd_->sg_per_wg_m_ * pd_->sg_size_,
+            (size_t)pd_->sg_per_wg_n_, 1};
+    compute::range_t gws = {utils::div_up(m_all, pd_->sg_tile_m_) * lws[0],
+            utils::div_up(n, pd_->sg_tile_n_) * lws[1], num_groups};
+    //std::cout << "LWS: " << lws.str() << std::endl;
+    //std::cout << "GWS: " << gws.str() << std::endl;
+
+    return parallel_for(ctx, compute::nd_range_t(gws, lws), kernel_, arg_list);
+}
+
+} // namespace matmul
+} // namespace intel
+} // namespace gpu
+} // namespace impl
+} // namespace dnnl
--- tests/gtests/test_grouped_gemm.cpp
+++ tests/gtests/test_grouped_gemm.cpp
@@ -1,5 +1,6 @@
 
 #include "dnnl_test_common.hpp"
+#include "internals/test_utils.cpp"
 #include "gtest/gtest.h"
 
 #include "oneapi/dnnl/dnnl.hpp"
@@ -8,7 +9,7 @@ class GroupedGEMM : public ::testing::Test {
 protected:
     void SetUp() override {
         // Initialize DNNL engine and stream
-        eng = dnnl::engine(dnnl::engine::kind::cpu, 0);
+        eng = dnnl::engine(dnnl::engine::kind::gpu, 0);
         strm = dnnl::stream(eng);
     }
 
@@ -23,27 +24,95 @@ using dnnl::matmul;
 using std::unordered_map;
 
 TEST_F(GroupedGEMM, BasicCreation) {
-    const int num_groups = 2;
-    const int num_experts = 4;
-    const int K = 256;
-    const int N = 256;
-    const int M_all = 9; // total rows (tokens) across all experts
-    const memory::data_type dt = memory::data_type::f32;
+    const int K = 128;
+    const int N = 512;
+    std::vector<int> M_group_sizes
+            = {256, 128, 64, 48, 96, 512, 32, 32}; // rows per group
+    const int num_groups = M_group_sizes.size();
+    std::vector<int> M_group_offsets(num_groups + 1, 0);
+    std::inclusive_scan(begin(M_group_sizes), end(M_group_sizes),
+            begin(M_group_offsets) + 1);
+    const int M_all = M_group_offsets.back();
 
-    memory::desc src_md = memory::desc::grouped(
-            {M_all, K}, dt, num_groups, {DNNL_RUNTIME_DIM_VAL, K});
+    const memory::dims src_sz = {M_all, K};
+    const memory::dims weights_sz = {num_groups, K, N};
+    const memory::dims dst_sz = {M_all, N};
 
-    auto weights_md
-            = memory::desc({num_experts, K, N}, dt, memory::format_tag::abc);
+    const memory::data_type sdt = memory::data_type::f16;
+    const memory::data_type wdt = memory::data_type::f16;
+    const memory::data_type ddt = memory::data_type::f32;
 
-    auto dst_md = memory::desc::grouped(
-            {M_all, N}, dt, num_groups, {DNNL_RUNTIME_DIM_VAL, N});
+    memory::desc src_md = memory::desc::grouped(src_sz, sdt, 0, num_groups);
+    auto weights_md = memory::desc(weights_sz, wdt, memory::format_tag::abc);
+    auto dst_md = memory::desc::grouped(dst_sz, ddt, 0, num_groups);
 
     memory src_mem(src_md, eng);
     memory dst_mem(dst_md, eng);
     memory weights_mem(weights_md, eng);
+
+    std::vector<float> src_data(product(src_sz));
+    std::vector<float> weights_data(product(weights_sz));
+
+#if 0
+    // Fill src and weights with sequential data for easier debugging
+    for (int i = 0, group = 0; i < (int)src_data.size(); ++i) {
+        if (i >= M_group_offsets[group + 1] * K) ++group;
+        src_data[i] = i / K
+                * 0.1f; //static_cast<float>(i % 13 + 1); // values from 1 to 13
+    }
+    for (int i = 0; i < (int)weights_data.size(); ++i) {
+        //int group = i / (K * N);
+        weights_data[i] = 1; //i / N * 0.1f; // * (group + 1);
+                //= static_cast<float>((i % 7) - 3.f); // values from -3 to 3
+    }
+
+#else
+    fill_random(src_data, src_md);
+    fill_random(weights_data, weights_md);
+#endif
+    write_to_dnnl_memory(src_data.data(), src_mem, eng, strm);
+    write_to_dnnl_memory(weights_data.data(), weights_mem, eng, strm);
+    write_to_dnnl_memory(M_group_offsets.data(), src_mem, eng, strm, 1);
+    write_to_dnnl_memory(M_group_offsets.data(), dst_mem, eng, strm, 1);
+    std::vector<memory> src_groups;
+    std::vector<memory> weights_groups;
+    std::vector<memory> dst_groups;
+    std::vector<matmul::primitive_desc> matmul_pds;
+    std::vector<matmul> matmul_prims;
+
+    //print_mem(src_mem, "src");
+    //print_mem(weights_mem, "weights");
+    //print_mem(dst_mem, "dst");
+
+    for (int g = 0; g < num_groups; ++g) {
+        src_groups.push_back(get_group_mem<float16_t>(src_mem, g));
+        dst_groups.push_back(get_group_mem<float>(dst_mem, g));
+        const memory::dims w_sz = {weights_sz[1], weights_sz[2]};
+        weights_groups.push_back(
+                memory(memory::desc(w_sz, wdt, memory::format_tag::ab), eng));
+        // Copy weights for group
+        size_t weights_group_offset = g * K * N;
+        write_to_dnnl_memory(&weights_data[weights_group_offset],
+                weights_groups[g], eng, strm);
+    }
+
     auto matmul_pd = matmul::primitive_desc(eng, src_md, weights_md, dst_md);
     auto matmul_prim = matmul(matmul_pd);
+
+    std::vector<matmul::primitive_desc> pds;
+    std::vector<matmul> prims;
+    for (int g = 0; g < num_groups; ++g) {
+        auto pd = matmul::primitive_desc(eng, src_groups[g].get_desc(),
+                weights_groups[g].get_desc(), dst_groups[g].get_desc());
+        pds.push_back(pd);
+        prims.push_back(matmul(pd));
+        unordered_map<int, memory> args;
+        args.insert({DNNL_ARG_SRC, src_groups[g]});
+        args.insert({DNNL_ARG_WEIGHTS, weights_groups[g]});
+        args.insert({DNNL_ARG_DST, dst_groups[g]});
+        prims[g].execute(strm, args);
+        strm.wait();
+    }
 
     unordered_map<int, memory> args;
     args.insert({DNNL_ARG_SRC, src_mem});
@@ -51,4 +120,49 @@ TEST_F(GroupedGEMM, BasicCreation) {
     args.insert({DNNL_ARG_DST, dst_mem});
     matmul_prim.execute(strm, args);
     strm.wait();
+
+    //print_mem(dst_mem, "dst");
+    for (int g = 0; g < num_groups; ++g) {
+        //print_mem(dst_groups[g], "gold");
+        auto dst_test = get_group_mem<float>(dst_mem, g);
+        auto dst_gold = dst_groups[g];
+        float *mapped_ptr_test = (float *)dst_test.map_data();
+        float *mapped_ptr_gold = (float *)dst_gold.map_data();
+        strm.wait();
+        for (int i = 0; i < M_group_sizes[g] * N; ++i) {
+            float diff = std::abs(mapped_ptr_test[i] - mapped_ptr_gold[i]);
+            ASSERT_NEAR(mapped_ptr_test[i], mapped_ptr_gold[i], 0.03f)
+                    << "Mismatch at group" << g << " index " << i
+                    << "test=" << mapped_ptr_test[i]
+                    << ", gold=" << mapped_ptr_gold[i] << ", diff=" << diff;
+        }
+    }
+
+    int iterations = 10;
+    strm.wait();
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int iter = 0; iter < iterations; ++iter) {
+        for (int g = 0; g < num_groups; ++g) {
+            unordered_map<int, memory> args2;
+            args2.insert({DNNL_ARG_SRC, src_groups[g]});
+            args2.insert({DNNL_ARG_WEIGHTS, weights_groups[g]});
+            args2.insert({DNNL_ARG_DST, dst_groups[g]});
+            prims[g].execute(strm, args2);
+        }
+    }
+    strm.wait();
+    auto end = std::chrono::high_resolution_clock::now();
+    double elapsed
+            = std::chrono::duration<double, std::milli>(end - start).count();
+    printf("Ref Grouped GEMM time: %.3f ms\n", elapsed / iterations);
+
+    strm.wait();
+    start = std::chrono::high_resolution_clock::now();
+    for (int iter = 0; iter < iterations; ++iter) {
+        matmul_prim.execute(strm, args);
+    }
+    strm.wait();
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = std::chrono::duration<double, std::milli>(end - start).count();
+    printf("DNNL Grouped GEMM time: %.3f ms\n", elapsed / iterations);
 }

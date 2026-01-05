3cd76ca3cb12dffdd1101bf43d1302bf27e7c105
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2026-01-05 11:24:35 -0800

    xe: matmul: ref: improve coverage

--- src/gpu/intel/matmul/ref.hpp
+++ src/gpu/intel/matmul/ref.hpp
@@ -81,27 +81,19 @@ struct ref_t : public primitive_t {
             const bool is_f64
                     = utils::everyone_is(f64, src_dt_, wei_dt_, dst_dt_);
             const bool is_f32 = src_dt_ == f32
-                    && utils::one_of(wei_dt_, f32, s8, u8, s4, u4)
-                    && utils::one_of(dst_dt_, f32, f16, bf16);
+                    && utils::one_of(wei_dt_, f32, s8, u8, s4, u4);
             const bool is_f16 = src_dt_ == f16
-                    && utils::one_of(wei_dt_, f16, s8, u8, s4, u4)
-                    && utils::one_of(dst_dt_, u8, s8, f16, bf16, f32);
+                    && utils::one_of(wei_dt_, f16, s8, u8, s4, u4);
             const bool is_bf16 = src_dt_ == bf16
-                    && utils::one_of(wei_dt_, bf16, s8, u8, s4, u4)
-                    && utils::one_of(dst_dt_, u8, s8, f16, bf16, f32);
+                    && utils::one_of(wei_dt_, bf16, s8, u8, s4, u4);
 
-            const bool is_f8
-                    = (utils::one_of(src_dt_, f8_e5m2, f8_e4m3)
-                              || utils::one_of(wei_dt_, f8_e5m2, f8_e4m3))
-                    && utils::one_of(dst_dt_, f32, bf16, f16, src_dt_);
+            const bool is_f8 = utils::one_of(src_dt_, f8_e5m2, f8_e4m3)
+                    || utils::one_of(wei_dt_, f8_e5m2, f8_e4m3);
             const bool is_f4
-                    = ((utils::one_of(src_dt_, f4_e2m1, f4_e3m0, f32, bf16, f16)
-                               || utils::one_of(wei_dt_, f4_e2m1, f4_e3m0))
-                            && utils::one_of(dst_dt_, f32, bf16, f16, f4_e3m0,
-                                    f4_e2m1, src_dt_));
+                    = utils::one_of(src_dt_, f4_e2m1, f4_e3m0, f32, bf16, f16)
+                    || utils::one_of(wei_dt_, f4_e2m1, f4_e3m0);
             const bool is_int8 = utils::one_of(src_dt_, u8, s8)
-                    && utils::one_of(wei_dt_, u8, s8, u4, s4)
-                    && utils::one_of(dst_dt_, f32, s8, u8, s32, f16, bf16);
+                    && utils::one_of(wei_dt_, u8, s8, u4, s4);
             VDISPATCH_MATMUL(
                     (is_int8
                             || ((is_f32 || is_f64 || is_f16 || is_f8 || is_f4

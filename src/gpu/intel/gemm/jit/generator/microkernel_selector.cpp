c749b3cfe39a62b2830c5bbe715d2dbfbedd9e80
Simon Ewing <simon.ewing@intel.com>
2026-01-07 14:56:56 -0800

    xe: gemm: improve ukernel verbosity

--- src/gpu/intel/gemm/jit/generator/microkernel_selector.cpp
+++ src/gpu/intel/gemm/jit/generator/microkernel_selector.cpp
@@ -270,6 +270,25 @@ Package selectGEMM(const GEMMOptions &options, HWInformation hwInfo, SizeParams 
 
     strategy.preflight(hw, problem);
 
+    if (getVerbose(gemmstone::GEMMVerbose::DebugInfo) >= 10) {
+        if (entry)
+            std::cout << "Selected microkernel catalog entry: " << entry->str() << std::endl;
+        else
+            std::cout << "Microkernel generated heuristically" << std::endl;
+    }
+    if (getVerbose(gemmstone::GEMMVerbose::DebugInfo) >= 2) {
+        std::string result = problem.toString();
+        result.append(" ");
+        result.append(std::to_string(strategy.unroll[LoopM]));
+        result.append(" ");
+        result.append(std::to_string(strategy.unroll[LoopN]));
+        result.append(" ");
+        result.append(problem.scalarsToString());
+        result.append(" ");
+        result.append(unparseStrategy(hw, problem, strategy));
+        std::cout << "Actual kernel: " << result << std::endl;
+    }
+
     /* Set up arguments for microkernel */
     InterfaceHandler interface(hw);
 

15da414f169f2f4af084bd69256bfae8dff6535b
Xuxin, Zeng <xuxin.zeng@intel.com>
2026-02-05 10:18:27 -0800

    cpu: x64: matmul: fix initialization of use_buffer_c in amx blocking

--- src/cpu/x64/matmul/amx_blocking_heuristics.cpp
+++ src/cpu/x64/matmul/amx_blocking_heuristics.cpp
@@ -1054,7 +1054,8 @@ bool matmul_amx_blocking_params_macro_t::set_blocking_parameters(
     current_lda_ = get_actual_lda();
 
     // Need a temp C buffer if a BRGEMM creates partial results
-    need_buf_c_ = (nthr_k_ != 1) || (k_blk_ != K && acc_dt != dst_dt);
+    need_buf_c_
+            = (nthr_k_ != 1) || (k_blk_ != K && (with_sum || acc_dt != dst_dt));
 
     efficiency_score_ = calculate_blocking_scores();
 

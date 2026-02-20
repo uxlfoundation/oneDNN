fde9e4de503d33f9dfa296013b2804a8be803af3
Simonov, Alexander <alexander.simonov@intel.com>
2026-02-20 05:37:04 -0800

    matmul: do not use div_up(a,b) with a<0
    This change should be reconsidered.

--- src/cpu/x64/matmul/amx_blocking_heuristics.cpp
+++ src/cpu/x64/matmul/amx_blocking_heuristics.cpp
@@ -1187,8 +1187,11 @@ void matmul_amx_blocking_params_micro_t::find_best_blocking(
             float cur_score = current_blocking.get_blocking_scores();
             float bst_score = best_blocking.get_blocking_scores();
 
-            int m_chunks = div_up(bgmmc.M, m_blk * m_ch_sz);
-            int n_chunks = div_up(bgmmc.N, n_blk * n_ch_sz);
+            // TODO: runtime dimensions should be handled properly
+            int m_chunks
+                    = div_up(nstl::max(dim_t(0), bgmmc.M), m_blk * m_ch_sz);
+            int n_chunks
+                    = div_up(nstl::max(dim_t(0), bgmmc.N), n_blk * n_ch_sz);
             int work_amount = bgmmc.batch * m_chunks * n_chunks;
 
             bool skip_config = work_amount < nthr_bmn * 3

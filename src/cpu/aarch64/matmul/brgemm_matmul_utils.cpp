5abfbf022b5e3412e6776c690dc55f2e809d4593
Simonov, Alexander <alexander.simonov@intel.com>
2026-02-25 07:00:52 -0800

    cpu: matmul: do not use div_up(a,b) with a<0

--- src/cpu/aarch64/matmul/brgemm_matmul_utils.cpp
+++ src/cpu/aarch64/matmul/brgemm_matmul_utils.cpp
@@ -1161,11 +1161,14 @@ void init_aux_values(brgemm_matmul_conf_t &bgmmc,
     bgmmc.M_chunk_elems = bgmmc.M_blk * bgmmc.M_chunk_size;
     bgmmc.N_chunk_elems = bgmmc.N_blk * bgmmc.N_chunk_size;
     bgmmc.K_chunk_elems = bgmmc.K_blk * bgmmc.brgemm_batch_size;
-    bgmmc.M_chunks = div_up(bgmmc.M, bgmmc.M_chunk_elems);
-    bgmmc.N_chunks = div_up(bgmmc.N, bgmmc.N_chunk_elems);
-    bgmmc.K_chunks = div_up(bgmmc.K, bgmmc.K_chunk_elems);
-    bgmmc.num_M_blocks = div_up(bgmmc.M, bgmmc.M_blk);
-    bgmmc.num_N_blocks = div_up(bgmmc.N, bgmmc.N_blk);
+    bgmmc.M_chunks
+            = bgmmc.is_runtime_M ? 1 : div_up(bgmmc.M, bgmmc.M_chunk_elems);
+    bgmmc.N_chunks
+            = bgmmc.is_runtime_N ? 1 : div_up(bgmmc.N, bgmmc.N_chunk_elems);
+    bgmmc.K_chunks
+            = bgmmc.is_runtime_K ? 1 : div_up(bgmmc.K, bgmmc.K_chunk_elems);
+    bgmmc.num_M_blocks = bgmmc.is_runtime_M ? 1 : div_up(bgmmc.M, bgmmc.M_blk);
+    bgmmc.num_N_blocks = bgmmc.is_runtime_N ? 1 : div_up(bgmmc.N, bgmmc.N_blk);
     const int last_chunck_batch_size
             = (nstl::max(bgmmc.K, bgmmc.K_blk)
                       - (bgmmc.K_chunks - 1) * bgmmc.K_chunk_elems)
@@ -1194,7 +1197,8 @@ void init_aux_values(brgemm_matmul_conf_t &bgmmc,
             = bgmmc.use_buffer_b ? bgmmc.wei_n_blk * bgmmc.N_chunk_size : 0;
     bgmmc.s8s8_comp_b_str = bgmmc.use_buffer_b
             ? 0
-            : div_up(bgmmc.N, bgmmc.wei_n_blk) * bgmmc.wei_n_blk;
+            : (bgmmc.is_runtime_N ? bgmmc.wei_n_blk
+                                  : rnd_up(bgmmc.N, bgmmc.wei_n_blk));
     bgmmc.s8s8_comp_n_str = bgmmc.wei_n_blk;
 
     bgmmc.A_ptr_shift_b = 0;

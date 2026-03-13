b5c10111f81b06707d5eb7444482ba88621f6710
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2026-03-13 14:15:47 -0700

    xe: eltwise: fix offset overflow

--- src/gpu/intel/eltwise/xe.hpp
+++ src/gpu/intel/eltwise/xe.hpp
@@ -50,9 +50,10 @@ struct xe_jit_params_t : public trivially_serializable_t<xe_jit_params_t> {
     alg_kind_t alg_kind;
     int work_group_size;
     int sub_group_size;
+    dim_t nelems;
     bool with_overflow;
     bool require_stateless_addressing;
-    uint8_t pad0[2] = {};
+    uint8_t pad0[6] = {};
 };
 
 struct xe_fwd_t : public primitive_t {

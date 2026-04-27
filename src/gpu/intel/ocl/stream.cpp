f37c137d05fa1c7ac6e2cd2c8eeeb0c1b2660647
Ankit Manerikar <ankit.v.manerikar@intel.com>
2026-04-27 15:00:02 -0700

    gpu: ocl: add async verbose profiling for opencl streams

--- src/gpu/intel/ocl/stream.cpp
+++ src/gpu/intel/ocl/stream.cpp
@@ -120,6 +120,87 @@ void stream_t::after_exec_hook() {
     if (is_profiling_enabled()) profiler_->stop_profiling();
 }
 
+status_t stream_t::run_verbose_profiler(
+        std::string &pd_info, double start_ms) const {
+
+    // utilize the verbose profiler only for profile_exec verbose levels.
+    if (!is_verbose_profiler_enabled()) return status::invalid_arguments;
+
+    // failsafe for primitive executions without any enqueued kernels
+    auto &deps = xpu::ocl::event_t::from(ctx().get_deps());
+    if (deps.size() < 1) {
+        double duration_ms = get_msec() - start_ms;
+        VPROF(start_ms, primitive, exec, VERBOSE_profile, pd_info.c_str(),
+                duration_ms);
+        return status::success;
+    }
+
+    // captured output event acts as the anchor to track primitive execution
+    // print profiling info asynchronously
+    cl_event out_evt = deps[0].get();
+
+    if (!profiler_->stamp()) {
+        VWARN(primitive, exec,
+                "%s, profiler error: failed to record events in context",
+                pd_info.c_str());
+        VPROF(start_ms, primitive, exec, VERBOSE_profile, pd_info.c_str(), 0.f);
+        return status::success;
+    }
+
+    auto *ocl_profiler
+            = utils::downcast<xpu::ocl::stream_profiler_t *>(profiler_.get());
+
+    // The verbose callback uses a snapshot of queued primitive events
+    // to compute execution timing in a thread-safe manner.
+    std::vector<cl_event> evt_snap;
+    CHECK(ocl_profiler->extract_primitive_events(evt_snap));
+
+    struct payload_t {
+        double start;
+        xpu::ocl::stream_profiler_t *prof;
+        std::string info_str;
+        std::vector<cl_event> evt_snap;
+    };
+
+    std::unique_ptr<payload_t> payload(new payload_t());
+    payload->prof = ocl_profiler;
+    payload->info_str = pd_info;
+    payload->start = start_ms;
+    payload->evt_snap = std::move(evt_snap);
+
+    // The prompt ensures the verbose headers are printed if they aren't
+    // already. Printing the headers asynchronously during the callback can
+    // result in access failures when printing engine-specific info.
+    verbose_printf(verbose_t::exec_profile, "\r");
+
+    cl_int err = xpu::ocl::clSetEventCallback(
+            out_evt, CL_COMPLETE, [](cl_event ev, cl_int, void *user) {
+        std::unique_ptr<payload_t> hold(static_cast<payload_t *>(user));
+        double duration_ms = 0.0;
+
+        if (hold->prof) {
+            hold->prof->get_aggregate_exec_timing(duration_ms, hold->evt_snap);
+        } else {
+            VWARN(primitive, exec, "%s, profiler error: profiler absent",
+                    hold->info_str.c_str());
+        }
+        VPROF(hold->start, primitive, exec, VERBOSE_profile,
+                hold->info_str.c_str(), duration_ms);
+    }, payload.get());
+
+    if (err != CL_SUCCESS) {
+        VWARN(primitive, exec,
+                "%s, profiler error: failed to set event callback",
+                pd_info.c_str());
+        VPROF(start_ms, primitive, exec, VERBOSE_profile, pd_info.c_str(), 0.f);
+        return status::success;
+    }
+
+    (void)payload.release();
+
+    return status::success;
+}
+
 status_t stream_t::copy(const memory_storage_t &src,
         const memory_storage_t &dst, size_t size, const xpu::event_t &deps,
         xpu::event_t &out_dep) {

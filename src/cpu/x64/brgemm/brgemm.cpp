6e12cd23fe9607a10273622e67d6ba9ada0e3888
Simonov, Alexander <alexander.simonov@intel.com>
2026-06-02 02:51:11 -0700

    cpu: x64: brgemm: rollback unnecessary changes

--- src/cpu/x64/brgemm/brgemm.cpp
+++ src/cpu/x64/brgemm/brgemm.cpp
@@ -375,7 +375,7 @@ status_t brgemm_desc_set_postops(brgemm_desc_t *brg,
     if (!IMPLICATION(one_of(data_type::f8_e5m2, dt_bias, dt_d)
                         || one_of(data_type::f8_e4m3, dt_bias, dt_d),
                 utils::one_of(
-                        true, mayiuse(avx512_core_amx), mayiuse(avx10_2))))
+                        true, mayiuse(avx512_core_amx_fp16), mayiuse(avx10_2))))
         return status::unimplemented;
     // check that combination of data types is allowed
     if ((brg->dt_a == data_type::u8 && brg->dt_b == data_type::s8)

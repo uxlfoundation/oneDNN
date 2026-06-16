0c11509b985bcb9c1f52e6a33dad3bcfcc2459de
Maria Zhukova <maria.zhukova@intel.com>
2026-06-15 17:20:29 -0700

    gpu: remove f4_e3m0 as dt in grouped mm

--- src/gpu/intel/matmul/grouped_micro_gemm.cpp
+++ src/gpu/intel/matmul/grouped_micro_gemm.cpp
@@ -352,10 +352,10 @@ status_t grouped_micro_gemm_t::pd_t::init(impl::engine_t *engine) {
 
     // only supported dt for now
     VDISPATCH_MATMUL(utils::one_of(src_dt, f32, f16, bf16, u8, s8, s4, u4,
-                             f8_e5m2, f8_e4m3, f4_e2m1, f4_e3m0),
+                             f8_e5m2, f8_e4m3, f4_e2m1),
             VERBOSE_UNSUPPORTED_DT_CFG);
     VDISPATCH_MATMUL(utils::one_of(wei_dt, f32, f16, bf16, u8, s8, s4, u4,
-                             f8_e5m2, f8_e4m3, f4_e2m1, f4_e3m0),
+                             f8_e5m2, f8_e4m3, f4_e2m1),
             VERBOSE_UNSUPPORTED_DT_CFG);
     VDISPATCH_MATMUL(
             utils::one_of(dst_dt, f32, f16, bf16), VERBOSE_UNSUPPORTED_DT_CFG);
@@ -404,7 +404,7 @@ status_t grouped_micro_gemm_t::pd_t::init(impl::engine_t *engine) {
     // Check for supported quantization schemes
     if (src_quant_.with_scale()) {
         VDISPATCH_MATMUL(utils::one_of(src_quant_.scale_dt(), f32, f16, bf16,
-                                 f8_e5m2, f8_e4m3, e8m0, f4_e2m1, f4_e3m0),
+                                 f8_e5m2, f8_e4m3, e8m0, f4_e2m1),
                 VERBOSE_UNSUPPORTED_SCALES_CFG ": src scales dt(%s)",
                 dnnl_dt2str(src_quant_.scale_dt()));
     }
@@ -435,7 +435,7 @@ status_t grouped_micro_gemm_t::pd_t::init(impl::engine_t *engine) {
         VDISPATCH_MATMUL(
                 utils::one_of(wei_mask, 7, 5), VERBOSE_UNSUPPORTED_SCALES_CFG);
         VDISPATCH_MATMUL(utils::one_of(wei_quant_.scale_dt(), f32, f16, bf16,
-                                 f8_e5m2, f8_e4m3, e8m0, f4_e2m1, f4_e3m0),
+                                 f8_e5m2, f8_e4m3, e8m0, f4_e2m1),
                 VERBOSE_UNSUPPORTED_SCALES_CFG ": wei scales dt(%s)",
                 dnnl_dt2str(wei_quant_.scale_dt()));
     }

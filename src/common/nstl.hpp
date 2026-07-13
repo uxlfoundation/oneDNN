679777a8f3b10943568bb7a544b4363d25575d7b
Maria Zhukova <maria.zhukova@intel.com>
2026-07-13 10:51:06 -0700

    common: add u3 data type and helpers

--- src/common/nstl.hpp
+++ src/common/nstl.hpp
@@ -29,6 +29,7 @@
 #include "float16.hpp"
 #include "float4.hpp"
 #include "float8.hpp"
+#include "int3.hpp"
 #include "int4.hpp"
 #include "internal_defs.hpp"
 #include "z_magic.hpp"
@@ -237,6 +238,17 @@ struct numeric_limits<int4_t> {
     static constexpr int4_t epsilon() { return int4_t(0); }
 };
 
+template <>
+struct numeric_limits<uint3_t> {
+    static constexpr uint3_t lowest() { return uint3_t(0); }
+    static constexpr uint3_t min() { return lowest(); }
+    static constexpr uint3_t max() { return uint3_t(7); }
+
+    static constexpr int digits = 3;
+
+    static constexpr uint3_t epsilon() { return uint3_t(0); }
+};
+
 template <typename T>
 struct is_integral { // NOLINT(readability-identifier-naming)
     static constexpr bool value = false;
@@ -263,6 +275,10 @@ struct is_integral<int4_t> {
 };
 template <>
 struct is_integral<uint4_t> {
+    static constexpr bool value = true;
+};
+template <>
+struct is_integral<uint3_t> {
     static constexpr bool value = true;
 };
 

8c80c319c947d00b26ab5f3a7920a43ef66cf254
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2026-07-13 23:15:02 -0700

    src: common: nstl: remove outdated types

--- src/common/nstl.hpp
+++ src/common/nstl.hpp
@@ -24,8 +24,6 @@
 #include <array>
 #include <cassert>
 #include <cstdlib>
-#include <map>
-#include <vector>
 
 #include "bfloat16.hpp"
 #include "float16.hpp"
@@ -282,92 +280,6 @@ struct is_integral<int4_t> {
 template <>
 struct is_integral<uint4_t> {
     static constexpr bool value = true;
-};
-
-template <typename T, typename U>
-struct is_same { // NOLINT(readability-identifier-naming)
-    static constexpr bool value = false;
-};
-template <typename T>
-struct is_same<T, T> {
-    static constexpr bool value = true;
-};
-
-// Rationale: oneDNN needs container implementations that do not generate
-// dependencies on C++ run-time libraries.
-//
-// Implementation philosophy: caller is responsible to check if the operation
-// is valid. The only functions that have to return status are those that
-// depend on memory allocation or similar operations.
-//
-// This means that e.g. an operator [] does not have to check for boundaries.
-// The caller should have checked the boundaries. If it did not we crash and
-// burn: this is a bug in oneDNN and throwing an exception would not have been
-// recoverable.
-//
-// On the other hand, insert() or resize() or a similar operation needs to
-// return a status because the outcome depends on factors external to the
-// caller. The situation is probably also not recoverable also, but oneDNN
-// needs to be nice and report "out of memory" to the users.
-
-enum nstl_status_t { success = 0, out_of_memory };
-
-template <typename T>
-class vector { // NOLINT(readability-identifier-naming)
-private:
-    std::vector<T> _impl;
-
-public:
-    using iterator = typename std::vector<T>::iterator;
-    using const_iterator = typename std::vector<T>::const_iterator;
-    using size_type = typename std::vector<T>::size_type;
-    vector() = default;
-    vector(size_type n) : _impl(n) {}
-    vector(size_type n, const T &value) : _impl(n, value) {}
-    template <typename input_iterator>
-    vector(input_iterator first, input_iterator last) : _impl(first, last) {}
-    ~vector() = default;
-    size_type size() const { return _impl.size(); }
-    T &operator[](size_type i) { return _impl[i]; }
-    const T &operator[](size_type i) const { return _impl[i]; }
-    iterator begin() { return _impl.begin(); }
-    const_iterator begin() const { return _impl.begin(); }
-    iterator end() { return _impl.end(); }
-    const_iterator end() const { return _impl.end(); }
-    template <typename input_iterator>
-    nstl_status_t insert(
-            iterator pos, input_iterator begin, input_iterator end) {
-        _impl.insert(pos, begin, end);
-        return success;
-    }
-    void clear() { _impl.clear(); }
-    void push_back(const T &t) { _impl.push_back(t); }
-    void resize(size_type count) { _impl.resize(count); }
-    void reserve(size_type count) { _impl.reserve(count); }
-};
-
-template <typename Key, typename T>
-class map { // NOLINT(readability-identifier-naming)
-private:
-    std::map<Key, T> _impl;
-
-public:
-    using iterator = typename std::map<Key, T>::iterator;
-    using const_iterator = typename std::map<Key, T>::const_iterator;
-    using size_type = typename std::map<Key, T>::size_type;
-    map() = default;
-    ~map() = default;
-    size_type size() const { return _impl.size(); }
-    T &operator[](const Key &k) { return _impl[k]; }
-    const T &operator[](const Key &k) const { return _impl[k]; }
-    iterator begin() { return _impl.begin(); }
-    const_iterator begin() const { return _impl.begin(); }
-    iterator end() { return _impl.end(); }
-    const_iterator end() const { return _impl.end(); }
-    template <typename input_iterator>
-    void clear() {
-        _impl.clear();
-    }
 };
 
 // Compile-time sequence of indices (part of C++14)

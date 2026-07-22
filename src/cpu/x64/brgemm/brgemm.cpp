125ddc4a3fa3b69bef0346e50cf92dd37cc68187
Alexey Makarevich <alexey.makarevich@intel.com>
2026-07-22 14:25:44 -0700

    cpu: x64: brgemm: drop ptr_wei_scales from brgemm_kernel_execute

--- src/cpu/x64/brgemm/brgemm.cpp
+++ src/cpu/x64/brgemm/brgemm.cpp
@@ -86,8 +86,7 @@ void brgemm_desc_t::cleanup_dst_md() {
 
 void brgemm_kernel_execute(const brgemm_kernel_t *brg_kernel, dim_t bs,
         const brgemm_batch_element_t *batch, void *ptr_C, void *scratch,
-        const brgemm_dynamic_values_t *dynamic_values,
-        const void *ptr_wei_scales) {
+        const brgemm_dynamic_values_t *dynamic_values) {
     brgemm_kernel_params_t brgemm_p;
 
     brgemm_p.batch = batch;
@@ -101,7 +100,6 @@ void brgemm_kernel_execute(const brgemm_kernel_t *brg_kernel, dim_t bs,
     brgemm_p.do_apply_comp = 0;
     brgemm_p.skip_accm = 0;
     brgemm_p.BS = bs;
-    brgemm_p.ptr_wei_scales = ptr_wei_scales;
     if (dynamic_values) {
         brgemm_p.dynamic_LDA = dynamic_values->dynamic_LDA;
         brgemm_p.dynamic_LDB = dynamic_values->dynamic_LDB;
@@ -117,8 +115,7 @@ void brgemm_kernel_execute(const brgemm_kernel_t *brg_kernel, dim_t bs,
 void brgemm_kernel_execute(const brgemm_kernel_t *brg_kernel, dim_t bs,
         const void *addr_A, const void *addr_B,
         const brgemm_batch_element_t *batch, void *ptr_C, void *scratch,
-        const brgemm_dynamic_values_t *dynamic_values,
-        const void *ptr_wei_scales) {
+        const brgemm_dynamic_values_t *dynamic_values) {
     brgemm_kernel_params_t brgemm_p;
 
     brgemm_p.batch = batch;
@@ -132,7 +129,6 @@ void brgemm_kernel_execute(const brgemm_kernel_t *brg_kernel, dim_t bs,
     brgemm_p.do_apply_comp = 0;
     brgemm_p.skip_accm = 0;
     brgemm_p.BS = bs;
-    brgemm_p.ptr_wei_scales = ptr_wei_scales;
     if (dynamic_values) {
         brgemm_p.dynamic_LDA = dynamic_values->dynamic_LDA;
         brgemm_p.dynamic_LDB = dynamic_values->dynamic_LDB;

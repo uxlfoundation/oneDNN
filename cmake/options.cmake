40f60b6803ca5f6821307655997efef7aa30a9f8
Maria Zhukova <maria.zhukova@intel.com>
2026-07-30 08:54:33 -0700

    build: make contiguous u3 layout default

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -205,7 +205,7 @@ onednn_option(EXPERIMENTAL_ENABLE_SYCL_PROFILING_TAG OFF
     for verbose profiling. When disabled, verbose profiling requires a
     profiling-enabled SYCL queue to function.")
 
-onednn_option(TEMPORARY_U3_CONTIGUOUS_LAYOUT OFF
+onednn_option(TEMPORARY_U3_CONTIGUOUS_LAYOUT ON
     "Uses the contiguous u3 bit-packing layout instead of the default OV
     transposed layout")
 

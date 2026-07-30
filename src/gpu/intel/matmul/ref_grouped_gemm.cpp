ishita-shreya/ishita-oneDNN-Zen

gpu: reroute grouped matmul ref to generic framework

src/gpu/intel/matmul/ref_grouped_gemm.cpp
@@ -90,9 +90,6 @@ status_t ref_grouped_t::execute(const exec_ctx_t &ctx) const {
 
     const bool with_bias = pd()->with_bias();
     const auto &attr_scales = pd()->attr()->scales_;
-    const bool with_src_scales = !attr_scales.has_default_values(DNNL_ARG_SRC);
-    const bool with_wei_scales
-            = !attr_scales.has_default_values(DNNL_ARG_WEIGHTS);
 
     compute::kernel_arg_list_t arg_list;
     int arg_idx = 0;
@@ -121,38 +118,16 @@ status_t ref_grouped_t::execute(const exec_ctx_t &ctx) const {
     arg_list.set(arg_idx++, wei_group_stride);
     arg_list.set(arg_idx++, dst_group_stride);
     if (with_bias) arg_list.set(arg_idx++, CTX_IN_STORAGE(DNNL_ARG_BIAS));
-    if (with_src_scales)
+    if (!attr_scales.has_default_values(DNNL_ARG_SRC))
         arg_list.set(
                 arg_idx++, CTX_IN_STORAGE(DNNL_ARG_ATTR_SCALES | DNNL_ARG_SRC));
-    if (with_wei_scales)
+    if (!attr_scales.has_default_values(DNNL_ARG_WEIGHTS))
         arg_list.set(arg_idx++,
                 CTX_IN_STORAGE(DNNL_ARG_ATTR_SCALES | DNNL_ARG_WEIGHTS));
+
     // Post-ops apply to the 2Dx3D pattern only (grouped dst)
-    if (pd()->with_post_op_) {
-        const auto &po_chain = pd()->po_chain_;
-        const memory_storage_t *grouped_scale
-                = &memory_storage_t::empty_storage();
-        const memory_storage_t *dense_scale
-                = &memory_storage_t::empty_storage();
-        const memory_storage_t *nvfp4_scale
-                = &memory_storage_t::empty_storage();
-        for (int i = 0; i < pd()->attr()->post_ops_.len(); ++i) {
-            auto &e = pd()->attr()->post_ops_.entry_[i];
-            if (!e.is_binary()) continue;
-            const int po_arg
-                    = DNNL_ARG_ATTR_MULTIPLE_POST_OP(i) | DNNL_ARG_SRC_1;
-            if (po_chain[i] == po_kind_t::binary_grouped_scale) {
-                grouped_scale = &CTX_IN_STORAGE(po_arg, 0);
-            } else if (po_chain[i] == po_kind_t::binary_dense_scale) {
-                dense_scale = &CTX_IN_STORAGE(po_arg, 0);
-            } else if (po_chain[i] == po_kind_t::binary_nvfp4_scale) {
-                nvfp4_scale = &CTX_IN_STORAGE(po_arg, 0);
-            }
-        }
-        arg_list.set(arg_idx++, *grouped_scale);
-        arg_list.set(arg_idx++, *dense_scale);
-        arg_list.set(arg_idx++, *nvfp4_scale);
-    }
+    arg_idx = append_post_ops_to_arg_list(
+            ctx, arg_list, arg_idx, pd()->generic_po_, pd()->group_po_dst_md_);
 
     // Simple 3D dispatch for ref impl clarity
     compute::range_t gws

uxlfoundation/oneDNN

src/gpu/intel/matmul/ref_grouped_gemm.cpp
@@ -71,31 +71,10 @@ status_t ref_grouped_t::execute_ref(const exec_ctx_t &ctx) const {
                 = CTX_IN_STORAGE(DNNL_ARG_ATTR_SCALES | DNNL_ARG_WEIGHTS);
         arg_list.set(next_arg++, wei_scales);
     }
-    if (pd()->with_post_op_) {
-        const auto &po_chain = pd()->po_chain_;
-        const memory_storage_t *grouped_scale
-                = &memory_storage_t::empty_storage();
-        const memory_storage_t *dense_scale
-                = &memory_storage_t::empty_storage();
-        const memory_storage_t *nvfp4_scale
-                = &memory_storage_t::empty_storage();
-        for (int i = 0; i < pd()->attr()->post_ops_.len(); ++i) {
-            auto &e = pd()->attr()->post_ops_.entry_[i];
-            if (!e.is_binary()) continue;
-            const int po_arg
-                    = DNNL_ARG_ATTR_MULTIPLE_POST_OP(i) | DNNL_ARG_SRC_1;
-            if (po_chain[i] == po_kind_t::binary_grouped_scale) {
-                grouped_scale = &CTX_IN_STORAGE(po_arg, 0);
-            } else if (po_chain[i] == po_kind_t::binary_dense_scale) {
-                dense_scale = &CTX_IN_STORAGE(po_arg, 0);
-            } else if (po_chain[i] == po_kind_t::binary_nvfp4_scale) {
-                nvfp4_scale = &CTX_IN_STORAGE(po_arg, 0);
-            }
-        }
-        arg_list.set(next_arg++, *grouped_scale);
-        arg_list.set(next_arg++, *dense_scale);
-        arg_list.set(next_arg++, *nvfp4_scale);
-    }
+    // Post-ops apply to the 2Dx3D pattern only (grouped dst)
+    next_arg = append_post_ops_to_arg_list(
+            ctx, arg_list, next_arg, pd()->generic_po_, pd()->group_po_dst_md_);
+
     // Simple 3D dispatch for ref impl clarity
     compute::range_t gws
             = {(size_t)group_count, (size_t)total_tokens, (size_t)N};
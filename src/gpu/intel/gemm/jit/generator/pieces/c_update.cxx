510b2e99669e6e9bc9a3781031d6f2a2389b29b7
Youssif, Daniel <daniel.youssif@intel.com>
2026-07-30 17:01:30 -0700

    T

--- src/gpu/intel/gemm/jit/generator/pieces/c_update.cxx
+++ src/gpu/intel/gemm/jit/generator/pieces/c_update.cxx
@@ -2205,7 +2205,7 @@ void Generator<hw>::gemmAccessSums(COperation op, const GEMMProblem &problem, co
 
     auto CO = problem.CO;
     auto CO_strategy = strategy.CO;
- //   CO_strategy.base = A64_A32S;
+   // CO_strategy.base =  A64_A32S;
     std::vector<GRFRange> CO_addrs;
     std::vector<MaskAssignment> masks;
     GRFMultirange CO_regs;

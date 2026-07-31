70efe7c64120305c80b40a543e4cea42e1dcce58
Maria Zhukova <maria.zhukova@intel.com>
2026-07-31 15:00:55 -0700

    cpu: update load_int_value for u3 data type

--- src/cpu/ref_io_helper.hpp
+++ src/cpu/ref_io_helper.hpp
@@ -62,6 +62,10 @@ inline int load_int_value(data_type_t dt, const void *ptr, dim_t idx) {
             uint2_t val(nibble_quartet.get(idx % 4));
             return static_cast<int>(val);
         }
+        case u3: {
+            uint3_t val(uint3_unpack(static_cast<const uint8_t *>(ptr), idx));
+            return static_cast<int>(val);
+        }
         default: assert(!"bad data_type");
     }
 

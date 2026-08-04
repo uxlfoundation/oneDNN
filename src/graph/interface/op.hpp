58549a26d7298dc682005009d93a51fbac0682e9
Lv, Tao A <tao.a.lv@intel.com>
2026-08-04 00:45:34 -0700

    api: graph: add mask attribute to quantization ops

--- src/graph/interface/op.hpp
+++ src/graph/interface/op.hpp
@@ -328,6 +328,7 @@ public:
             CASE(axis);
             CASE(begin_norm_axis);
             CASE(groups);
+            CASE(mask);
             CASE(axes);
             CASE(dilations);
             CASE(weights_shape);

30f7c4f947b71a585a4bd248b957659840d7dd8b
Vadim Pirogov <vadim.o.pirogov@intel.com>
2026-08-27 13:28:57 -0700

    gtests: extended test_env_vars_* to cover verbose mode

--- tests/gtests/internals/test_env_vars_dnnl.cpp
+++ tests/gtests/internals/test_env_vars_dnnl.cpp
@@ -153,8 +153,21 @@ TEST(dnnl_default_fpmath_mode_env_var_test, TestEnvVars) {
     EXPECT_EQ(func_got_val, dnnl_fpmath_mode_strict);
 }
 
-// There's no a separate test for VERBOSE variable as there's no programmable
-// public API to identify if it was set through env var or not.
-// Same situation with the rest of variables.
+TEST(dnnl_verbose_env_var_test, TestEnvVars) {
+    // Variables with "ONEDNN_" prefix have higher precedence, so unset a
+    // potentially set "ONEDNN_VERBOSE".
+    custom_unsetenv("ONEDNN_VERBOSE");
+    custom_setenv("DNNL_VERBOSE", "profile", 1);
+#if !defined(DISABLE_VERBOSE)
+    EXPECT_TRUE(verbose_profiling_enabled());
+#endif
+
+    // The setting function takes precedence over the environment variable.
+    EXPECT_EQ(set_verbose(0), status::success);
+    EXPECT_FALSE(verbose_profiling_enabled());
+}
+
+// The rest of the variables have no programmable public API to identify if
+// they were set through env var or not, so they are not tested here.
 
 } // namespace dnnl

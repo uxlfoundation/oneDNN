d96677715be3e2c1a41da582f44155b4b70bb94b
Umar Arshad <umar.arshad@intel.com>
2026-08-28 10:07:02 -0700

    xe: ggemm: Use int Tc for <int>X<int> matmul cases

--- src/gpu/intel/matmul/grouped_post_ops_gen.cpp
+++ src/gpu/intel/matmul/grouped_post_ops_gen.cpp
@@ -176,7 +176,7 @@ static std::string to_ocl_float(float v) {
 std::string generate_post_ops_microgemm_header(
         const primitive_attr_t &attr, const po_kind_t *po_chain) {
     std::string s = R"(
-inline void apply_post_ops_chain(ugemm_grouped_c_type *c_tile, long n, long m, long lddst,
+inline void apply_post_ops_chain(c_tile_type_float *c_tile, long n, long m, long lddst,
     off_t sg_i0, off_t sg_j0, off_t src_offset, off_t batch,
     const global binary_scale_grouped_tile_data_t *grouped_scale,
     const global binary_scale_dense_tile_data_t *dense_scale,

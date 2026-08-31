658629f7c5017cb400058fa7b3e69c06eaff445d
Youssif, Daniel <daniel.youssif@intel.com>
2026-08-31 06:38:48 -0700

    rebase

--- src/cpu/matmul/ref_matmul.hpp
+++ src/cpu/matmul/ref_matmul.hpp
@@ -54,6 +54,7 @@ struct ref_matmul_t : public primitive_t {
             VDISPATCH_MATMUL(utils::one_of(src_type, f32, bf16, f16, f8_e5m2,
                                      f8_e4m3, f4_e2m1),
                     VERBOSE_UNSUPPORTED_DT);
+
             VDISPATCH_MATMUL(utils::one_of(wei_type, f32, bf16, f16, f8_e5m2,
                                      f8_e4m3, f4_e2m1, u8, s8, u4, s4, u3),
                     VERBOSE_UNSUPPORTED_DT);

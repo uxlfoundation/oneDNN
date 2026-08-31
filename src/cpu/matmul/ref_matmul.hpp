54a5c3b9f419031c22954586d9e195396ad0855b
Youssif, Daniel <daniel.youssif@intel.com>
2026-08-31 06:38:48 -0700

    rebase

--- src/cpu/matmul/ref_matmul.hpp
+++ src/cpu/matmul/ref_matmul.hpp
@@ -56,7 +56,7 @@ struct ref_matmul_t : public primitive_t {
                     VERBOSE_UNSUPPORTED_DT);
             VDISPATCH_MATMUL(
                     utils::one_of(wei_type, f32, bf16, f16, f8_e5m2, f8_e4m3,
-                            f4_e2m1, f4_e3m0, u8, s8, u4, s4, u3),
+                            f4_e2m1, u8, s8, u4, s4, u3),
                     VERBOSE_UNSUPPORTED_DT);
             VDISPATCH_MATMUL(utils::one_of(dst_type, f32, bf16, f16, f8_e5m2,
                                      f8_e4m3, f4_e2m1, u8, s8),
@@ -64,7 +64,7 @@ struct ref_matmul_t : public primitive_t {
             VDISPATCH_MATMUL(
                     (src_type == wei_type
                             || utils::one_of(wei_type, bf16, f16, f8_e5m2,
-                                    f8_e4m3, u8, s8, u4, s4, u3, f4_e3m0)),
+                                    f8_e4m3, u8, s8, u4, s4, u3)),
                     VERBOSE_UNSUPPORTED_DT);
             /* int weights decompression support */
             VDISPATCH_MATMUL(

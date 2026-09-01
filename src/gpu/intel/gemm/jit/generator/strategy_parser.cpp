f8d1d81ecdd8acbb3476090e58d6253c69e29de2
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2026-09-01 13:19:26 -0700

    cc_sb

--- src/gpu/intel/gemm/jit/generator/strategy_parser.cpp
+++ src/gpu/intel/gemm/jit/generator/strategy_parser.cpp
@@ -154,11 +154,7 @@ void getCaching(std::stringstream &s, HW hw, MatrixAddressingStrategy &astrategy
 
     if (!leaveDefault) {
         cachingR = CacheSettingsLSC::L1C_L3C;
-        cachingW = CacheSettingsLSC::L1WB_L3WB;
-        if (hw >= HW::XeHPC)
-            cachingW = CacheSettingsLSC::L1UC_L3WB;
-        if (hw >= HW::Xe3p)
-            cachingR = CacheSettingsLSC::L1C_L2C_L3C;
+        cachingW = CacheSettingsLSC::L1S_L3WB;
     }
 
     if (s.peek() == '{') {

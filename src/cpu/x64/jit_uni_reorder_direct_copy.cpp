b46903241502714b19bbeb427eab07ab00bb7182
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2026-09-10 15:29:43 -0700

    reorder: move internal infrastructure to the general path
    The change makes it using common primitive_desc abstraction and
    primitive_desc_iterator.

--- src/cpu/x64/jit_uni_reorder_direct_copy.cpp
+++ src/cpu/x64/jit_uni_reorder_direct_copy.cpp
@@ -240,20 +240,6 @@ private:
     const int emu_kmask_aux_idx_ = 2;
 };
 
-status_t jit_uni_reorder_direct_copy_t::pd_t::create(reorder_pd_t **reorder_pd,
-        const engine_t *engine, const primitive_attr_t *attr,
-        const engine_t *src_engine, const memory_desc_t *src_md,
-        const engine_t *dst_engine, const memory_desc_t *dst_md) {
-    auto desc = reorder_pd_t::create_desc(
-            src_md, dst_md, src_engine->kind(), dst_engine->kind());
-    auto _pd = make_unique_pd<pd_t>(&desc, attr, nullptr);
-    if (_pd == nullptr) return status::out_of_memory;
-
-    CHECK(_pd->init(engine, src_engine, dst_engine));
-
-    return safe_ptr_assign(*reorder_pd, _pd.release());
-}
-
 status_t jit_uni_reorder_direct_copy_t::pd_t::init(const engine_t *engine,
         const engine_t *src_engine, const engine_t *dst_engine) {
     using namespace data_type;

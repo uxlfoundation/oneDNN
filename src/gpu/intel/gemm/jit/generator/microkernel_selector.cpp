63cafc08d5b5089f885e4b9dd30eb416dee79f10
Umar Arshad <umar.arshad@intel.com>
2026-09-17 19:45:26 -0700

    xe: ukernel: Reduce ka_load on m_unroll. Allows for larger m_unroll

--- src/gpu/intel/gemm/jit/generator/microkernel_selector.cpp
+++ src/gpu/intel/gemm/jit/generator/microkernel_selector.cpp
@@ -48,8 +48,8 @@ namespace microkernel {
 using namespace ngen;
 
 static inline bool getStrategyByHeuristics(HW hw, GEMMStrategy &strategy, bool localA, bool localB,
-                                           GEMMProblem &problem, HWInformation hwInfo, SizeParams sizes,
-                                           const std::vector<StrategyRequirement> &reqs);
+                                           GEMMProblem &problem, HWInformation hwInfo, HostPayload host,
+                                           SizeParams sizes, const std::vector<StrategyRequirement> &reqs);
 
 static constexpr int smallGRF = 128, largeGRF = 256;
 
@@ -424,7 +424,7 @@ Package selectGEMM(const GEMMOptions &options, HostPayload host, HWInformation h
                 strategy.raHW = ngen::HW::XeHPC;
             }
         } else if (!reqs.empty() &&
-                   !getStrategyByHeuristics(hw, strategy, localA, localB, problem, hwInfo, sizes, reqs))
+                   !getStrategyByHeuristics(hw, strategy, localA, localB, problem, hwInfo, host, sizes, reqs))
             return false; /* No heuristic strategy found */
 
         strategy.systolicAvailable &= hwInfo.systolicAvailable;
@@ -478,13 +478,31 @@ Package selectGEMM(const GEMMOptions &options, HWInformation hwInfo, SizeParams 
 }
 
 static inline bool getStrategyByHeuristics(HW hw, GEMMStrategy &strategy, bool localA, bool localB,
-                                           GEMMProblem &problem, HWInformation hwInfo, SizeParams sizes,
-                                           const std::vector<StrategyRequirement> &reqs)
+                                           GEMMProblem &problem, HWInformation hwInfo, HostPayload host,
+                                           SizeParams sizes, const std::vector<StrategyRequirement> &reqs)
 {
     if (problem.C.layout == MatrixLayout::T) return false;
 
     int min2DAlignmentA = block2DMinAlignment(hw, problem.A, strategy.A, /* asIfBlock2D */ true);
     int min2DAlignmentB = block2DMinAlignment(hw, problem.B, strategy.B, /* asIfBlock2D */ true);
+
+    auto &s = strategy;
+    s.unroll[LoopK] = 1;
+    s.wg[LoopK] = 1;
+    s.unroll[LoopM] = s.unroll[LoopN] = 0;
+    s.wg[LoopM] = s.wg[LoopN] = 0;
+
+    for (auto &req: reqs) switch (req.param) {
+        case StrategyRequirement::UnrollM: s.unroll[LoopM] = req.value; break;
+        case StrategyRequirement::UnrollN: s.unroll[LoopN] = req.value; break;
+        case StrategyRequirement::WGM:         s.wg[LoopM] = req.value; break;
+        case StrategyRequirement::WGN:         s.wg[LoopN] = req.value; break;
+        case StrategyRequirement::WGK:         s.wg[LoopK] = req.value; break;
+        default: break;
+    }
+
+    if (s.wgTile(LoopM) * s.wgTile(LoopN) == 0)
+        return false;
 
     bool systolic = hwInfo.systolicAvailable &&
                    (problem.Ta.paddedSize() <= 2 || problem.Ta == Type::tf32) &&
@@ -501,7 +519,6 @@ static inline bool getStrategyByHeuristics(HW hw, GEMMStrategy &strategy, bool l
     bool block2DB = (hw >= HW::XeHPC) && systolic && (problem.B.alignment % min2DAlignmentB) == 0;
     bool useNewDP = (hw >= HW::XeHP);
 
-    auto &s = strategy;
     s.ka_load = s.kb_load = 16;
     if (!systolic) {
         // Keep eight bytes per integer operand without enlarging the other
@@ -510,6 +527,7 @@ static inline bool getStrategyByHeuristics(HW hw, GEMMStrategy &strategy, bool l
         s.kb_load = problem.Tb_ext.isInteger() ? 8 / problem.Tb_ext : 4;
     }
 
+    auto m_iter = s.unroll[LoopM] / host.simd;
     if (problem.A.layout == MatrixLayout::Pc) {
         s.A.accessType = AccessType::Block;
         s.A_copies = 2;
@@ -517,15 +535,14 @@ static inline bool getStrategyByHeuristics(HW hw, GEMMStrategy &strategy, bool l
     } else if (!block2DA) {
         s.A.accessType = AccessType::Block;
         if (systolic)
-            s.ka_load = (problem.A.layout == MatrixLayout::T) ? 64 / problem.Ta_ext : 16;
+            s.ka_load = (problem.A.layout == MatrixLayout::T) ? 64 / problem.Ta_ext / m_iter : 16;
         s.slmA = (hw >= HW::XeHP);
     } else if (problem.A.layout == MatrixLayout::T) {
         s.A.accessType = AccessType::Block2DTranspose;
-        s.ka_load = (int)(64.f / ceil(( 1.f * problem.Ta) +
-                                (problem.aOffset2D() ? (1.f * problem.Tao) : 0)));
-        s.ka_load = utils::roundup_pow2(s.ka_load);
+        s.ka_load = 64 / problem.Ta_ext / m_iter;
     } else if (problem.A.layout == MatrixLayout::N) {
         s.A.accessType = AccessType::Block2DVNNI;
+        s.ka_load =  s.unroll[LoopM] / m_iter;
         s.A_copies = 2;
     }
 
@@ -548,6 +565,13 @@ static inline bool getStrategyByHeuristics(HW hw, GEMMStrategy &strategy, bool l
     }
 
     s.C.accessType = AccessType::Block;
+
+    bool slmDequantize2DA = (problem.aOffset2D() || problem.aScale2D()) && s.slmA;
+    bool slmDequantize2DB = (problem.bOffset2D() || problem.bScale2D()) && s.slmB;
+    if (slmDequantize2DA && slmDequantize2DB) {
+        int min_load = std::max(s.ka_load, s.kb_load);
+        s.ka_load = s.kb_load = min_load;
+    }
 
     s.A.base = localA ? AddressBase::createSLM() : AddressBase::createA64(true);
     s.B.base = localB ? AddressBase::createSLM() : AddressBase::createA64(true);
@@ -575,20 +599,6 @@ static inline bool getStrategyByHeuristics(HW hw, GEMMStrategy &strategy, bool l
         s.kb_pfStride = s.kb_prefetch = s.kb_load;
     }
 
-    s.unroll[LoopK] = 1;
-    s.wg[LoopK] = 1;
-    s.unroll[LoopM] = s.unroll[LoopN] = 0;
-    s.wg[LoopM] = s.wg[LoopN] = 0;
-
-    for (auto &req: reqs) switch (req.param) {
-        case StrategyRequirement::UnrollM: s.unroll[LoopM] = req.value; break;
-        case StrategyRequirement::UnrollN: s.unroll[LoopN] = req.value; break;
-        case StrategyRequirement::WGM:         s.wg[LoopM] = req.value; break;
-        case StrategyRequirement::WGN:         s.wg[LoopN] = req.value; break;
-        case StrategyRequirement::WGK:         s.wg[LoopK] = req.value; break;
-        default: break;
-    }
-
     if(block2DA && !localA) {
         problem.A.alignment = std::min(problem.A.alignment,
                                         static_cast<uint8_t>(block2DMinAlignment(hw, problem.A, strategy.A)));
@@ -600,23 +610,6 @@ static inline bool getStrategyByHeuristics(HW hw, GEMMStrategy &strategy, bool l
                                         static_cast<uint8_t>(block2DMinAlignment(hw, problem.B, strategy.B)));
     } else {
         problem.B.alignment = std::min<uint8_t>(16, problem.B.alignment);
-    }
-
-    if (s.wgTile(LoopM) * s.wgTile(LoopN) == 0)
-        return false;
-
-    if(s.A.accessType == AccessType::Block2DVNNI) {
-        s.ka_load =  s.unroll[LoopN] / problem.Ta_ext;
-    } else if(s.A.accessType == AccessType::Block2DTranspose) {
-        s.ka_load = std::min(s.ka_load, s.unroll[LoopM] * 2);
-    }
-
-    bool slmDequantize2DA = (problem.aOffset2D() || problem.aScale2D()) && s.slmA;
-    bool slmDequantize2DB = (problem.bOffset2D() || problem.bScale2D()) && s.slmB;
-    if (slmDequantize2DA && slmDequantize2DB) {
-        // TODO: try max of ka_load/kb_load and see if it performs better
-        int min_load = std::min(s.ka_load, s.kb_load);
-        s.ka_load = s.kb_load = min_load;
     }
 
     s.systolic = systolic;

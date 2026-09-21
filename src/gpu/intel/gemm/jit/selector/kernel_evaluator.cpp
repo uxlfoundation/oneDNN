f1c9e4367a892ab386edaa2ec11f82b57c1c1e4a
Andy Kassen <andrew.kassen@intel.com>
2026-09-21 12:21:14 -0700

    xe: gemm: disable beta/post-op fusion on kv override

--- src/gpu/intel/gemm/jit/selector/kernel_evaluator.cpp
+++ src/gpu/intel/gemm/jit/selector/kernel_evaluator.cpp
@@ -600,7 +600,7 @@ void modifyStrategy(GEMMStrategy &strategy, const EvaluateAuxOutput &aux)
 {
     strategy.kParallel = aux.kParallel;
     if (strategy.kParallelVariable && !aux.kParallelVariable && !aux.kParallel)
-        strategy.C.atomic = strategy.CO.atomic = false;
+        strategy.C.atomic = strategy.CO.atomic = strategy.fuseBeta = strategy.fusePostOps = false;
     strategy.kParallelVariable = aux.kParallelVariable;
 }
 

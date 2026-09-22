1a38c2dc5c15efc7d4e77f2d1c05e17b93354a27
Lv, Tao A <tao.a.lv@intel.com>
2026-09-21 20:21:03 -0700

    gpu: intel: sdpa: fallback f32 sdpa w/ explicit mask on more platforms

--- src/gpu/intel/sdpa/micro.hpp
+++ src/gpu/intel/sdpa/micro.hpp
@@ -335,10 +335,7 @@ struct micro_fwd_t : public primitive_t {
                         vgs, static_cast<long int>(desc()->val_md()->dims[3]));
             }
 
-            VDISPATCH_SDPA(IMPLICATION((arch() == compute::gpu_arch_t::xe_hpc)
-                                           && (desc()->qry_md()->data_type
-                                                   == data_type::f32),
-                                   with_causal_mask()),
+            VDISPATCH_SDPA(IMPLICATION(is_f32, with_causal_mask()),
                     "fused f32 SDPA only optimized for causal mask"); //TODO: update when performance improved
             VDISPATCH_SDPA(
                     IMPLICATION(arch() == compute::gpu_arch_t::xe_hpg
